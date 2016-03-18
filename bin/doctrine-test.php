855a86f6d5e3d4b2700d36dab42dc1badf926938
david mintz <david@davidmintz.org>
2016-03-17 22:26:19 -0400

    scribble

--- bin/doctrine-test.php
+++ bin/doctrine-test.php
@@ -1,5 +1,18 @@
 <?php
-
+namespace Application\Entity;
 $em = require('./doctrine-bootstrap.php');
 
 printf("we have a %s\n",get_class($em));
+
+$interpreter = new Interpreter;
+
+$interpreter
+	->setLastname('Mintz')
+	->setDob(new \DateTime('1958-05-26'))
+	->setEmail('david@davidmintz.org')
+	->setPhone('201 978-0608');	
+	$em->persist($interpreter);
+	$em->flush();
+
+exit("all good\n");
+
