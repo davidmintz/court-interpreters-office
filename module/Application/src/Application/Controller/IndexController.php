62214e4d09755f18c53768625e4a65b972d30053
David Mintz <david@davidmintz.org>
2016-03-20 00:06:05 -0400

    update constructor to inject ServiceManager

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -1,28 +1,30 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * main controller
  */
 
 namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class IndexController extends AbstractActionController
 {
+    /** @var Zend\ServiceManager\ServiceLocatorInterface */
+    protected $serviceManager;
+    
+    public function __construct(ServiceLocatorInterface $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+    }
+    
     public function indexAction()
     {
         
-    	/*
-    	$sm = $this->getServiceLocator();
-    	$em = $sm->get('doctrine.entitymanager.orm_default');
-    	printf("Good news from %s:  we have a %s at line %d<br>",__CLASS__,get_class($em),__LINE__);
-    	printf("...and %s<br>",get_class($em->getConnection()));
-    	*/
+    	//$em = $this->serviceManager->get('doctrine.entitymanager.orm_default');
+    	//printf("Good news from %s:  we have a %s at line %d<br>",__CLASS__,get_class($em),__LINE__);
+    	//printf("...and %s<br>",get_class($em->getConnection()));
         return new ViewModel();
     }
 }
