798811f1f155d267dd00c7b6ac42691cb93a7643
David Mintz <david@davidmintz.org>
2016-03-20 00:05:21 -0400

    update controllers config

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -67,8 +67,8 @@ return array(
         ),
     ),
     'controllers' => array(
-        'invokables' => array(
-            'Application\Controller\Index' => Controller\IndexController::class
+        'factories' => array(
+            'Application\Controller\Index' => 'Application\Controller\Factory\IndexControllerFactory',
         ),
     ),
     'view_manager' => array(
