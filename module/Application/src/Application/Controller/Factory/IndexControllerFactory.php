f995efef3a3b4636869a6cd13257c6e92b8ab546
David Mintz <david@davidmintz.org>
2016-10-04 17:25:00 -0400

    update to ZF3

--- module/Application/src/Application/Controller/Factory/IndexControllerFactory.php
+++ module/Application/src/Application/Controller/Factory/IndexControllerFactory.php
@@ -4,6 +4,7 @@ namespace Application\Controller\Factory;
 
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
 use Application\Controller\IndexController;
 
 class IndexControllerFactory implements FactoryInterface
@@ -11,5 +12,10 @@ class IndexControllerFactory implements FactoryInterface
 	public function createService(ServiceLocatorInterface $serviceLocator)
 	{
 		return new IndexController($serviceLocator->getServiceLocator());
+	}
+
+	public function __invoke(ContainerInterface $container, $requestedName, Array $options = null) {
+		$serviceLocator = $container->getServiceLocator();
+		return new IndexController($serviceLocator);
 	}
 }
