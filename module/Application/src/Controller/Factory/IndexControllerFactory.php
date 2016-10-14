43b68970de8840ce48c1b5f36ee0f1aa2f03231b
David Mintz <david@davidmintz.org>
2016-10-14 16:44:51 -0400

    whatever

--- module/Application/src/Controller/Factory/IndexControllerFactory.php
+++ module/Application/src/Controller/Factory/IndexControllerFactory.php
@@ -7,12 +7,11 @@ use Zend\ServiceManager\ServiceLocatorInterface;
 use Interop\Container\ContainerInterface;
 use Application\Controller\IndexController;
 
-class IndexControllerFactory implements FactoryInterface
-{
-	
+class IndexControllerFactory implements FactoryInterface {
 
-	public function __invoke(ContainerInterface $container, $requestedName, Array $options = null) {
-		
-		return new IndexController($container);
-	}
+    public function __invoke(ContainerInterface $container, $requestedName, Array $options = null) {
+
+        return new IndexController($container);
+    }
+
 }
