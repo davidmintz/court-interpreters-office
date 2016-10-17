6638672b5dcd68b3cf4eca432a3dc15c74d7a8c9
David Mintz <david@davidmintz.org>
2016-10-17 19:13:28 -0400

    add unique index to deft name entity/table

--- module/Application/src/Entity/DefendantName.php
+++ module/Application/src/Entity/DefendantName.php
@@ -4,7 +4,8 @@ namespace Application\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /** 
-*  @ORM\Entity  @ORM\Table(name="defendant_names") 
+*  @ORM\Entity  @ORM\Table(name="defendant_names",uniqueConstraints={
+    @uniqueConstraint(name="unique_deftname",columns="{given_names, surnamess}")}) 
 *  //ORM\Entity(repositoryClass="Application\Entity\DefendantNameRepository") 
 */
 class DefendantName 
