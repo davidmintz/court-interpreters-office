25086e5ad1804cd289ac30a273b7367ee1be15ea
David Mintz <david@davidmintz.org>
2016-10-19 12:31:25 -0400

    revise sample_data sql script

--- bin/sql/sample_data.sql
+++ bin/sql/sample_data.sql
@@ -1,58 +1,25 @@
 
--- MySQL dump 10.13  Distrib 5.5.50, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: office
--- ------------------------------------------------------
--- Server version	5.5.50-0+deb8u1
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+DELETE FROM `languages`;
 INSERT INTO `languages` VALUES (1,'Spanish',''),(2,'Russian',''),(3,'French',''),(4,'Foochow',''),(5,'Arabic','');
-/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Dumping data for table `languages`
---
-
-LOCK TABLES `languages` WRITE;
-/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
-UNLOCK TABLES;
-
-
---
--- Dumping data for table `languages`
---
-
-LOCK TABLES `languages` WRITE;
-/*!40000 ALTER TABLE `languages` DISABLE KEYS */;
-INSERT INTO `languages` VALUES (1,'Spanish',''),(2,'Foochow',''),(3,'Russian',''),(4,'Arabic',''),(5,'French','');
-/*!40000 ALTER TABLE `languages` ENABLE KEYS */;
-
--- Dumping data for table `judge_flavors`
---
-
-LOCK TABLES `judge_flavors` WRITE;
+DELETE FROM `judge_flavors`;
 /*!40000 ALTER TABLE `judge_flavors` DISABLE KEYS */;
 INSERT INTO `judge_flavors` VALUES (1,'USDJ'),(2,'USMJ');
 /*!40000 ALTER TABLE `judge_flavors` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-10-17 16:48:12
+DELETE FROM hats;
+INSERT INTO `hats` VALUES 
+(1,'staff Court Interpreter'),
+(2,'staff, Interpreters Office'),
+(3,'contract court interpreter'),
+(4,'defense attorney'),
+(5,'AUSA'),
+(6,'Courtroom Deputy'),
+(7,'Law Clerk'),
+(8,'USPO'),
+(9,'Pretrial Services Officer'),
+(10,'paralegal'),
+(11,'staff, US Attorneys Office');
+DELETE FROM `anonymous_hats`;
+INSERT INTO `anonymous_hats` VALUES (1,'Magistrate'),(2,'Pretrial'),(3,'defense attorney');
