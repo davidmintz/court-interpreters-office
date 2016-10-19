3148f578729418d4d24521219b8139c1a04b0c01
David Mintz <david@davidmintz.org>
2016-10-19 12:22:03 -0400

    rename/doodle with FixtureSetupTest

---
+++ module/Application/test/FixtureSetupTest.php
@@ -0,0 +1,86 @@
+<?php
+namespace ApplicationTest;
+
+
+use Zend\Stdlib\ArrayUtils;
+use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
+//use PHPUnit_Framework_TestCase;
+
+/** @todo put this bit in a bootstrap file and add it to phpunit.xml ? */
+
+//require __DIR__. '/../../../vendor/autoload.php';
+    
+class FixtureSetupTest extends  
+   // PHPUnit_Framework_TestCase
+   AbstractHttpControllerTestCase
+{
+    
+    public function setUp()
+    {
+        ///*
+        $configOverrides = [
+            
+            'module_listener_options' => [
+                'config_glob_paths' => [
+                    __DIR__.'/config/autoload/{{,*.}global,{,*.}local}.php'
+                ],
+            ],
+            
+        ];
+        
+        $this->setApplicationConfig(ArrayUtils::merge(
+            include __DIR__ . '/../../../config/application.config.php',
+            $configOverrides
+        ));
+       // */
+        parent::setUp();
+        
+        
+    }
+    /**
+     * @return Doctrine\ORM\EntityManager
+     */
+    public function getEntityManager()
+    {
+        return $this->getApplicationServiceLocator()->get('entity-manager');
+    }
+    public function testSomething()
+    {       
+        
+        $container = $this->getApplicationServiceLocator();
+        $this->assertTrue($container instanceof \Interop\Container\ContainerInterface);
+        
+        $objectManager = $this->getEntityManager();
+        $connection = $objectManager->getConnection();
+        $driver = $connection->getDriver();
+        $this->assertEquals('pdo_sqlite',$driver->getName());
+        $this->dispatch('/');
+        $this->assertResponseStatusCode(200);
+       
+    }
+    
+    public function testDataFixtureSanity() {
+        
+        $this->assertTrue(class_exists('ApplicationTest\FixtureManager'));
+        $shit = FixtureManager::getFixtureExecutor();
+        $this->assertTrue(is_object($shit));
+        $fixtureExecutor = FixtureManager::getFixtureExecutor();
+        //FixtureManager::start();
+        $fixtureExecutor->execute([
+            new DataFixture\LanguageLoader(),
+            new DataFixture\HatLoader(),
+         ]);
+        $entityManager = FixtureManager::getEntityManager();
+        $this->assertTrue(is_object($entityManager));
+        //echo get_class($entityManager);
+        $languages = $entityManager->getRepository('Application\Entity\Language')->findAll();
+        $this->assertTrue(is_array($languages));
+        /** @var Doctrine\DBAL\Connection $connection */
+        $connection = $entityManager->getConnection();
+        $count = (int) $connection->fetchColumn("select count(*) from languages");
+        $this->assertEquals($count,count($languages));
+
+    }
+}
+
+
