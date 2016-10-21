1e668596d8084c88c68245b13594e9893c0d6f16
David Mintz <david@davidmintz.org>
2016-10-21 14:12:07 -0400

    HatLoader:  loads more entities

--- module/Application/test/DataFixture/HatLoader.php
+++ module/Application/test/DataFixture/HatLoader.php
@@ -7,35 +7,64 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 
 use Application\Entity;
 
-
+/**
+* loads test fixtures for "hats" and similar
+*
+*/
 
 
 class HatLoader implements FixtureInterface {
+
     public function load(ObjectManager $objectManager)
     {
+        
+        // create the Role entities
+        foreach (['submitter','manager','administrator'] as $roleName) {
+            $role = new Entity\Role;
+            $role->setName($roleName);
+        }
+        $objectManager->flush();
+
+        $submitter = $objectManager->getRepository('Application\Entity\Role')->findOneBy(['name'=>'submitter']);
+        $manager = $objectManager->getRepository('Application\Entity\Role')->findOneBy(['name'=>'manager']);
+
+        // create the Hat entities
         $hats = [
-            ['AUSA',false],
-            ['contract court interpreter',false],
-            ['Courtroom Deputy',false],
-            ['defense attorney',true],
-            ['Law Clerk',false],
-            ['paralegal',false],
-            ['Pretrial Services Officer',false],
-            ['staff Court Interpreter',false],
-            ['staff, Interpreters Office',false],
-            ['staff, US Attorneys Office',false],
-            ['USPO',false],
-            ['Magistrates office',true],
-            ['Pretrial services',true],
+            ['AUSA',false,null],
+            ['contract court interpreter',false,null],
+            ['Courtroom Deputy',false,$submitter],
+            ['defense attorney',true,null],
+            ['Law Clerk',false,$submitter],
+            ['paralegal',false,null],
+            ['Pretrial Services Officer',false,$submitter],
+            ['staff Court Interpreter',false,$submitter],
+            ['staff, Interpreters Office',false,$manager],
+            ['staff, US Attorneys Office',false,null],
+            ['USPO',false,$submitter],
+            ['Magistrates office',true,null],
+            ['Pretrial services',true,null],
            
         ];
         foreach ($hats as $hat) {
             
             $entity = new Entity\Hat();
             $entity->setName($hat[0])->setAnonymous($hat[1]);
+            if ($hat[2]) { $entity->setRole($hat[2]); }
             $objectManager->persist($entity);
         }
       
         $objectManager->flush();
+
+        // create the AnonymousJudge entities
+        $anonymous_judges = ['Magistrate','not applicable','unknown'];
+
+        foreach ($anonymous_judges as $j) {
+            $entity = new Entity\AnonymousJudge;
+            $entity->setName($j);
+            $objectManager->persist($entity);
+        }
+        $objectManager->flush();
+
+
     }
 }
