aded3fdd062db70a9477a17e560441cd96cdea69
David Mintz <david@davidmintz.org>
2016-10-26 15:53:50 -0400

    add a test to prove Event entity's lifecycle callback checks that either judge or anonymous judge is NOT NULL

--- module/Application/test/FixtureSetupTest.php
+++ module/Application/test/FixtureSetupTest.php
@@ -6,6 +6,8 @@ use Zend\Stdlib\ArrayUtils;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 //use PHPUnit_Framework_TestCase;
 
+use Application\Entity;
+
 /** @todo put this bit in a bootstrap file and add it to phpunit.xml ? */
 
 //require __DIR__. '/../../../vendor/autoload.php';
@@ -34,8 +36,6 @@ class FixtureSetupTest extends
         ));
        // */
         parent::setUp();
-        
-        
     }
     /**
      * @return Doctrine\ORM\EntityManager
@@ -58,7 +58,24 @@ class FixtureSetupTest extends
         $this->assertResponseStatusCode(200);
        
     }
-    
+    public function loadTestEventData()
+    {
+        $fixtureExecutor = FixtureManager::getFixtureExecutor();
+        //$entityManager->getC
+        //FixtureManager::start();
+        $fixtureExecutor->execute([
+            new DataFixture\LanguageLoader(),
+            new DataFixture\HatLoader(),
+            new DataFixture\EventTypeLoader(),
+            new DataFixture\LocationLoader(),
+            new DataFixture\DefendantNameLoader(),
+            new DataFixture\JudgeLoader(),
+            new DataFixture\InterpreterLoader(),
+            new DataFixture\CancellationReasonLoader(),
+            new DataFixture\UserLoader(),
+            new DataFixture\EventLoader(),
+         ]);
+    }
     public function testDataFixtureSanity() {
         
         $this->assertTrue(class_exists('ApplicationTest\FixtureManager'));
@@ -89,6 +106,65 @@ class FixtureSetupTest extends
         $count = (int) $connection->fetchColumn("select count(*) from languages");
         $this->assertEquals($count,count($languages));
 
+    }
+    /**
+     * test that a RuntimeException will be thrown if we try to persist an Event
+     * with no Judge and no anonymous judge.
+     */
+    public function testExceptionThrownWhenNoJudgeOrAnonymousJudgeIsSet() {
+        $this->loadTestEventData();
+        $event = new Entity\Event();
+                $date = new \DateTime("next monday");
+
+        $time = new \DateTime('10:00 am');
+        $objectManager = FixtureManager::getEntityManager();
+        $language = $objectManager->getRepository('Application\Entity\Language')
+                ->findOneBy(['name'=>'Spanish']);
+
+        $eventType = $objectManager->getRepository('Application\Entity\EventType')
+                ->findOneBy(['name'=>'pretrial conference']);
+
+        $comments = 'test one two';
+
+        $dql = "SELECT u FROM Application\Entity\User u JOIN u.person p "
+                . "WHERE p.email = 'john_somebody@nysd.uscourts.gov'";
+        $query = $objectManager->createQuery($dql);
+        $user = $query->getSingleResult();
+
+        $interpreter = $objectManager->getRepository('Application\Entity\Interpreter')
+                ->findOneBy(['lastname'=>'Mintz']);
+        
+        $defendant =  $objectManager->getRepository('Application\Entity\DefendantName')
+                ->findOneBy(['surnames'=>'Fulano Mengano']);
+        $event = new Entity\Event();
+        $now = new \DateTime(); 
+        //$judge = $objectManager->getRepository('Application\Entity\Judge')
+        //        ->findOneBy(['lastname'=>'Failla']);
+        $event
+            ->setDate($date)
+            ->setTime($time)
+            ->setJudge(null)
+            ->setLanguage($language)
+            ->setEventType($eventType)
+            ->setDocket('2016-CR-0123')
+            ->setComments($comments)
+            ->setAdminComments('')
+            ->setSubmitter($user->getPerson())
+            ->setModified($now)
+            ->setCreated($now)
+            ->setCreatedBy($user)
+            ->setModifiedBy($user)
+            ->addInterpretersAssigned(
+                 (new Entity\InterpreterEvent($interpreter,$event))->setCreatedBy($user)
+             )
+             ->addDefendant($defendant);//->setJudge($judge); 
+        
+        $this->expectException(\RuntimeException::class);
+        // this should suffice to throw a RuntimeException
+        // and prove our lifecycle callback works
+        $objectManager->persist($event);
+        
+        
     }
 }
 
