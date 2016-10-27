18a7602bcfd48016208abae7517dc9e58daeced8
David Mintz <david@davidmintz.org>
2016-10-27 15:09:38 -0400

    add experimental stuff involving Language entity and Doctrine form hydrator

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -42,11 +42,38 @@ class IndexController extends AbstractActionController
      */
     public function indexAction()
     {
+        
+    }
+    /**
+     * temporary action for experimenting and doodling.
+     * 
+     * this demonstrates that we can build a form from annotations
+     * and bind the form to a Doctrine entity. our thinking is to 
+     * use this technique for creating and processing forms for create and
+     * update actions on the simple or relatively simple entities.  
+     * 
+     */
+    public function testAction()
+    {
         $builder = new  \Zend\Form\Annotation\AnnotationBuilder($this->serviceManager->get('entity-manager'));
         $form    = $builder->createForm(\Application\Entity\Language::class);
         $em      = $this->serviceManager->get('entity-manager');
         $form->setHydrator(new \DoctrineModule\Stdlib\Hydrator\DoctrineObject($em));
+        $viewModel = new ViewModel(['form' => $form]);
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $data = $request->getPost();
+            $language = new \Application\Entity\Language();
+            $form->bind($language);
+            $form->setData($data);
+            if (! $form->isValid()) {
+                return $viewModel;
+            }
+            $em->persist($language);
+            $em->flush();
+            $this->flashMessenger()->addMessage("congratulations! you inserted the language $language.");
+            return $this->redirect()->toRoute('home');
+        }
         return new ViewModel(['form'=>$form]);
     }
-
 }
