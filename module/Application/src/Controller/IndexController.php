2e488099d5bd1cbf0a17c82cd81b1de54c0c6cc7
David Mintz <david@davidmintz.org>
2016-11-15 18:41:57 -0500

    scribble on IndexController to experiment with authentication

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -224,15 +224,17 @@ class IndexController extends AbstractActionController
                 return $viewModel;
             }
         }
-
-
-
     */
+
+
+    /**
+     * example to show authentication is working
+     */
     public function shitAction()
     {
         echo "shit is happening...<br>"; 
         $service = new \Zend\Authentication\AuthenticationService();
-        $adapter = new \Application\Service\AuthAdapter([
+        $adapter = new \Application\Service\Authentication\Adapter([
             'object_manager' => $this->em,//'Doctrine\ORM\EntityManager',
             //'identity_class' => '\Application\Entity\User',
             // maybe change this to identity_properties, plural?
@@ -254,116 +256,5 @@ class IndexController extends AbstractActionController
     }
 
 }
-namespace Application\Service\Authentication;
 
-use Zend\Authentication\Result as AuthResult;
 
-class Result extends AuthResult {
-    const FAILURE_USER_ACCOUNT_DISABLED = -10;
-}
-
-namespace Application\Service;
-//use Zend\Authentication\Adapter\AdapterInterface;
-
-use DoctrineModule\Authentication\Adapter\ObjectRepository;
-use Application\Service\Authentication\Result;
-
-class AuthAdapter extends ObjectRepository
-{
-
-    public function __construct($options = [])
-    {
-        
-        parent::__construct($options);
-    }
-
-   public function authenticate()
-    {
-        $this->setup();
-        $options  = $this->options;
-        $objectManager = $options->getObjectManager();
-        $query = $objectManager->createQuery("SELECT u FROM Application\Entity\User u JOIN u.person p "
-                . "WHERE p.email = :identity OR u.username = :identity")
-                ->setParameters([':identity'=>$this->identity]);
-        
-        $identity = $query->getOneOrNullResult();
-            // rather than:
-            //->getObjectRepository()->findOneBy(array($options->getIdentityProperty() => $this->identity));
-
-        if (!$identity) {            
-            $this->authenticationResultInfo['code']       = \Zend\Authentication\Result::FAILURE_IDENTITY_NOT_FOUND;
-            $this->authenticationResultInfo['messages'][] = 'A record with the supplied identity could not be found.';
-
-            return $this->createAuthenticationResult();
-        }
-
-        $authResult = $this->validateIdentity($identity);
-
-        return $authResult;
-    }
-    
-    protected function validateIdentity($identity) {
-        //parent::validateIdentity($identity);
-        $credentialProperty = $this->options->getCredentialProperty();
-        $getter             = 'get' . ucfirst($credentialProperty);
-        $documentCredential = null;
-
-        if (method_exists($identity, $getter)) {
-            $documentCredential = $identity->$getter();
-        } elseif (property_exists($identity, $credentialProperty)) {
-            $documentCredential = $identity->{$credentialProperty};
-        } else {
-            throw new Exception\UnexpectedValueException(
-                sprintf(
-                    'Property (%s) in (%s) is not accessible. You should implement %s::%s()',
-                    $credentialProperty,
-                    get_class($identity),
-                    get_class($identity),
-                    $getter
-                )
-            );
-        }
-        //$identity->getActive() ? " user is active ": " user is NOT active ";
-        $credentialValue = $this->credential;
-        $callable        = $this->options->getCredentialCallable();
-
-        if ($callable) {
-            $credentialValue = call_user_func($callable, $identity, $credentialValue);
-        }
-
-        if ($credentialValue !== true && $credentialValue !== $documentCredential) {
-            $this->authenticationResultInfo['code']       = Result::FAILURE_CREDENTIAL_INVALID;
-            $this->authenticationResultInfo['messages'][] = 'Supplied credential is invalid.';
-
-            return $this->createAuthenticationResult();
-        }
-        // this is in addition to the method we've overridden
-        if (! $identity->isActive()) {
-             $this->authenticationResultInfo['code']       = Result::FAILURE_USER_ACCOUNT_DISABLED;
-             $this->authenticationResultInfo['messages'][] = 'User account is disabled (inactive).';
-             
-             return $this->createAuthenticationResult();
-        }
-
-        $this->authenticationResultInfo['code']       = Result::SUCCESS;
-        $this->authenticationResultInfo['identity']   = $identity;
-        $this->authenticationResultInfo['messages'][] = 'Authentication successful.';
-
-        return $this->createAuthenticationResult();
-    }
-     /**
-     * Creates a Application\Service\Authentication\Result object from the information 
-     * that has been collected during the authenticate() attempt.
-     *
-     * @return Application\Service\Authentication\Result
-     */
-    protected function createAuthenticationResult()
-    {
-        return new Result(
-            $this->authenticationResultInfo['code'],
-            $this->authenticationResultInfo['identity'],
-            $this->authenticationResultInfo['messages']
-        );
-    }
-
-}
