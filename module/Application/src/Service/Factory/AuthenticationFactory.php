1a9613ba32034d3c675b10b6376ea230a06d94ae
David Mintz <david@davidmintz.org>
2016-11-16 14:35:01 -0500

    Authentication factory returns a Zend\Authentication\AuthenticationService

--- module/Application/src/Service/Factory/AuthenticationFactory.php
+++ module/Application/src/Service/Factory/AuthenticationFactory.php
@@ -4,11 +4,15 @@ namespace Application\Service\Factory;
 
 use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\Authentication\AuthenticationService;
+
 
 class AuthenticationFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        return $container->get('doctrine.authenticationservice.orm_default');
+        //echo "shit? in ".__CLASS__ . " ... ";
+        //return $container->get('doctrine.authenticationservice.orm_default');
+        return new AuthenticationService;
     }
 }
