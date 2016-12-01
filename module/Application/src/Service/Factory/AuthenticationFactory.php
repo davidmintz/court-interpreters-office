95ae0c3a85fb964112ab456fd1349e8acacf8ad2
David Mintz <david@davidmintz.org>
2016-12-01 16:48:48 -0500

    clean up AuthenticationFactory

--- module/Application/src/Service/Factory/AuthenticationFactory.php
+++ module/Application/src/Service/Factory/AuthenticationFactory.php
@@ -11,16 +11,10 @@ class AuthenticationFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        //echo "shit? in ".__CLASS__ . " ... ";
-        //return $container->get('doctrine.authenticationservice.orm_default');
         $adapter = new AuthenticationAdapter([
-            'object_manager' => $this->em,//'Doctrine\ORM\EntityManager',
+            'object_manager' => $container->get('entity-manager'),
             'credential_property' => 'password',
-            'credential_callable' => Application\Entity\User::verifyPassword,
-            // 'credential_callable' => function (User $user, $passwordGiven) {
-            //     return my_awesome_check_test($user->getPassword(), $passwordGiven);
-            // },
-
+            'credential_callable' => 'Application\Entity\User::verifyPassword',
             ]);
         return new AuthenticationService(null, $adapter);
     }
