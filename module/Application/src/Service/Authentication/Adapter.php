1225fbf022191731f3d2c1ff14e9a65155861b0b
David Mintz <david@davidmintz.org>
2016-12-01 17:20:10 -0500

    streamline validateIdentity() in authentication adapter

--- module/Application/src/Service/Authentication/Adapter.php
+++ module/Application/src/Service/Authentication/Adapter.php
@@ -42,46 +42,34 @@ class Adapter extends ObjectRepository
     
     /**
      * validates the identity
-     * @param User - not type-hinted for the sake of interface compatibility
+     * @param User $identity -- not type-hinted for the sake of interface compatibility
      * @return \Application\Service\Authentication\Result
      */
     protected function validateIdentity($identity) {
 
-        //parent::validateIdentity($identity);
-        $credentialProperty = $this->options->getCredentialProperty();
-        $getter             = 'get' . ucfirst($credentialProperty);
-        $documentCredential = null;
-
-        if (method_exists($identity, $getter)) {
-            $documentCredential = $identity->$getter();
-        } elseif (property_exists($identity, $credentialProperty)) {
-            $documentCredential = $identity->{$credentialProperty};
-        } else {
+        if (! method_exists($identity, 'getPassword')) {
             throw new Exception\UnexpectedValueException(
-                sprintf(
-                    'Property (%s) in (%s) is not accessible. You should implement %s::%s()',
-                    $credentialProperty,
-                    get_class($identity),
-                    get_class($identity),
-                    $getter
-                )
+                  'validateIdentity() expects an object that implements '
+                    . ' a public getPassword() method'
             );
         }
-        //$identity->getActive() ? " user is active ": " user is NOT active ";
-        $credentialValue = $this->credential;
+        $documentCredential = $identity->getPassword();
+        
+        // $documentCredential means the hashed password, as stored
+        $credentialValue = $this->credential; // i.e., submitted password
         $callable        = $this->options->getCredentialCallable();
-        //var_dump($callable);
+        
         if ($callable) {
             $credentialValue = call_user_func($callable, $identity, $credentialValue);
         } 
-
+        
         if ($credentialValue !== true && $credentialValue !== $documentCredential) {
             $this->authenticationResultInfo['code']       = Result::FAILURE_CREDENTIAL_INVALID;
             $this->authenticationResultInfo['messages'][] = 'Supplied credential is invalid.';
 
             return $this->createAuthenticationResult();
         }
-        // this is in addition to the method we've overridden
+        // this is what we've added to the parent method
         if (! $identity->isActive()) {
              $this->authenticationResultInfo['code']       = Result::FAILURE_USER_ACCOUNT_DISABLED;
              $this->authenticationResultInfo['messages'][] = 'User account is disabled (inactive).';
