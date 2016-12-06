819cfdfaf901a976529c45f1e76db2ebb5e928f4
David Mintz <david@davidmintz.org>
2016-12-06 16:30:45 -0500

    add test routes to Admin module

--- module/Admin/config/module.config.php
+++ module/Admin/config/module.config.php
@@ -1,1 +1,30 @@
-<?php return [];
+<?php 
+namespace InterpretersOffice\Admin;
+use Zend\Router\Http\Segment;
+
+return [
+    
+    'router'=>[
+        'routes' => [
+            
+            'test' => [
+                'type' => Segment::class,
+                'options' => [
+                    'route' => '/admintest',
+                    'defaults' => [
+                        'module' => __NAMESPACE__,
+                        'controller' => Controller\IndexController::class,
+                        'action' => 'test',
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        
+        'invokables'  => [
+            Controller\IndexController::class => Controller\IndexController::class,
+        ],
+    ],
+    
+];
