548d9545549b0a56e34222b19760313196b96a5b
David Mintz <david@davidmintz.org>
2016-12-06 16:32:21 -0500

    add IndexController to Admin module

---
+++ module/Admin/src/Controller/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+namespace InterpretersOffice\Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+//use Zend\View\Model\ViewModel;
+//use Interop\Container\ContainerInterface;
+
+class IndexController extends AbstractActionController {
+    
+    public function testAction()
+    {
+        echo "HELLO!<br>";
+        return false;
+            
+    }
+    
+}
