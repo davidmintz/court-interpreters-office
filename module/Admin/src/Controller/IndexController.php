85f4d08a958836ceca503d3f1cc6f62478791a56
David Mintz <david@davidmintz.org>
2016-12-08 15:50:00 -0500

    make stub index action and view for admin main page

--- module/Admin/src/Controller/IndexController.php
+++ module/Admin/src/Controller/IndexController.php
@@ -2,16 +2,16 @@
 namespace InterpretersOffice\Admin\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-//use Zend\View\Model\ViewModel;
+use Zend\View\Model\ViewModel;
 //use Interop\Container\ContainerInterface;
 
 class IndexController extends AbstractActionController {
     
-    public function testAction()
+    public function indexAction()
     {
-        echo "HELLO!<br>";
-        return false;
-            
+        //echo "HELLO!<br>";
+       //return false;
+       return new ViewModel();   
     }
     
 }
