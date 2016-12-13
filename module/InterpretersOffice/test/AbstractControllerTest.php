4959c1e09d8e605d82092905097bf3320df4b0db
David Mintz <david@davidmintz.org>
2016-12-13 14:49:19 -0500

    base AbstractControllerTest: add getCsrfToken() and update login() methods

--- module/InterpretersOffice/test/AbstractControllerTest.php
+++ module/InterpretersOffice/test/AbstractControllerTest.php
@@ -10,6 +10,8 @@ use Zend\Stdlib\ArrayUtils;
 use Zend\Stdlib\Parameters;
 use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase;
 
+use Zend\Dom\Query;
+
 /**
  * base class for unit tests.
  */
@@ -47,12 +49,29 @@ class AbstractControllerTest extends AbstractHttpControllerTestCase
         $this->getRequest()->setMethod('POST')->setPost(
             new Parameters(
                 [
-                    'identity' => 'susie',
-                    'password' => 'boink',
+                    'identity' => $identity,
+                    'password' => $password,
+                    'csrf'     => $this->getCsrfToken('/login'),
                 ]
             )
         );
         $this->dispatch('/login');
         $this->reset(true);
+    }
+    /**
+     * parses out a csrf token from a form
+     * 
+     * @param string $url to dispatch
+     * @param string $name name of the CSRF form element
+     * @return string $token parsed from document body
+     */
+    public function getCsrfToken($url,$name='csrf')
+    {
+        $this->dispatch($url,'GET');
+        $query = new Query($this->getResponse()->getBody());
+        $selector = sprintf('input[name="%s"]',$name);
+        $node = $query->execute($selector)->current();
+        $token = $node->attributes->getNamedItem('value')->nodeValue;
+        return $token;
     }
 }
