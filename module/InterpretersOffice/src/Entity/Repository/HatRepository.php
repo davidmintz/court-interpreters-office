ed9a84a725bf616fe0c0ceab0abc4f34ab11d2c9
David Mintz <david@davidmintz.org>
2016-12-20 17:08:51 -0500

    add custom Hat repository

---
+++ module/InterpretersOffice/src/Entity/Repository/HatRepository.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  module/InterpretersOffice/src/Entity/Repository/HatRepository.php
+ */
+
+namespace InterpretersOffice\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Hat repository
+ *
+ * @author david
+ */
+class HatRepository extends EntityRepository
+{
+    
+    /**
+     * returns Hat entities for Person form's select element.
+     * 
+     * this excludes the Hat entities that correspond to subtypes of the 
+     * Person entity
+     * 
+     * @return array
+     */
+    public function getHatsForPersonForm()
+    {
+        $dql = 'SELECT h FROM InterpretersOffice\Entity\Hat h '
+           . 'WHERE h.name NOT LIKE \'%court interpreter%\' AND h.name <> \'Judge\'';
+        $query = $this->getEntityManager()->createQuery($dql);
+        return $query->getResult();
+    }
+    
+}
