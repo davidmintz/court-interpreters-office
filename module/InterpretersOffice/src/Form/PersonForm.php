ff4808ce61d9a4ff518353bc4fd297376494235c
David Mintz <david@davidmintz.org>
2016-12-21 16:31:20 -0500

    PersonForm: make fieldset classname an instance variable, merge email validation in constructor.

--- module/InterpretersOffice/src/Form/PersonForm.php
+++ module/InterpretersOffice/src/Form/PersonForm.php
@@ -1,5 +1,5 @@
 <?php 
-
+/** module/InterpretersOffice/src/Form/PersonForm.php */
 namespace InterpretersOffice\Form;
 
 use Zend\Form\Form as ZendForm;
@@ -8,21 +8,91 @@ use Zend\Form\Element\Csrf;
 //use DoctrineModule\Persistence\ObjectManagerAwareInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use Zend\InputFilter;
+
+/**
+ * Form for a Person entity.
+ */
 class PersonForm extends ZendForm //implements ObjectManagerAwareInterface
 {
 	//use ObjectManagerAwareTrait;
     
+    /** 
+     * name of the form
+     * 
+     * @var string
+     */
     protected $formName = 'person-form';
     
+    /**
+     * name of Fieldset class to instantiate and add to the form.
+     * 
+     * the idea is that subclasses can override this. 
+     * 
+     * @var string
+     */
+    protected $fieldsetClass = PersonFieldset::class;
+    
+    /**
+     * constructor
+     * 
+     * @todo figure out how best to make it easy/obligatory for subclasses to 
+     * provide their own validation to certain elements. extend an abstract class
+     * with an abstract getAdditionalInputFilter() or something ?
+     * 
+     * @param ObjectManager $objectManager
+     * @param Array $options
+     */
 	public function __construct(ObjectManager $objectManager,$options = null)
 	{
 			
 		parent::__construct($this->formName,$options);
-		$fieldset = new PersonFieldset($objectManager);
+		$fieldset = new $this->fieldsetClass($objectManager);
 		$this->add($fieldset);
+        // how to add email validation after the fact
+        $factory = new InputFilter\Factory();
+        
+        $inputFilter = $factory->createInputFilter(
+            [
+                'email' => [
+                    'allow_empty' => true,
+                    'validators' => [
+                        /*
+                        [
+                            'name' => 'NotEmpty',
+                            'options' => [
+                                'messages'=>['isEmpty'=> 'email address is required']
+                            ],
+                            'break_chain_on_failure' => true,
+                        ],*/
+                        [
+                            'name' => 'Zend\Validator\EmailAddress',
+                            'options' => [
+                                'messages' => [
+                                    \Zend\Validator\EmailAddress::INVALID_FORMAT =>
+                                        'well-formed email address is required'
+                                ],
+                            ],
+                            //'break_chain_on_failure' => true,
+                        ],
+                    ],
+                    'filters' => [
+                        ['name' => 'StringTrim'],
+                    ],
+                ],
+            ]
+        );
+        $this->getInputFilter()->get('person')->merge($inputFilter);
         $this->add(new Csrf('csrf'));
 	}
-
-
-
 }
+
+/*
+    // does not work:
+    $input = $this->getInputFilter()->get('person')->get('email');
+    $input->getValidatorChain()->prependByName('NotEmpty', [
+        'allow_empty' => false,
+        'break_chain_on_failure'=> true,
+        'messages'=>['isEmpty'=> 'email is required']]
+    );
+*/
