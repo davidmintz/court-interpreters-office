17d171cbd544232005775a5d0633a92ae982cd11
David Mintz <david@davidmintz.org>
2016-12-27 11:11:40 -0500

    edit comments in PeopleControllerTest

--- module/Admin/test/Controller/PeopleControllerTest.php
+++ module/Admin/test/Controller/PeopleControllerTest.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- *
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * module/Admin/test/Controller/PeopleControllerTest.php
  */
 
 namespace ApplicationTest\Controller;
