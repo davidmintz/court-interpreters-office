cf46943495f246edd65cc1b4eb8e9d2c8cb097e4
David Mintz <david@davidmintz.org>
2016-12-28 17:03:24 -0500

    scribble with rendering strategies

--- module/InterpretersOffice/src/Module.php
+++ module/InterpretersOffice/src/Module.php
@@ -24,5 +24,37 @@ class Module
         return include __DIR__.'/../config/module.config.php';
     }
 
-    //public function onBootstrap(MvcEvent $event) { }
+     /**
+     * @param  \Zend\Mvc\MvcEvent $e The MvcEvent instance
+     * @return void
+     */
+    public function onBootstrap($e)
+    {
+       // from the tutorial, a waste of time:
+       
+        // Register a "render" event, at high priority (so it executes prior
+       // to the view attempting to render)
+       // $app = $e->getApplication();
+       // $app->getEventManager()->attach('render', [$this, 'registerJsonStrategy'], 100);
+    }
+
+    /**
+     * DOES NOT WORK!
+     * https://docs.zendframework.com/zend-view/quick-start/#creating-and-registering-alternate-rendering-and-response-strategies
+     * @param  \Zend\Mvc\MvcEvent $e The MvcEvent instance
+     * @return void
+     */
+    public function registerJsonStrategy($e)
+    {
+        $app          = $e->getTarget();
+        $locator      = $app->getServiceManager();
+        $view         = $locator->get('Zend\View\View');
+        $jsonStrategy = $locator->get('ViewJsonStrategy');
+
+        // Attach strategy, which is a listener aggregate, at high priority
+        $view->getEventManager()->attach(
+            \Zend\View\ViewEvent::EVENT_RENDERER,
+            [$jsonStrategy,'selectRenderer'], 100);
+    }
 }
+
