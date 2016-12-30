97c373dd1faa88d61274397105e7bf44578c9bf2
David Mintz <david@davidmintz.org>
2016-12-30 13:25:04 -0500

    add some session configuration to global config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -10,7 +10,28 @@
  * control, so do not include passwords or other sensitive information in this
  * file.
  */
+use Zend\Session\Storage\SessionArrayStorage;
+use Zend\Session\Validator\RemoteAddr;
+use Zend\Session\Validator\HttpUserAgent;
 
 return [
-
+    // courtesy of:
+    //https://github.com/olegkrivtsov/using-zf3-book-samples/blob/master/userdemo/config/autoload/global.php
+    'session_config' => [
+        // maybe change these values
+        'cookie_lifetime'     => 60*60*1, // Session cookie will expire in 1 hour.
+        'gc_maxlifetime'      => 60*60*24*30, // How long to store session data on server (for 1 month).        
+    ],
+    // Session manager configuration.
+    'session_manager' => [
+        // Session validators (used for security).
+        'validators' => [
+            RemoteAddr::class,
+            HttpUserAgent::class,
+        ]
+    ],
+    // Session storage configuration.
+    'session_storage' => [
+        'type' => SessionArrayStorage::class
+    ],
 ];
