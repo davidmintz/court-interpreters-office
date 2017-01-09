1f10e2f74c385002561f37af06f441eb52ca67c7
David Mintz <david@davidmintz.org>
2017-01-09 18:21:14 -0500

    add stub InterpretersControllerTest

---
+++ module/Admin/test/Controller/InterpretersControllerTest.php
@@ -0,0 +1,24 @@
+<?php
+
+/** module/Application/test/Controller/InterpretersControllerTest.php */
+
+namespace ApplicationTest\Controller;
+
+use ApplicationTest\AbstractControllerTest;
+use InterpretersOffice\Admin\Controller\InterpretersController;
+use ApplicationTest\FixtureManager;
+use ApplicationTest\DataFixture;
+use Zend\Stdlib\Parameters;
+use Zend\Dom\Query;
+use InterpretersOffice\Entity;
+
+/**
+ * test interpreters controller.
+ *
+ * 
+ */
+class InterpretersControlllersTest extends AbstractControllerTest
+{
+
+	
+}
