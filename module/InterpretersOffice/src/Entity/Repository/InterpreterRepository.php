5d06dac096978822801155800b8a7ad713bb70cc
David Mintz <david@davidmintz.org>
2017-01-09 22:14:51 -0500

    add Interpreter repository class

---
+++ module/InterpretersOffice/src/Entity/Repository/InterpreterRepository.php
@@ -0,0 +1,32 @@
+<?php 
+/**  module/InterpretersOffice/src/Entity/Repository/InterpreterRepository.php */
+
+
+namespace InterpretersOffice\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+/**
+ * custom repository class for EventType entity.
+ */
+class InterpreterRepository extends EntityRepository
+{
+    
+	use ResultCachingQueryTrait;
+
+    /**
+     * constructor.
+     *
+     * @param EntityManagerInterface              $em
+     * @param \Doctrine\ORM\Mapping\ClassMetadata $class
+     */
+    public function __construct(EntityManagerInterface $em, \Doctrine\ORM\Mapping\ClassMetadata $class)
+    {
+        $em->getConfiguration()->getResultCacheImpl()->setNamespace('interpreters');
+        parent::__construct($em, $class);
+    }
+
+    
+ }
+
