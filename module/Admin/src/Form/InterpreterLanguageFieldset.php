041b6cea36b6aa5c0c62336ad8beaf93c7bc9854
David Mintz <david@davidmintz.org>
2017-01-12 16:48:02 -0500

    add experimental InterpreterLanguage fieldset

---
+++ module/Admin/src/Form/InterpreterLanguageFieldset.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * module/InterpretersOffice/src/Form/InterpreterLanguageFieldset.php
+ */
+
+namespace InterpretersOffice\Admin\Form;
+
+
+use Zend\Form\Fieldset;
+use DoctrineModule\Persistence\ObjectManagerAwareInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+
+use Zend\InputFilter\InputFilterProviderInterface;
+
+use InterpretersOffice\Form\ObjectManagerAwareTrait;
+
+use InterpretersOffice\Entity;
+
+
+/**
+ * Description of InterpreterLanguageFieldset
+ *
+ * @author david
+ */
+class InterpreterLanguageFieldset extends Fieldset implements InputFilterProviderInterface, ObjectManagerAwareInterface
+{
+    
+    
+    use ObjectManagerAwareTrait;
+    
+    public function __construct(ObjectManager $objectManager, $options = []) {
+        
+        parent::__construct('interpreterLanguages', $options);
+        $this->objectManager = $objectManager;
+        $this->setHydrator(new DoctrineHydrator($objectManager));
+        $this->setObject(new Entity\InterpreterLanguage);
+        
+        // try this and see how far we get
+        
+        $this->add([
+             
+            'name' => 'interpreter',
+            'type' => 'hidden',             
+        ]);
+        
+        $this->add([
+            'name' => 'language',
+            'type' => 'DoctrineModule\Form\Element\ObjectSelect',
+            'options' => [
+                'object_manager' => $this->objectManager,
+                'target_class' => 'InterpretersOffice\Entity\Language',
+                'property' => 'name',
+                'label' => 'language',
+                'display_empty_item' => true,
+                'empty_item_label' => '',
+                
+
+            ],
+             'attributes' => [
+                'class' => 'form-control',
+                
+             ],
+            
+        ]);
+        
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return [];
+    }
+    
+    
+}
