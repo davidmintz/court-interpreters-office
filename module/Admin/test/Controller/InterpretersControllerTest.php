929ae7ed7cfbaaaf2b99151cba0c90f12b3be680
David Mintz <david@davidmintz.org>
2017-02-01 16:05:23 -0500

    start writing test assertions for InterpretersController

--- module/Admin/test/Controller/InterpretersControllerTest.php
+++ module/Admin/test/Controller/InterpretersControllerTest.php
@@ -19,6 +19,54 @@ use InterpretersOffice\Entity;
  */
 class InterpretersControllerTest extends AbstractControllerTest
 {
+    public function setUp()
+    {
+        parent::setUp();
+        $fixtureExecutor = FixtureManager::getFixtureExecutor();
+        $fixtureExecutor->execute(
+            [
+            new DataFixture\MinimalUserLoader(),
+            new DataFixture\LanguageLoader(),
+            new DataFixture\InterpreterLoader(),
+            ]
+        );
 
+        $this->login('susie', 'boink');
+    }
+    
+    public function testIndexAction()
+    {
+        $this->dispatch('/admin/interpreters');
+        $this->assertResponseStatusCode(200);
+    }
+    
+    public function testUpdateInterpreter()
+    {
+        // what is the id of an interpreter?
+        $em = FixtureManager::getEntityManager();
+        $interpreter = $em->getRepository('InterpretersOffice\Entity\Interpreter')
+                ->findOneBy(['lastname' => 'Mintz']);
+        // sanity-check
+        $this->assertInstanceOf(Entity\Interpreter::class, $interpreter);
+        
+        $url = '/admin/interpreters/edit/'.$interpreter->getId();
+        
+        $this->dispatch($url);
+        $this->assertQuery('form');
+        $this->assertQuery('#lastname');
+        $this->assertQuery('#firstname');
+        $this->assertQuery('#middlename');
+        $query = new Query($this->getResponse()->getBody());
+        $node1 = $query->execute('#lastname')->current();
+        $lastname = $node1->attributes->getNamedItem('value')->nodeValue;
+        $this->assertEquals('Mintz', $lastname);
+        
+        // add Russian
+        $russian = $interpreter = $em->getRepository('InterpretersOffice\Entity\Language')
+                ->findOneBy(['name' => 'Russian']);
+        $this->assertInstanceOf(Entity\Language::class, $russian);
+        
+        // to be continued
+    }
 
 }
