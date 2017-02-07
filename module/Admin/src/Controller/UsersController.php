9a2cbced4dc899a5865e656962198b4254dc7e80
David Mintz <david@davidmintz.org>
2017-02-07 11:36:40 -0500

    minimal fiddle with UsersController

---
+++ module/Admin/src/Controller/UsersController.php
@@ -0,0 +1,53 @@
+<?php
+
+/** module/Admin/src/Controller/UsersController.php */
+
+namespace InterpretersOffice\Admin\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Doctrine\ORM\EntityManagerInterface;
+use InterpretersOffice\Entity\User;
+
+
+/**
+ * controller for admin/users
+ * 
+ * things we need to do here: 
+ *  
+ *   * supply a way to browse and edit existing users
+ *   * add new user: encourage (require?) looking up existing person first.
+ *     autocompletion. 
+ *   * ACL has to be in play. only admin can elevate manager to admin
+ * 
+ */
+class UsersController extends AbstractActionController
+{
+    /**
+     * entity manager.
+     *
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+	/**
+	 * constructor
+	 */
+	public function __construct(EntityManagerInterface $entityManager)
+	{
+
+		$this->entityManager = $entityManager;
+	}
+
+    /**
+     * index action.
+     *
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        //echo "it works"; return false;
+        return new ViewModel(['title' => 'admin | users']);
+    }
+}
