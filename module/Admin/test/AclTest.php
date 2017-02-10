84783fbda93a7561bafeaee1886e2a5841614ca2
David Mintz <david@davidmintz.org>
2017-02-10 13:34:14 -0500

    add AclTest

---
+++ module/Admin/test/AclTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * module/Application/test/AclTest.php.
+ */
+
+namespace ApplicationTest;
+
+use ApplicationTest\AbstractControllerTest;
+use InterpretersOffice\Admin\Service\Acl;
+
+
+
+
+class AuthenticationTest extends AbstractControllerTest
+{
+    
+    /**
+     *
+     * @var Acl
+     */
+    protected $acl;
+    
+    public function setUp()
+    {
+
+        parent::setUp();
+        $this->acl = $this->getApplicationServiceLocator()->get('acl');
+    }
+    
+    public function testAclIsAvailableFromServiceManager()
+    {
+        $this->assertInstanceOf(Acl::class,$this->acl );
+    }
+    
+    public function testAcl()
+    {
+        
+        
+        $this->assertFalse($this->acl->isAllowed('submitter','events','update'),"submitter should NOT be allowed to edit events");
+        $this->assertFalse($this->acl->isAllowed('submitter','events','boink'),"submitter should NOT be allowed undefined privilege");
+        $this->assertFalse($this->acl->isAllowed('submitter','judges','edit'),"submitters should NOT be allowed to edit judges" );
+        $this->assertTrue($this->acl->isAllowed('submitter','requests','create') ,"submitted SHOULD be allowed to create a request");
+        $this->assertTrue($this->acl->isAllowed('manager','events','edit'),"manager SHOULD be allowed to edit events");
+        $this->assertTrue($this->acl->isAllowed('administrator','events','edit'),"admin SHOULD be allowed to edit events");
+        $this->assertTrue($this->acl->isAllowed('manager','event-types','edit'));
+        // too tedious! try something else....
+        $allow = [
+            ['manager','event-types','edit'],
+            ['manager','users','edit'],
+            ['manager','languages','create'],
+            ['administrator','languages','create'],
+        ];
+        foreach ($allow as $rule) {
+            list($role, $resource, $privilege) = $rule;
+            $this->assertTrue($this->acl->isAllowed($role, $resource, $privilege),"$role SHOULD be allowed to $privilege $resource");
+        }
+        $deny = [
+            
+            ['staff','event-types','edit'],
+            ['staff','languages','add'],
+            ['administrator','requests','create'],
+            
+        ];
+        foreach ($deny as $rule) {
+            list($role, $resource, $privilege) = $rule;
+            $this->assertFalse($this->acl->isAllowed($role, $resource, $privilege),"$role SHOULD NOT be allowed to $privilege $resource");
+        }
+        
+    }
+    
+}
