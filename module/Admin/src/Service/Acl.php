6a214ecfe4b13a8cb66f902b695e8f777b4650dd
David Mintz <david@davidmintz.org>
2017-02-10 10:47:03 -0500

    process deny rules in Service/Acl.php

--- module/Admin/src/Service/Acl.php
+++ module/Admin/src/Service/Acl.php
@@ -70,58 +70,18 @@ class Acl extends ZendAcl {
                // ? $privileges : implode(",",$privileges));
                $this->allow($role,$resource,$privileges);               
            }            
-        }        
+        }
+        foreach($this->config['deny'] as $role => $rules ) {
+           if (null === $rules) {
+               $this->deny($role);
+               continue;
+           }
+           foreach($rules as $resource => $privileges) {
+               //printf ("we are setting allow on role %s, resource %s, privs %s<br>",$role,$resource, is_scalar($privileges)
+               // ? $privileges : implode(",",$privileges));
+               $this->deny($role,$resource,$privileges);               
+           }            
+        } 
     }
-    /**
-     * dumps some rules, for debugging
-     * @todo move to unit test etc
-     */
-    public function testStuff()
-    {
-        //printf('<pre>%s</pre>',print_r($this->config,true));
-        //return;
-        echo '<pre>';
-        echo "submitter update events? ";
-        echo $this->isAllowed('submitter','events','update') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "submitter boink events? ";
-        echo $this->isAllowed('submitter','events','boink') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "submitter edit judges? ";
-        echo $this->isAllowed('submitter','judges','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "submitter create requests? ";
-        echo $this->isAllowed('submitter','requests','create') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "admin create requests? ";
-        echo $this->isAllowed('administrator','requests','create') ? "allowed" : "denied"; echo "<br>";
-        echo "manager update events? ";
-        echo $this->isAllowed('manager','events','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "admin update events? ";
-        echo $this->isAllowed('administrator','events','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "manager update event-types? ";
-        echo $this->isAllowed('manager','event-types','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "manager update judges? ";
-        echo $this->isAllowed('manager','event-types','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "manager edit users? ";
-        echo $this->isAllowed('manager','users','edit') ? "allowed" : "denied"; echo "<br>";
-        
-        echo "manager create languages? ";
-        echo $this->isAllowed('manager','languages','create') ? "allowed" : "denied"; echo "<br>";
-         $this->allow('manager','languages');
-        
-        echo "admin create languages? ";
-        echo $this->isAllowed('administrator','languages','create') ? "allowed" : "denied"; echo "<br>";
-        
-       echo "staff edit languages? ";
-       echo $this->isAllowed('staff','languages','add') ? "allowed" : "denied"; echo "<br>";
-        
-        echo '</pre>';
-        
-        
-    }
+    
 }
