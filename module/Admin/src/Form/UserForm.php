2fb04638f8a7f66e78a3cfbbc9a6f9e4df9ba9bb
David Mintz <david@davidmintz.org>
2017-02-13 17:28:40 -0500

    add UserForm class

---
+++ module/Admin/src/Form/UserForm.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * module/Admin/src/Form/JudgeForm.php.
+ */
+
+namespace InterpretersOffice\Admin\Form;
+
+use Zend\Form\Form;
+use InterpretersOffice\Form\CsrfElementCreationTrait;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use InterpretersOffice\Admin\Form\UserFieldset;
+
+/**
+ * UserForm intended for administrative use
+ *
+ * @author david
+ */
+class UserForm extends Form {
+    
+    use CsrfElementCreationTrait;
+
+    /**
+     * name of the form.
+     *
+     * @var string
+     */
+    protected $formName = 'user-form';
+    
+    public function __construct(ObjectManager $objectManager, $options = []) {
+        
+        parent::__construct($this->form_name, $options);
+        $this->add(new UserFieldset($objectManager,$options));
+        $this->addCsrfElement();
+    }
+
+    
+}
