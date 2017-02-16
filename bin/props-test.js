ff0f6eff9027b91210472803eb503af3ea2786d7
David Mintz <david@davidmintz.org>
2017-02-16 17:45:08 -0500

    add node.js scribble

---
+++ bin/props-test.js
@@ -0,0 +1,15 @@
+var props = require("properties");
+var options = {
+  path: true,
+  sections: true,
+  comments: ";", //Some INI files also consider # as a comment, if so, add it, comments: [";", "#"]
+  separators: "=",
+  strict: true
+};
+props.parse('/home/david/.my.cnf',
+	options,
+	function(error,obj) {
+		if (error) { console.error(error); } 
+		console.log(obj.client);  
+	}	
+);
