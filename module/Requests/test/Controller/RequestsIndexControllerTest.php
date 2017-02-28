f3c8defef056b428a703daadcb6876d25cec474b
David Mintz <david@davidmintz.org>
2017-02-28 10:50:27 -0500

    add skeletal RequestIndexController unit test

---
+++ module/Requests/test/Controller/RequestsIndexControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * module/Requests/test/Controller/RequestsIndexControllerTest.php
+ * 
+ */
+
+namespace ApplicationTest\Controller;
+
+use InterpretersOffice\Requests\Controller\RequestsIndexController;
+use ApplicationTest\AbstractControllerTest;
+/**
+ * unit test for InterpretersOffice\Requests module's main controller
+ */
+class RequestsIndexControllerTest extends AbstractControllerTest {
+   
+    public function testIndexCannotBeAccessedWithoutLogin()
+    {
+        $this->dispatch('/requests');
+        $this->assertRedirect();
+    }
+}
