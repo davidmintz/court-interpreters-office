296a95fb0cde57e2cdd8b34045ff9af8b0fed92d
David Mintz <david@davidmintz.org>
2017-03-01 14:46:32 -0500

    add LanguageSelect element

---
+++ module/InterpretersOffice/src/Form/Element/LanguageSelect.php
@@ -0,0 +1,55 @@
+<?php
+/** module/InterpretersOffice/src/Form/Element/LanguageSelect.php */
+
+namespace InterpretersOffice\Form\Element;
+
+use DoctrineModule\Form\Element\ObjectSelect;
+
+
+/**
+ * specialized form element for selecting a language
+ */
+class LanguageSelect extends ObjectSelect
+{    
+    /**
+     *
+     * @var array
+     */
+    protected $defaultOptions = [
+        'name' => 'language-select',
+        'options' => [
+            'target_class' => 'InterpretersOffice\Entity\Language',
+            'property' => 'name',         
+            'label' => 'languages',            
+        ],
+        'attributes' => [
+            'class' => 'form-control',
+            'id' => 'language-select',
+        ],        
+    ];
+    
+    /**
+     * constructor
+     * 
+     * @param string $name element name
+     * @param array $options
+     */
+    public function __construct($name = null, Array $options ) {
+        
+        $this->getProxy()->setObjectManager($options['objectManager']);
+        $this->getProxy()->setTargetClass('InterpretersOffice\Entity\Language');
+        parent::__construct($name, $options);
+        $attributes = $this->defaultOptions['attributes'];
+        if (key_exists('attributes',$options)) {
+            $attributes = array_merge($attributes,$options['attributes']);
+        }
+        $this->setAttributes($attributes);        
+        $select_options = $this->getValueOptions();
+        array_unshift($select_options, [
+            'label' => '-- select a language --',
+            'value' => '',
+            'attributes' => ['label' => ' '],
+        ]);
+        $this->setValueOptions($select_options);
+    }    
+}
