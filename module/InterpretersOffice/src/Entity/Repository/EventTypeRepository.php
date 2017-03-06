c231eb053ac8ea62d8612ccaf50db2919293b9c3
David Mintz <david@davidmintz.org>
2017-03-06 14:43:31 -0500

    EventTypeRepository: use a cache_id in findAll()

--- module/InterpretersOffice/src/Entity/Repository/EventTypeRepository.php
+++ module/InterpretersOffice/src/Entity/Repository/EventTypeRepository.php
@@ -46,6 +46,6 @@ class EventTypeRepository extends EntityRepository
     {
         $dql = 'SELECT t FROM InterpretersOffice\Entity\EventType t ORDER BY t.name';
 
-        return $this->createQuery($dql)->getResult();
+        return $this->createQuery($dql,0,'event-types-all')->getResult();
     }
 }
