c079bb5391cd0220cf730f9cb6b6df02ad4f4539
David Mintz <david@davidmintz.org>
2017-03-10 16:36:36 -0500

    judge-scraper: fix iteration/order of execution nightmare

--- bin/scrape-judges.casper.js
+++ bin/scrape-judges.casper.js
@@ -1,66 +1,55 @@
 /**
  * 
- * screen-scraper for CasperJs to get judge courtrooms
- * invoke as casperjs <scriptname>.js
+ * screen-scraper for CasperJs to get SDNY judge courtrooms
+ * Usage: casperjs <scriptname>.js
  * 
  */
 
-var links;
 var casper = require('casper').create({
-    verbose:true
-    //clientScripts: ["../public/js/lib/jquery.min.js"]
+    verbose:true //, clientScripts: ["../public/js/lib/jquery.min.js"]
 });
-//require('fs');
-//var fs = require('fs');
-var fileUrl = "file:///opt/www/court-interpreters-office/public/judges.html";
-var url = "http://nysd.uscourts.gov/judges/District";
-var links;
 
-casper.start(url, function() {
+// var fs = require('fs');
+// var fileUrl = "file:///opt/www/court-interpreters-office/public/judges.html";
+
+var baseUrl = "http://nysd.uscourts.gov";
+
+casper.start(baseUrl+ '/judges/District', function() {
   
    this.waitForSelector('table.judge_info'); 
    // get an object in the form { judge_name : url, ... }
    links = this.evaluate(function(){       
        var links = jQuery("table.judge_info tbody tr td a");
+       var baseUrl = "http://nysd.uscourts.gov/";
        $return = {};
        links.each(function(i,e){
-           $return[e.textContent.trim()] = e.getAttribute('href');
+           $return[e.textContent.trim()] = baseUrl+e.getAttribute('href');
        });
        return $return;       
-   });   
-   this.echo("leaving 1st function. return is "+typeof links);
+   });
 });
 
-casper.then(function(){
-    
-    // this.echo("this is your second function, links is a "+typeof links);
-    // var json = JSON.stringify(links,null,5);
-    var count = 0;
-    for (item in links) {
-        count++;
-        var url = "http://nysd.uscourts.gov/"+links[item];        
-         if (count > 1) {
-            this.echo("debug: stopping iteration");
-            break;
-        }
-        this.echo(item +": url: http://nysd.uscourts.gov/"+links[item]);
-        casper.open(url).then(function(){
-            // parse out the text from the first two td > p elements in the table
-            data = this.evaluate(function(){
-                var text = '';
-                var elements = jQuery("table#text.main tbody tr.whiteback td table tbody tr td p:lt(2)");
-                elements.each(function(i,e){
-                    text += e.innerHTML.replace(/<br *\/?>/g,"\n").trim();
-                    text += "\n";
-                    }
-                )
-                return text;
-            });            
-            this.echo(data);
-        });
-    }
-    
-    
+casper.then(function(){   
+    for(var judge in links) {
+       var url = links[judge];
+       // and now, use a IIFE and tear your hair out no more! 
+       // http://stackoverflow.com/questions/24360993/casperjs-iterating-through-urls
+       // https://groups.google.com/forum/#!topic/casperjs/n_zXlxiPMtk
+       (function(url,name){
+           casper.thenOpen(url,function(){
+               this.echo(name+ ":  "+this.getCurrentUrl());
+               data = this.evaluate(function() {
+                    var text = '';
+                    var elements = jQuery("table#text.main tbody tr.whiteback td table tbody tr td p:lt(2)");
+                    elements.each(function (i, e) {
+                        text += e.innerHTML.replace(/\s*<br *\/?>\s*/g, "\n").trim();
+                        text += "\n";
+                    });
+                    return text;
+                });
+               this.echo(data);
+           });})
+       (url,judge);       
+    }  
 });
-
 casper.run();
