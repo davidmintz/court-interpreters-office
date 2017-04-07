1a8b1ab7cc483c2947366928ea068c5302b50c0a
david-chromebook <david@davidmintz.org>
2017-04-06 20:24:35 -0400

    add more fields to Interpreter entity

--- module/InterpretersOffice/src/Entity/Interpreter.php
+++ module/InterpretersOffice/src/Entity/Interpreter.php
@@ -37,13 +37,45 @@ class Interpreter extends Person
     protected $phone;
 
     /**
-     * date of birth.
+     * date of birth. 
+     *
+     * string rather than date because it will be encrypted
      *
      * @ORM\Column(type="string",length=125,nullable=true)
      *
      * @var string
      */
     protected $dob;
+
+
+    /**
+     * 
+     * @ORM\Column(type="string",length=255,nullable=true)
+     */
+    protected $ssn;
+
+    /**
+     * date the security clearance expires.
+     *
+     * @ORM\Column(type="date",name="security_expiration_date",nullable=true)
+     */
+    protected $securityExpirationDate;
+
+     /**
+     * date fingerprints taken.
+     *
+     * @ORM\Column(type="date",name="fingerprint_date",nullable=true)
+     */
+    protected $fingerprintDate;
+
+    /**
+     * date oath taken.
+     *
+     * @ORM\Column(type="date",name="oath_date",nullable=true)
+     */
+    protected $oathDate;
+
+
 
     /**
      * working languages.
@@ -54,11 +86,6 @@ class Interpreter extends Person
      */
     protected $interpreterLanguages;
 
-    /**
-     * 
-     * @ORM\Column(type="string",length=255,nullable=true)
-     */
-    protected $ssn;
 
 
 
@@ -251,4 +278,78 @@ class Interpreter extends Person
             $this->interpreterLanguages->removeElement($interpreterLanguage);
         }
     }
+
+
+    /**
+     * Set securityExpirationDate
+     *
+     * @param \DateTime $securityExpirationDate
+     *
+     * @return Interpreter
+     */
+    public function setSecurityExpirationDate($securityExpirationDate)
+    {
+        $this->securityExpirationDate = $securityExpirationDate;
+
+        return $this;
+    }
+
+    /**
+     * Get securityExpirationDate
+     *
+     * @return \DateTime
+     */
+    public function getSecurityExpirationDate()
+    {
+        return $this->securityExpirationDate;
+    }
+
+    /**
+     * Set fingerprintDate
+     *
+     * @param \DateTime $fingerprintDate
+     *
+     * @return Interpreter
+     */
+    public function setFingerprintDate($fingerprintDate)
+    {
+        $this->fingerprintDate = $fingerprintDate;
+
+        return $this;
+    }
+
+    /**
+     * Get fingerprintDate
+     *
+     * @return \DateTime
+     */
+    public function getFingerprintDate()
+    {
+        return $this->fingerprintDate;
+    }
+
+    /**
+     * Set oathDate
+     *
+     * @param \DateTime $oathDate
+     *
+     * @return Interpreter
+     */
+    public function setOathDate($oathDate)
+    {
+        $this->oathDate = $oathDate;
+
+        return $this;
+    }
+
+    /**
+     * Get oathDate
+     *
+     * @return \DateTime
+     */
+    public function getOathDate()
+    {
+        return $this->oathDate;
+    }
+    
 }
