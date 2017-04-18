23a4e55163c4b0ebb184dfcbe2aca82e0fc6e6af
David Mintz <david@davidmintz.org>
2017-04-18 14:51:24 -0400

    add "abstract" keyword to AbstractControllerTest and make setUp() return $this

--- module/InterpretersOffice/test/AbstractControllerTest.php
+++ module/InterpretersOffice/test/AbstractControllerTest.php
@@ -14,7 +14,7 @@ use Zend\Dom\Query;
 /**
  * base class for unit tests.
  */
-class AbstractControllerTest extends AbstractHttpControllerTestCase
+abstract class AbstractControllerTest extends AbstractHttpControllerTestCase
 {
     public function setUp()
     {
@@ -34,6 +34,7 @@ class AbstractControllerTest extends AbstractHttpControllerTestCase
         ));
 
         parent::setUp();
+        return $this;
     }
 
     /**
