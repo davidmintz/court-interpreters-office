ec9dd176ccd462c05d2b9e78e7c3b0104f32f138
David Mintz <david@davidmintz.org>
2017-04-20 16:28:37 -0400

    add a couple more methods to Vault client

--- module/Vault/src/Service/Vault.php
+++ module/Vault/src/Service/Vault.php
@@ -34,6 +34,13 @@ class Vault {
     protected $prefix = '/v1';
     
     /**
+     * additional configuration options
+     * 
+     * @var Array 
+     */
+    protected $config;
+    
+    /**
      * constructor
      * 
      * @param array $config
@@ -42,11 +49,14 @@ class Vault {
         
         $this->vault_address = $config['vault_address'] . $this->prefix;
         unset($config['vault_address']);
+        $this->config = $config;
         // hand off the rest of the config to Zend\Http\Client
         $this->client =  new Client(null,$config);        
         $this->client->getRequest()
             ->getHeaders()
-            ->addHeaderLine('Accept: application/json');        
+            ->addHeaderLine('Accept: application/json');
+        
+        
     }
     
     /**
@@ -59,12 +69,24 @@ class Vault {
         return $this->vault_address;
     }
     
+    /**
+     * gets client
+     * 
+     * returns our configured client so that the caller
+     * can do whatever with it
+     * @return \Zend\Http\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
     
     /**
-     * attempts to authenticate a user
+     * attempts user/password authentication
      * 
-     * this will attempt to  authenticate user against Vault's
+     * this will attempt to authenticate user against Vault's
      * userpass auth backend
+     * @link https://www.vaultproject.io/docs/auth/userpass.html
      * 
      * @param string $user
      * @param string $password
@@ -78,9 +100,40 @@ class Vault {
                 ->setUri($uri)                
                 ->setMethod('POST')
                 ->send();
-        $json = $this->client->getResponse()->getBody();
-        return $json;
+        
+        return $this->client->getResponse()->getBody();
         
     }
     
+    /**
+     * attempts Vault TSL authentication
+     * 
+     * this will attempt to authenticate using TLS certificates
+     * @link https://www.vaultproject.io/docs/auth/cert.html
+     */
+    public function authenticateTLSCert($options = [])
+    {
+        $adapter = $this->client->getAdapter()->setOptions($this->config['curl_options']);
+        
+        $this->client->setMethod('POST')
+            ->setUri($this->vault_address .'/auth/cert/login')
+            ->send();
+        
+        return $this->client->getResponse()->getBody();        
+    }
+    
+    /**
+     * sets Vault authentication token header
+     * 
+     * @param string $token
+     * @return \SDNY\Service\Vault
+     */
+    public function setAuthToken($token)
+    {
+        $this->client->getRequest()
+            ->getHeaders()
+            ->addHeaderLine("X-Vault-Token:$token");
+        
+        return $this;
+    }
 }
