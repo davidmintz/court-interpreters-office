f8ecacc7211cdce087d751423b6862a9407d421e
David Mintz <david@davidmintz.org>
2017-04-25 17:10:25 -0400

    add getCipherAccessToken() to Vault service class

--- module/Vault/src/Service/Vault.php
+++ module/Vault/src/Service/Vault.php
@@ -6,10 +6,12 @@
 namespace SDNY\Vault\Service;
 use Zend\Http\Client;
 
+
 /**
  * A stab at a basic Vault client
  * 
  * @todo consider:  maybe this should simply extend Zend\Http\Client
+ * instead of wrapping one?
  */
 
 class Vault {
@@ -36,13 +38,6 @@ class Vault {
     protected $prefix = '/v1';
     
     /**
-     * additional configuration options
-     * 
-     * @var Array 
-     */
-    protected $config;
-    
-    /**
      * constructor
      * 
      * @param array $config
@@ -51,14 +46,11 @@ class Vault {
         
         $this->vault_address = $config['vault_address'] . $this->prefix;
         unset($config['vault_address']);
-        $this->config = $config;
         // hand off the rest of the config to Zend\Http\Client
         $this->client =  new Client(null,$config);        
         $this->client->getRequest()
             ->getHeaders()
             ->addHeaderLine('Accept: application/json');
-        
-        
     }
     
     /**
@@ -87,7 +79,9 @@ class Vault {
      * attempts user/password authentication
      * 
      * this will attempt to authenticate user against Vault's
-     * userpass auth backend
+     * userpass auth backend.
+     * NOTE: looks like we won't be using
+     * this after all, so this method can disappear
      * @link https://www.vaultproject.io/docs/auth/userpass.html
      * 
      * @param string $user
@@ -115,8 +109,6 @@ class Vault {
      */
     public function authenticateTLSCert($options = [])
     {
-        $adapter = $this->client->getAdapter()->setOptions($this->config['curl_options']);
-        
         $this->client->setMethod('POST')
             ->setUri($this->vault_address .'/auth/cert/login')
             ->send();
@@ -137,5 +129,32 @@ class Vault {
             ->addHeaderLine("X-Vault-Token:$token");
         
         return $this;
+    }
+    
+    /**
+     * attempts to acquire and return Vault access token that is
+     * authorized to read the cipher we use for symmetrical encryption/decryption
+     * of sensitive Interpreter data.
+     * 
+     * @param string $auth_token 
+     * @return \SDNY\Service\Vault
+     */
+    public function getCipherAccessToken($auth_token = null)
+    {
+        if ($auth_token) {
+            $this->client->getRequest()
+            ->getHeaders()
+            ->addHeaderLine("X-Vault-Token:$auth_token");
+        }
+        $endpoint = $this->vault_address . '/auth/token/create/read-cipher';
+        $this->client->getRequest()->setContent(json_encode(
+               [
+                'ttl' => '5m',
+                'num_uses' => 3,
+               ]
+        ));
+        $this->client->setMethod('POST')->setUri($endpoint)->send();
+        
+        return $this->client->getResponse()->getBody();           
     }
 }
