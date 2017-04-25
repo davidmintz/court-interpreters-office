08488ba62c8a8e68e81587d5d89341400235dc97
David Mintz <david@davidmintz.org>
2017-04-25 17:11:29 -0400

    tweak Vault module config

--- module/Vault/config/module.config.php
+++ module/Vault/config/module.config.php
@@ -6,12 +6,13 @@ return [
 // override this with your local configuration
     
     'vault' => [    
-        'vault_address' => 'https://vault.sdnyinterpreters.org:8200', 
-        'sslcafile'     => '/usr/share/ca-certificates/ca-chain.cert.pem',
         // do not change this adapter
         'adapter'       => 'Zend\Http\Client\Adapter\Curl',
-        // do override these
-        'curl_options' => [            
+        // do override these with a local config
+        'vault_address' => 'https://vault.sdnyinterpreters.org:8200', 
+        'sslcafile'     => '/usr/share/ca-certificates/ca-chain.cert.pem',
+        // these need to match the configuration set in Vault
+        'curloptions' => [            
             \CURLOPT_SSLKEY => '/opt/ssl/vault/int807a.nysd.key.pem',
             \CURLOPT_SSLCERT => '/opt/ssl/vault/usr.int807a.cert.pem',            
         ],
