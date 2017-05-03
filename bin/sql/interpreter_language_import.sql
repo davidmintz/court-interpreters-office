4a771f0b3e40317ff5e397bc137413abd6d5907a
David Mintz <david@davidmintz.org>
2017-05-03 14:37:44 -0400

    work on interpreter import SQL script

--- bin/sql/interpreter_language_import.sql
+++ bin/sql/interpreter_language_import.sql
@@ -3,11 +3,15 @@ SQL for importing interpreters, languages and interpreters_languages
 */
 
 /* FIRST purge interpreters_languages, interpreters, people and users */
-
+SET NAMES 'utf8';
 DELETE FROM interpreters_languages;
 DELETE FROM languages;
 DELETE FROM users;
-DELETE i, p FROM interpreters i JOIN people p ON i.id = p.id ;
+DELETE FROM interpreters;
+
+DELETE FROM people WHERE hat_id IN (1,3);
+
+/* DELETE i, p FROM interpreters i JOIN people p ON i.id = p.id ;*/
 
 /* INSERT INTO people table */
 INSERT INTO people 
@@ -20,14 +24,17 @@ INSERT INTO people
     "interpreter"
 FROM dev_interpreters.interpreters ORDER BY interp_id);
 /* followed by interpreters table */
+
+
 INSERT INTO interpreters (
 id, phone, security_clearance_date, contract_expiration_date,fingerprint_date, oath_date
 )
 (SELECT interp_id, home, 
     IF (security_clearance = "0000-00-00",NULL,security_clearance), 
     IF (contract_expiration = "0000-00-00",NULL,contract_expiration),
-    fingerprinted, 
-    oath  
+    IF (fingerprinted = "0000-00-00",NULL,fingerprinted),
+    IF (oath = "0000-00-00",NULL,oath)
+    
 FROM dev_interpreters.interpreters ORDER BY interp_id);
 
 INSERT INTO languages (SELECT * FROM dev_interpreters.languages);
