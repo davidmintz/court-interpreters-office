98ff0340d8850c7bf7fd2ea79797661154cfafe4
David Mintz <david@davidmintz.org>
2017-05-23 17:18:26 -0400

    comments in vault monitor script

--- bin/vault-monitor.sh
+++ bin/vault-monitor.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# early sketch of a vault audit-log monitoring tool
+
 LOG=/var/log/vault_audit.log
 echo [$(date )] Begin monitoring $LOG ...
 while inotifywait -q -e modify $LOG; do
