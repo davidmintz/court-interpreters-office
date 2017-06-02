3170be301644680d5443cad7809dd5629df0bea8
David Mintz <david@davidmintz.org>
2017-06-02 17:03:31 -0400

    comments etc in InterpreterEntityListener

--- module/InterpretersOffice/src/Entity/Listener/InterpreterEntityListener.php
+++ module/InterpretersOffice/src/Entity/Listener/InterpreterEntityListener.php
@@ -19,15 +19,14 @@ class InterpreterEntityListener implements EventManagerAwareInterface
     
     use EventManagerAwareTrait;
     
-    //private $ssn_obscured = '*********';//
-    //private $dob_obscured = '**/**/****//';
     
     /**
      * callback 
      * 
      * runs when Interpreter entity is loaded
      * 
-     * this is experimental. there might be a better approach.
+     * doesn't do anything at the moment, except trigger another event 
+     * which in turn doesn't do anything
      * 
      * @param Interpreter $interpreter
      * @param LifecycleEventArgs $event
@@ -35,16 +34,7 @@ class InterpreterEntityListener implements EventManagerAwareInterface
     public function postLoad(Interpreter $interpreter, LifecycleEventArgs $event)
     {        
 
-        if ($interpreter->getSsn()) {
-           // $interpreter->setSsn($this->ssn_obscured);
-        }
-        if ($interpreter->getDob()) {
-           // $interpreter->setDob($this->dob_obscured);
-        }
-        //if ($this->events) {
-            // to do: make this work even in test environment
         $this->events->trigger(__FUNCTION__, $this);
-        //}
-        //printf("\nshit is STILL running in %s! yay!",__METHOD__);
+
     }
 }
