c108d2c20d8bac0e4d418d340ec954df08c6b584
David Mintz <david@davidmintz.org>
2017-06-26 15:52:51 -0400

    InterpreterEntityListener update callbacks that do encryption

--- module/InterpretersOffice/src/Entity/Listener/InterpreterEntityListener.php
+++ module/InterpretersOffice/src/Entity/Listener/InterpreterEntityListener.php
@@ -73,13 +73,10 @@ class InterpreterEntityListener implements EventManagerAwareInterface, LoggerAwa
     public function postLoad(Interpreter $interpreter, LifecycleEventArgs $event)
     {        
 
-        $this->encrypted_values = [
-            'dob' => $interpreter->getDob(),
-            'ssn' => $interpreter->getSsn(),
-        ];
+       
         $this->getEventManager()->trigger(__FUNCTION__, $this);
         //var_dump(is_null($this->log));
-        $this->log->debug("this is ".__FUNCTION__ . " in your InterpreterEntityListener ...");
+        $this->log->debug("running ".__FUNCTION__ . " in your InterpreterEntityListener ...");
         //echo "Interpreter entity listener WTF? hello what the FUCKING FUCK??";
 
     }
@@ -87,11 +84,8 @@ class InterpreterEntityListener implements EventManagerAwareInterface, LoggerAwa
     /**
      * callback 
      * 
-     * runs when Interpreter entity is loaded
-     * 
-     * doesn't do anything at the moment, except trigger another event 
-     * which in turn doesn't do anything
-     * 
+     * runs when Interpreter entity about to be updated
+     *     
      * @param Interpreter $interpreter
      * @param PreUpdateEventArgs $event
      */
@@ -99,42 +93,47 @@ class InterpreterEntityListener implements EventManagerAwareInterface, LoggerAwa
     {        
         $this->getEventManager()->trigger(__FUNCTION__, $this);
         if (! $this->vault) { return; }
-        $this->log->debug(sprintf('getSsn() now returns %s', $interpreter->getSsn()));
+        //$this->log->debug(sprintf('getSsn() now returns %s', $interpreter->getSsn()));
         $this->log->debug(sprintf(
             'hasChangedField? %s', $event->hasChangedField('ssn') ? "yes":"no"
         ));
-        if ($event->hasChangedField('ssn')) {
-            $old_value = $event->getOldValue('ssn');
-            $new_value = $event->getNewValue('ssn');
-            // if there is NO old value, but there IS a new value, just encrypt it
-            if ($new_value && ! $old_value ) {
-                $this->log->debug("updating from null ssn to not-null?");
-                $interpreter->setSsn($this->vault->encrypt($new_value));
-            } elseif ($old_value && ! $new_value) {
-                // pass
-                $this->log->debug("updating from  not-null to null?");
-            } else {
-                // compare old value decrypted with new
-                $this->log->debug("comparing old-decrypted to new");
-                $decrypted_old_value = $this->vault->decrypt($old_value);
-                if ($decrypted_old_value != $new_value) {
-                    // it really changed. encrypt
-                    $this->log->debug("...it really was updated");
-                    $interpreter->setSsn($this->vault->encrypt($new_value));
+        foreach (['dob','ssn'] as $prop) {
+            if ($event->hasChangedField($prop)) {
+                $old_value = $event->getOldValue($prop);
+                $new_value = $event->getNewValue($prop);
+                $setter = 'set'.lcfirst($prop);
+                // if there is NO old value, but there IS a new value, just encrypt it
+                if ($new_value && ! $old_value ) {
+                    $this->log->debug("updating from null ssn to not-null?");
+                    $setter = 'set'.lcfirst($prop);
+                    $interpreter->$setter($this->vault->encrypt($new_value));
+                } elseif ($old_value && ! $new_value) {
+                    // pass
+                    $this->log->debug("updating from  not-null to null?");
                 } else {
-                    // not really modified.
-                    $this->log->debug("NOT really updated, attempting to unset from changeset");
-                    $changeset = $event->getEntityChangeSet();
-                    unset($changeset['ssn']);
+                    // compare old value decrypted with new
+                    $this->log->debug("comparing old-decrypted to new");
+                    $decrypted_old_value = $this->vault->decrypt($old_value);
+                    if ($decrypted_old_value != $new_value) {
+                        // it really changed. encrypt
+                        $this->log->debug("...and it really was updated");
+                        $interpreter->$setter($this->vault->encrypt($new_value));
+                    } else {
+                        // not really modified.
+                        $this->log->debug("NOT really updated, attempting to unset from changeset");
+                        $changeset = $event->getEntityChangeSet();
+                        unset($changeset[$prop]);
+                    }
                 }
-            }
-        }
+            }            
+        }        
     }
     
     public function prePersist(Interpreter $interpreter, LifecycleEventArgs $event) {
         
         $this->log->debug("this is ".__FUNCTION__);
-        if (! $this->vault) { return; }
+        // @todo throw Exception if they try to save w/o encryption??
+        if (! $this->vault) { $this->log->debug("no vault enabled, returning");return; }
         foreach (['dob','ssn'] as $field) {
             $getter = 'get'.lcfirst($field);
             $value = $interpreter->$getter();
