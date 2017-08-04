61f28a3e4b619971db5c2b1da4252b8fb7e4780d
David Mintz <david@davidmintz.org>
2017-08-04 16:50:06 -0400

    work on navigation config, layout

--- config/autoload/navigation.global.php
+++ config/autoload/navigation.global.php
@@ -1,34 +1,96 @@
-<?php
-
+<?php /** config/autoload/navigation.global.php */
 return [
 
     'navigation' => [
         'default' => [
-            [
+              [
+                  'label'=>'schedule',
+                  'route' => 'events',
+              ],
+              [
+                  'label'=>'interpreters',
+                  'route' => 'interpreters',
+              ],
+              [                
                 'label' => 'admin',
                 'route' => 'admin',
                 'pages' => [
                     [
-                    'label' => 'languages',
-                    'route' => 'languages',
-                    /*
-                    'pages' => [
-                        [
-                            'label' => 'add new',
-                            'route' => 'languages/add'
-                        ]
-                    ]
-                     
-                     */
+                        'label' => 'main',
+                        'route' => 'admin',
                     ],
                     [
-                    'label' => 'locations',
-                    'route' => 'locations',
+                        'label' => 'languages',
+                        'route' => 'languages',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'languages/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'languages/edit',
+                            ]
+                        ]
+                    ],
+                    [
+                        'label' => 'locations',
+                        'route' => 'locations',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'locations/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'locations/edit'
+                            ],
+                        ]
+                    ],
+                    [
+                        'label' => 'judges',
+                        'route' => 'judges',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'judges/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'judges/edit'
+                            ],
+                        ] 
+                    ],
+                    [
+                        'label' => 'users',
+                        'route' => 'users',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'users/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'users/edit'
+                            ],
+                        ]
+                    ],
+                    [
+                        'label' => 'event-types',
+                        'route' => 'event-types',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'event-types/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'event-types/edit'
+                            ],
+                        ]
                     ],
                 ]
             ],
-            
-            
         ],
     ],
     'service_manager' => [
