b3c45bc7a522438445bed3443dbea2ca1d8be3e7
David Mintz <david@davidmintz.org>
2017-08-10 11:39:52 -0400

    EventsController constructor takes AuthenticationServiceInterface as second parameter

--- module/Admin/src/Controller/EventsController.php
+++ module/Admin/src/Controller/EventsController.php
@@ -9,6 +9,7 @@ use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Zend\Authentication\AuthenticationServiceInterface;
 
 use InterpretersOffice\Admin\Form;
 
@@ -17,15 +18,30 @@ use InterpretersOffice\Admin\Form;
  */
 class EventsController extends AbstractActionController
 {
-
+    
+    /**
+     * entity manager
+     * 
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+    
+    /**
+     * authentication service
+     * 
+     * @var AuthenticationServiceInterface 
+     */
+    protected $auth;
+    
     /**
      * constructor
      *
      * @param EntityManagerInterface $em
      */
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, AuthenticationServiceInterface $auth)
     {
         $this->entityManager = $em;
+        $this->auth = $auth;
     }
     /**
      * index action
@@ -45,7 +61,9 @@ class EventsController extends AbstractActionController
     {
         $form = new Form\EventForm(
             $this->entityManager,
-            ['action' => 'create']
+            [  'action' => 'create',
+                'auth_user_role'=> $this->auth->getIdentity()->role
+            ]
         );
         $request = $this->getRequest();
         $form->setAttribute('action', $request->getRequestUri());
