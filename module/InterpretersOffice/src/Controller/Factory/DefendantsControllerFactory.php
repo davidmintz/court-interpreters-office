70b604707e7e372543b96d5814aea00b79d11fe4
David Mintz <david@davidmintz.org>
2017-09-29 13:37:51 -0400

    add Factory for DefendantsController

---
+++ module/InterpretersOffice/src/Controller/Factory/DefendantsControllerFactory.php
@@ -0,0 +1,24 @@
+<?php
+/** module/InterpretersOffice/src/Controller/Factory/DefendantsControllerFactory.php */
+
+namespace InterpretersOffice\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use InterpretersOffice\Controller\DefendantsController;
+
+class DefendantsControllerFactory implements FactoryInterface
+{
+    /**
+     * yadda yadda
+     * 
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
+     * @return DefendantsController
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new DefendantsController($container->get(\Doctrine\ORM\EntityManager::class));
+    }
+}
