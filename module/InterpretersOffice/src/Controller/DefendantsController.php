a44ed52a190042940c4d294534a6f4b75a0173ce
David Mintz <david@davidmintz.org>
2017-09-29 13:46:26 -0400

    created DefendantsController for autocompletion of names

---
+++ module/InterpretersOffice/src/Controller/DefendantsController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * module/InterpretersOffice/src/Controller/DefendantsController.php
+ */
+
+namespace InterpretersOffice\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;       
+use Zend\View\Model\JsonModel;
+use Doctrine\ORM\EntityManager;
+
+use InterpretersOffice\Entity;
+
+/**
+ *
+ * for fetching defendant data for autocompletion, etc
+ */
+class DefendantsController extends AbstractActionController {
+    
+    /**
+     * entity manager
+     * 
+     * @var EntityManager
+     */
+    protected $entityManager;
+    
+    /**
+     * constructor
+     * 
+     * @param EntityManager $em
+     */
+    public function __construct(EntityManager $em) {
+        $this->entityManager = $em;
+    }
+    
+    /**
+     * don't really need this. it can be removed
+     * 
+     * @return \InterpretersOffice\Controller\ViewModel
+     */
+    public function indexAction()
+    {        
+        return $this->getResponse()
+            ->setContent("LocationsController/indexAction works");
+    }
+    
+   /**
+     * autocompletion for the defendant-name search box in 
+     * the interpreter-request form
+     */
+    public function autocompleteAction()
+    {
+        $repo = $this->entityManager->getRepository(Entity\DefendantName::class);
+        
+        
+        return $this->getResponse()
+            ->setContent("LocationsController/autocomplete works, repo is a ".get_class($repo));
+    	$term = $this->getRequest()->getQuery('term');
+    	if (! $term) { return false; }
+
+    	/**
+    	 * @var $em Doctrine\ORM\EntityManager
+    	 */
+    	$em = $this->entityManager;
+    	/**
+    	 * @var $repo Application\Entity\DefendantNameRepository
+    	 */
+    	$repo = $em->getRepository('InterpretersOffice\Entity\DefendantName');
+    	$data = json_encode($repo->autocomplete($term));
+        
+        return new JsonModel($data);
+    }
+
+}
