bede99aff35101b1353864357a7c034a0e4523eb
David Mintz <david@davidmintz.org>
2017-10-02 15:26:59 -0400

    hack development.config.php

--- config/development.config.php
+++ config/development.config.php
@@ -5,11 +5,17 @@
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
+$path = __DIR__.'/zend-developer-tools.toggle.php';
+if (file_exists($path)) {
+    $zf_tools = trim(file_get_contents(__DIR__.'/zend-developer-tools.toggle.php'));
+    $modules = (boolean)$zf_tools ? [ 'ZendDeveloperTools', ] : [];
+} else {
+    $modules = [];
+}
+
 return [
     // Additional modules to include when in development mode
-    'modules' => [
-        'ZendDeveloperTools',
-    ],
+    'modules' => $modules,
     // Configuration overrides during development mode
     'module_listener_options' => [
         'config_glob_paths' => [realpath(__DIR__).'/autoload/{,*.}{global,local}-development.php'],
