c472fff12fbfe0a2bce15311ab39b967dda2e454
David Mintz <david@davidmintz.org>
2017-10-03 11:37:26 -0400

    create an EventsController test

---
+++ module/Admin/test/Controller/EventsControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace ApplicationTest\Controller;
+
+use ApplicationTest\AbstractControllerTest;
+use ApplicationTest\FixtureManager;
+use ApplicationTest\DataFixture;
+
+class EventControllerTest extends AbstractControllerTest
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $fixtureExecutor = FixtureManager::getFixtureExecutor();
+        FixtureManager::dataSetup();
+
+        $this->login('david', 'boink');
+        $this->reset(true);
+    }
+    
+    public function testLoadEventInsertForm()
+    {
+        $this->dispatch('/admin/schedule/add');
+        $this->assertResponseStatusCode(200);
+        $this->assertQueryCount('form#event-form', 1);
+        /** to be continued */
+        
+    }
+        
+}
