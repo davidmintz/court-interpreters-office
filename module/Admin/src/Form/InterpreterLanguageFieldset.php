c55fbca585dbe8b30cce94385d55e8456ce33f07
David Mintz <david@davidmintz.org>
2017-10-18 17:01:40 -0400

    edit comments in InterpreterLanguageFieldset

--- module/Admin/src/Form/InterpreterLanguageFieldset.php
+++ module/Admin/src/Form/InterpreterLanguageFieldset.php
@@ -100,7 +100,7 @@ class InterpreterLanguageFieldset extends Fieldset implements InputFilterProvide
                         'name' => 'Callback',
                         'options' => [
                             'callback' => function($value,$context) use ($certified_languages) {
-                                
+                                // this shit does not seem to run
                                 //echo "VALIDATOR: value is $value, context is \n".print_r($context,true);
                                 $certified_language_ids = array_keys($certified_languages);
                                 //echo "certified languages ids are \n".print_r($certified_language_ids,true);                               
