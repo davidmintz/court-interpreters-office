c41672aaea80ebb4802ff29a1f1e137381aa7954
David Mintz <david@davidmintz.org>
2017-11-08 16:02:11 -0500

    (bullshit) comments in test Bootstrap

--- module/InterpretersOffice/test/Bootstrap.php
+++ module/InterpretersOffice/test/Bootstrap.php
@@ -6,5 +6,11 @@ class Bootstrap
 {
     // maybe later
 }
-
 putenv('APP_ENV=testing');
+/*
+FixtureManager::start();
+$script = __DIR__.'/data/sqlite.schema.sql';
+$db = FixtureManager::getEntityManager()->getConnection();
+$shit = $db->executeQuery(file_get_contents($script));
+etc
+ */
