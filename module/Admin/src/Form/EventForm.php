ea6b8ae7a3f279addbdeeac4c61cc2c0e9c43c7a
David Mintz <david@davidmintz.org>
2017-11-14 12:42:10 -0500

    EventForm: refactoring some pre- and post-processing

--- module/Admin/src/Form/EventForm.php
+++ module/Admin/src/Form/EventForm.php
@@ -8,6 +8,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use InterpretersOffice\Form\CsrfElementCreationTrait;
 use Zend\Stdlib\Parameters;
 
+
+
 /**
  * form for Event entity
  *
@@ -49,7 +51,7 @@ class EventForm extends ZendForm
         
     }
    /**
-    * preprocesses input and conditionall modifies validators
+    * preprocesses input and conditionally modifies validators
     * 
     * @param Parameters $input
     * @return \InterpretersOffice\Admin\Form\EventForm
@@ -66,15 +68,18 @@ class EventForm extends ZendForm
             $judge_input->setAllowEmpty(false);
             $judge_input->getValidatorChain()->attach($validator);
         }
-        
+        // heads up:  setData() has yet to happen. therefore your elements
+        // like anonymousSubmitter etc will be null 
         /** @todo untangle this and make error message specific to context */
         $anonSubmitterElement = $this->get('event')->get('anonymousSubmitter');
         $hat_options = $anonSubmitterElement->getValueOptions();
-        $hat_id = $anonSubmitterElement->getValue();        
+        $hat_id = $event['anonymousSubmitter'];
         $key = array_search($hat_id, array_column($hat_options, 'value'));
+        
         $can_be_anonymous = (!$key) ? false : 
                 $hat_options[$key]['attributes']['data-can-be-anonymous'];
-        //var_dump($hat_options[$key]['attributes']['data-can-be-anonymous']);
+        //echo "can be anonymous? " ;var_dump((boolean)$can_be_anonymous);
+        //printf("did you just fuck yourself at %d?<br>",__LINE__);
         if ((empty($event['submitter']) && empty($event['anonymousSubmitter'])) 
                 or
             (!$can_be_anonymous  && empty($event['submitter']))
@@ -94,9 +99,11 @@ class EventForm extends ZendForm
         // if NO submitter but YES anonymous submitter, submitter = NULL
         if (empty($event['submitter']) && !empty($event['anonymousSubmitter'])) {
             $event['submitter'] = null;
+            // printf("did you just fuck yourself at %d?<br>",__LINE__);
         // if YES submitter and YES anonymous submitter, anon submitter = NULL
         } elseif (!empty($event['submitter']) && !empty($event['anonymousSubmitter'])) {
             $event['anonymousSubmitter'] = null;
+            // printf("did you just fuck yourself at %d?<br>",__LINE__);
         }
         if (!empty($event['submission_date']) && !empty($event['submission_time'])) {            
             $event['submission_datetime'] = "$event[submission_date] $event[submission_time]";
@@ -114,8 +121,58 @@ class EventForm extends ZendForm
         
     }
     
-    public function postValidate()
+    
+    /**
+     * processes form data before rendering
+     * 
+     * @return void
+     */
+    public function prePopulate()
     {
-        
+        /** 
+            some data needs to be reset, reformatted, moved around
+        */
+        $event = $this->getObject();
+        $fieldset = $this->get('event');
+        // if location is set and has a parent, set parent_location element
+        $location = $event->getLocation();
+        if ($location && $parentLocation = $location->getParentLocation()) {
+            $fieldset->get('parent_location')->setValue($parentLocation->getId());
+        }
+        // OK, it is BULLSHIT that we have to do all this. am I doing something wrong 
+        // that makes this necessary?
+        // 
+        // if submitter !== NULL, set anonymousSubmitter element = hat_id of submitter
+        if (null !== $event->getSubmitter()) {
+            $hat = $event->getSubmitter()->getHat();
+            $fieldset->get('anonymousSubmitter')->setValue($hat->getId());
+            // the form element value needs to be an integer, not an object.
+            $fieldset->get('submitter')->setValue($event->getSubmitter()->getId());
+        }
+        $judge_element = $fieldset->get('judge');
+        // judge element value needs to be an integer
+        $judge = $fieldset->get('judge')->getValue();
+        if (is_object($judge)) {
+           $fieldset->get('judge')->setValue($judge->getId());
+        }
+        // if the anonymousJudge property is not null,
+        $anonymous_judge = $event->getAnonymousJudge();
+        if (is_object($anonymous_judge)) {
+            // set the judge element accordingly
+            $judge_element->setValue($anonymous_judge->getId());
+        }
+        // this needs to be a string
+        $submission_datetime = $fieldset->get('submission_datetime')->getValue();
+        if (is_object($submission_datetime)) {
+             $fieldset->get('submission_datetime')
+                 ->setValue($submission_datetime->format('Y-m-d H:i:s'));     
+        }
+        // and now that it's a string, split it into two fields
+        $submission_datetime_string =  $fieldset->get('submission_datetime')->getValue();
+        if ($submission_datetime_string) {
+            list($date,$time) = explode(' ',$submission_datetime_string);
+            $fieldset->get('submission_date')->setValue($date);
+            $fieldset->get('submission_time')->setValue($time);
+        }
     }
 }
