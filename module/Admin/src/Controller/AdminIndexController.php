8355d8db669faf2dd505bcaa87888ffe32d2e30b
David Mintz <david@davidmintz.org>
2017-11-30 16:50:55 -0500

    mutilated AdminIndexController with some experimental/scratch stuff

--- module/Admin/src/Controller/AdminIndexController.php
+++ module/Admin/src/Controller/AdminIndexController.php
@@ -19,6 +19,42 @@ class AdminIndexController extends AbstractActionController
      */
     public function indexAction()
     {
+        $target = \InterpretersOffice\Admin\Service\Acl::class;
+        /*
+        echo "triggering some-shit event on $target..." ;
+        $sharedEvents = $this->getEvent()->getApplication()->getServiceManager()->get('SharedEventManager');
+        (new \Zend\EventManager\EventManager($sharedEvents))
+                ->trigger($target,
+                        "some-shit",
+                        //$this,
+                        ['message'=>'eat shit']);
+        echo " ... wtf?";*/
+        $this->getEventManager()->trigger('some-shit',$target,['foo'=>'gack']);
+        $sharedEvents =  $this->getEventManager()->getSharedManager();
+        
+        //printf('<pre>%s</pre>',print_r($this->getEventManager()->getIdentifiers(),true));
         return new ViewModel(['title' => 'admin']);
     }
+    
+    public function __getEventManager() {
+        if (! $this->events) {
+            $container = $this->getEvent()->getApplication()->getServiceManager();
+            $sharedEvents = $container->get('SharedEventManager');
+            $this->events = new \Zend\EventManager\EventManager($sharedEvents);
+        }
+        return $this->events;
+    }
+    
+    public function __setEventManager(\Zend\EventManager\EventManagerInterface $events) {
+        
+        parent::setEventManager($events);
+        echo get_class($events),"<br>";
+        //$events->setIdentifiers([\InterpretersOffice\Admin\Service\Acl::class]);
+        $sharedEvents =  $this->getEventManager()->getSharedManager();
+        echo "\$sharedEvents is: " ,gettype($sharedEvents),"<br>";
+        $events->attach('some-shit',function($e){echo "Someshit happened involving {$e->getTarget()}!<br>";});
+        //$this->events = $events;
+        return $this;
+    }
+    
 }
