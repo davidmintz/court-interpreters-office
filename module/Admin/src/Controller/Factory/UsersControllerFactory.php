fd80f0eaddca2b2b214d97aec7134a3bbc497b64
David Mintz <david@davidmintz.org>
2017-12-06 12:12:14 -0500

    add admin UserControllerFactory

---
+++ module/Admin/src/Controller/Factory/UsersControllerFactory.php
@@ -0,0 +1,29 @@
+<?php
+/* module/Admin/src/Controller/Factory/UsersControllerFactory.php */
+namespace InterpretersOffice\Admin\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Zend\ServiceManager\Factory\FactoryInterface;
+use InterpretersOffice\Admin\Controller\UsersController;
+
+/**
+ * UsersController factory
+ */
+class UsersControllerFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
+     * @return UsersController
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        return new UsersController(
+                $container->get('entity-manager'),
+                $container->get('auth')
+        );
+        //$controller->setAuthenticationService
+        //return $controller;
+    }
+}
