63bdd4bb5ec513288e83f9bb224550423bb5117c
David Mintz <david@davidmintz.org>
2018-01-16 10:44:58 -0500

    furhter fiddling with script to import judges and courtrooms

--- bin/import-judges-and-courtrooms.php
+++ bin/import-judges-and-courtrooms.php
@@ -1,8 +1,6 @@
 <?php
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * imports judges and courtrooms using JSON data provided as stdin
  */
 
 $json = file_get_contents("php://stdin");
@@ -18,10 +16,6 @@ if (! $data) {
 const TYPE_COURTHOUSE = 7;
 const TYPE_COURTROOM = 1;
 
-
-//print_r($data); exit;
-
-
 $hat_id = $db->query('SELECT id from hats WHERE name = "Judge"')->fetchColumn();
 $flavors = $db->query('SELECT flavor, id FROM judge_flavors')->fetchAll(PDO::FETCH_KEY_PAIR);
 
@@ -29,7 +23,7 @@ $courthouses =  $db->query('SELECT name,id FROM locations WHERE type_id = '.TYPE
 
 $courthouses['300 Quarropas'] = $courthouses['White Plains']; // alias
 
-$USDJ = $data['District'];
+
 $person_insert = $db->prepare(
     'INSERT INTO people (hat_id,lastname,firstname,middlename,active,discr) 
         VALUES (:hat_id,:lastname,:firstname,:middlename,1, "judge")'
@@ -58,55 +52,59 @@ $location_select = $db->prepare('SELECT id FROM locations WHERE name = :name and
 | active             | tinyint(1)           | NO   |     | 1       |                |
 +--------------------+----------------------+------+-----+---------+----------------+
 */
-//[Swain, Laura Taylor] => 17C, 500 Pearl
 
-foreach ($USDJ as $name => $location) {
+//[Swain, Laura Taylor] => 17C, 500 Pearl
+//$USDJ = $data['District'];
+foreach ($data as $flavor) {
     
-    list($lastname,$given_names) = preg_split('/, +/',$name);    
-    if (strstr($given_names,' ')) {
-        list($firstname,$middlename) = preg_split('/ +/',$given_names);
-    } else {
-        $firstname = $given_names; 
-        $middlename = '';
-    }
-    list($courtroom, $courthouse) = preg_split('/, +/',$location);
-        //printf("courtroom: %s, courthouse %s\n",$courtroom, $courthouse);
-        //continue;
-    
-    try {
-        
-        $location_insert->execute([
-            ':type_id'=>  TYPE_COURTROOM,
-            ':name' => $courtroom,
-            ':parent_location_id' => $courthouses[$courthouse],
-        ]);
-        $location_id =  $db->query('SELECT last_insert_id()')->fetchColumn();
-        echo "inserted new location $courtroom with id $location_id\n";
-        
-    } catch (Exception $e) {
-        
-        printf("caught %s: %s\n",get_class($e),$e->getMessage());
-        
-        $location_select->execute(['name'=>$courtroom,'type_id'=>TYPE_COURTROOM]);   //.TYPE_COURTROOM);
-        $location_id = $location_select->fetchColumn();
-        echo "found location ($courtroom) id $location_id already existing\n";
-    }
-    
-    try {
-        $person_insert->execute(
-            compact('hat_id','lastname','firstname','middlename')
-        );
-        $id = $db->query('SELECT last_insert_id()')->fetchColumn();
-        printf("judge %s added to people id %s\n",$lastname,$id);
-        $judge_insert->execute([
-            ':id'=>$id,
-            ':default_location_id'=>$location_id,
-            ':flavor_id' => $flavors['USDJ'],
-         ]);
-        
-    } catch (Exception $e) {
-        echo "insert threw exception: ".$e->getMessage();
-        echo "\n";
+    foreach ($flavor as $name => $location) {
+
+        list($lastname,$given_names) = preg_split('/, +/',$name);    
+        if (strstr($given_names,' ')) {
+            list($firstname,$middlename) = preg_split('/ +/',$given_names);
+        } else {
+            $firstname = $given_names; 
+            $middlename = '';
+        }
+        list($courtroom, $courthouse) = preg_split('/, +/',$location);
+            //printf("courtroom: %s, courthouse %s\n",$courtroom, $courthouse);
+            //continue;
+
+        try {
+
+            $location_insert->execute([
+                ':type_id'=>  TYPE_COURTROOM,
+                ':name' => $courtroom,
+                ':parent_location_id' => $courthouses[$courthouse],
+            ]);
+            $location_id =  $db->query('SELECT last_insert_id()')->fetchColumn();
+            echo "inserted new location $courtroom with id $location_id\n";
+
+        } catch (Exception $e) {
+
+            printf("caught %s: error code %d\n%s\n",get_class($e),$e->getCode(),$e->getMessage());
+
+            $location_select->execute(['name'=>$courtroom,'type_id'=>TYPE_COURTROOM]);   //.TYPE_COURTROOM);
+            $location_id = $location_select->fetchColumn();
+            echo "found location ($courtroom) id $location_id already existing\n";
+        }
+
+        try {
+            $person_insert->execute(
+                compact('hat_id','lastname','firstname','middlename')
+            );
+            $id = $db->query('SELECT last_insert_id()')->fetchColumn();
+            printf("judge %s added to people id %s\n",$lastname,$id);
+            $judge_insert->execute([
+                ':id'=>$id,
+                ':default_location_id'=>$location_id,
+                ':flavor_id' => $flavors['USDJ'],
+             ]);
+
+        } catch (Exception $e) {
+            echo "insert threw exception: ".$e->getMessage();
+            echo "\n";
+        }
     }
 }
 /*
