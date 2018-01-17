c6b96aeaa4c95de89f12c4e72537dc75e79ee761
David Mintz <david@davidmintz.org>
2018-01-17 13:00:20 -0500

    bin/import-event-types.php: save to disk a map of old => new event_type_ids as JSON

--- bin/import-event-types.php
+++ bin/import-event-types.php
@@ -30,27 +30,38 @@ $event_types_query = $old_db->query('SELECT proceeding_id, type, category, comme
 
 // one canonical kind of probation PSI interview, etc
 // i.e., no variations based on location
-foreach( ['probation PSI interview',"PTS supervison interview",'probation supervision interview'] as $type) {
+foreach( [
+    'probation PSI interview' => 'ID_PROBATION_PSI',
+    "PTS supervison interview"  => 'ID_PTS_SUPERVISION',
+    'probation supervision interview' => 'ID_PROBATION_SUPERVISION',] as $type => $varname) {
     try {
      $event_type_insert->execute([
         'name' => $type,'category_id' => $event_categories['out'],
          'comments' => '',
      ]);
-     
+     ${$varname} = $db->query('SELECT LAST_INSERT_ID()')->fetchColumn(); 
     } catch (PDOException $e) {
          if ($e->getCode() == 23000) {
              echo("'$type' already exists\n");
+             ${$varname} = $db->query("SELECT id FROM event_types WHERE name = '$type'")->fetchColumn();             
              continue;
          } else {
              throw $e;
          }     
-    }
+    }    
 }
- 
+// old => new
+$event_type_map = [];
 while ($type = $event_types_query->fetch(PDO::FETCH_ASSOC)) {
-    if (stristr($type['type'],'probation') or 1 === preg_match('/PTS.+supervision/i', $type['type'])) {
+    if (stristr($type['type'],'probation')) {
+        $this_id = stristr($type['type'],'supervision') ? $ID_PROBATION_SUPERVISION :  $ID_PROBATION_PSI;
+        $event_type_map[$type['proceeding_id']] = $this_id;
+        //or 1 === preg_match('/PTS.+supervision/i', $type['type'])) {
         printf("skipping event type: %s\n",$type['type']);
         continue;
+    }
+    if ( 1 === preg_match('/PTS.+supervision/i', $type['type'])) {
+         $event_type_map[$type['proceeding_id']] = $ID_PTS_SUPERVISION;
     }
    
     if (! $type['display']) {
@@ -70,14 +81,27 @@ while ($type = $event_types_query->fetch(PDO::FETCH_ASSOC)) {
             'category_id' => $event_categories[$type['category']],
             'comments' => $type['comments']
         ]);
+        $event_type_map[$type['proceeding_id']] =$db->query('SELECT LAST_INSERT_ID()')->fetchColumn();
         echo "OK\n";
     } catch (PDOException $e) {
         if ($e->getCode() == 23000) {
+            // to do: prepare, parameterize!
+            $event_type_map[$type['proceeding_id']] = $db->query("SELECT id FROM event_types WHERE name = '{$type['type']}'")->fetchColumn(); 
             printf("'%s' is a duplicate, moving on\n",$type['type']);
         } else {
             printf("insertion of '%s' FAILED: %s\n",$type['type'], $e->getMessage());
         }        
     }    
+}
+$map = json_encode($event_type_map);
+$path = __DIR__ . '/event-type-map.json';
+$result = file_put_contents($path,$map);
+if (false === $result) {
+    echo "SHIT. failed writing \$event_type_map to $path\n";
+    exit(1);
+} elseif (0 === $result) {
+    echo "SHIT. no data contained in our \$event_type_map";
+    exit(1);
 }
 exit(0);
 
