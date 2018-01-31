391c2cdf2b84ecabf45b862f45c7cfedd23d1484
David Mintz <david@davidmintz.org>
2018-01-31 16:28:37 -0500

    more work on scripting data import

--- bin/sdny/connect.php
+++ bin/sdny/connect.php
@@ -2,7 +2,7 @@
 
 $db_params = parse_ini_file(getenv('HOME').'/.my.cnf');
 $db = new PDO('mysql:host=localhost;dbname=office', $db_params['user'], $db_params['password'],[
-    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
 ]);
 
 return $db;
--- bin/sdny/import-staff-users.php
+++ bin/sdny/import-staff-users.php
@@ -1,5 +1,16 @@
 #!/usr/bin/env php
 <?php
 $db = require(__DIR__."/connect.php");
+$db->exec("use dev_interpreters");
+$sql=
+'SELECT DISTINCT user_id, name, email, interpreter_id FROM users LEFT JOIN events 
+ON (events.lastmod_by = user_id OR events.created_by = user_id) 
+WHERE COALESCE(lastmod_by, created_by) 
+IS NOT NULL OR (req_class = 3 AND req_by = users.user_id)';
+
+$staff_users = $db->query($sql)->fetchAll();
+
+print_r($staff_users);
+
 
 
