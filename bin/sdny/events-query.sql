489e4be1ea57e77763f85ff0e3707cb1c375be2e
David Mintz <david@davidmintz.org>
2018-02-07 10:31:57 -0500

    work on event data import

--- bin/sdny/events-query.sql
+++ bin/sdny/events-query.sql
@@ -1,20 +1,3 @@
-/*
-+----+------------------+
-| id | type             |
-+----+------------------+
-|  1 | AUSA             |
-|  2 | USPO             |
-|  3 | staff interp     |
-|  4 | freelance interp |
-|  5 | ctroom staff     |
-|  6 | Pretrial         |
-|  7 | Magistrates      |
-|  8 | defense atty     |
-|  9 | other            |
-| 10 | USAO staff       |
-+----+------------------+
-
-*/
 SELECT e.event_id AS id, e.event_date AS date, e.event_time AS time, e.end_time, e.docket,
 e.proceeding_id  AS event_type_id, p.type, e.language_id, l.name language,
 e.judge_id, j.lastname judge_lastname, j.firstname judge_firstname,
@@ -40,7 +23,9 @@ CASE
         "UNKNOWN"
 END AS submitter,
 g.id AS submitter_group_id,
-e.created, e.created_by, e.lastmod AS modified, e.lastmod_by AS modified_by_id,
+e.created, e.created_by AS created_by_id, u.name AS created_by,
+e.lastmod AS modified, e.lastmod_by AS modified_by_id,
+u_modifier.name as modified_by,
 e.cancel_reason, e.notes AS comments, e.admin_notes AS admin_comments
 
 
@@ -56,4 +41,6 @@ LEFT JOIN request_users ru ON ru.id = e.req_by
 LEFT JOIN request_by rb ON e.req_by = rb.id
 LEFT JOIN interpreters i ON e.req_by = i.interp_id
 LEFT JOIN groups g ON g.id = ru.group_id
-LEFT JOIN users u ON u.user_id = e.req_by AND e.req_class = 3
+LEFT JOIN users u_submitter ON u_submitter.user_id = e.req_by /*AND e.req_class = 3*/
+LEFT JOIN users u ON u.user_id = e.created_by
+LEFT JOIN users u_modifier ON u_modifier.user_id = e.lastmod_by
