2978845291a26983769f6f866917a0edbc8ae435
David Mintz <david@davidmintz.org>
2018-02-21 12:52:00 -0500

    tweak Event repository getView()

--- module/InterpretersOffice/src/Entity/Repository/EventRepository.php
+++ module/InterpretersOffice/src/Entity/Repository/EventRepository.php
@@ -17,38 +17,38 @@ use InterpretersOffice\Entity;
  */
 class EventRepository extends EntityRepository implements CacheDeletionInterface
 {
-    
+
     /**
      * DQL statement for human-friendly representation of Event
-     * 
+     *
      * note to self: maybe re-implement with querybuilder as a learning
      * exercise, and because Doctrine recommends it?
-     * 
-     * @var string 
+     *
+     * @var string
      */
     protected $view_dql = <<<DQL
-            
-         SELECT e.id, e.date, e.time, 
-         COALESCE(j.lastname, aj.name) AS judge, 
+
+         SELECT e.id, e.date, e.time,
+         COALESCE(j.lastname, aj.name) AS judge,
          t.name AS type,
          lang.name AS language,
          e.docket,
          loc.name AS location,
          ploc.name AS parent_location,
-         COALESCE(CONCAT(p.lastname,', ',p.firstname), anon_submitter.name) 
+         COALESCE(CONCAT(p.lastname,', ',p.firstname), anon_submitter.name)
              AS submitter,
          h.name AS submitter_hat,
          e.submission_date, e.submission_time,
          user1.username AS created_by,
          e.created,
          user2.username AS last_updated_by,
-         e.modified,
+         e.modified AS last_updated,
          e.comments,
          e.admin_comments
          FROM InterpretersOffice\Entity\Event e
          LEFT JOIN e.judge j JOIN e.eventType t
          LEFT JOIN e.anonymousJudge aj
-         JOIN e.language lang            
+         JOIN e.language lang
          LEFT JOIN e.location loc
          LEFT JOIN loc.parentLocation ploc
          LEFT JOIN e.submitter p
@@ -57,63 +57,63 @@ class EventRepository extends EntityRepository implements CacheDeletionInterface
          JOIN e.createdBy user1
          LEFT JOIN e.modifiedBy user2
 DQL;
-    
+
     /**
      * constructor
      *
      * @param \Doctrine\ORM\EntityManager  $em    The EntityManager to use.
      * @param \Doctrine\ORM\Mapping\ClassMetadata $class The class descriptor.
      */
-    public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class)            
-    {       
+    public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class)
+    {
         parent::__construct($em, $class);
         $this->cache = $em->getConfiguration()->getResultCacheImpl();
         $this->cache->setNamespace($this->cache_namespace);
     }
-    
+
     /**
      * query result cache
-     * 
+     *
      * @var \Doctrine\Common\Cache\Cache
      */
     protected $cache;
-    
+
     /**
      * cache namespace
-     * 
+     *
      * @var string
      */
     protected $cache_namespace = 'events';
-    
+
     /**
      * enable result cache
-     * 
+     *
      * for convenience during development
-     * 
+     *
      * @var boolean whether to enable caching
      */
     protected $cache_enabled = false;
-    
-    
+
+
     /**
      * returns human-readable representation of event
-     * 
+     *
      * @param type $id
      * @return array
      */
     public function getView($id)
     {
-        
+
          return $this
                  ->getEntityManager()
                  ->createQuery($this->view_dql . ' WHERE e.id = :id')
                  ->setParameters(['id'=>$id])
                  ->getOneOrNullResult();
     }
-    
+
     /**
      * gets data for populating cancellation menu options
-     * 
+     *
      * @return Array
      */
     public function getCancellationOptions()
@@ -127,7 +127,7 @@ DQL;
         $dql = 'SELECT r.id AS value, r.reason AS label FROM '
                 . 'InterpretersOffice\Entity\ReasonForCancellation r '
                 . ' ORDER BY r.reason';
-        $result = $this->getEntityManager()->createQuery($dql)->getResult();        
+        $result = $this->getEntityManager()->createQuery($dql)->getResult();
         $index = array_search('other',array_column($result, 'label'));
         if ($index) {
             $tmp = $result[$index];
@@ -137,14 +137,14 @@ DQL;
         $cache->save($id,$result);
 
         return $result;
-        
+
     }
-    
+
     /**
-     * gets the schedule 
-     * 
+     * gets the schedule
+     *
      * to be continued
-     * 
+     *
      * @param array $options
      * @return array
      */
@@ -153,8 +153,8 @@ DQL;
         if (! isset($options['date'])) {
             $options['date'] = date('Y-m-d');
         }
-        $dql = 'SELECT e.id, e.date, e.time, 
-         COALESCE(j.lastname, aj.name) AS judge, 
+        $dql = 'SELECT e.id, e.date, e.time,
+         COALESCE(j.lastname, aj.name) AS judge,
          t.name AS type,
          lang.name AS language,
          e.docket, e.comments,
@@ -163,35 +163,35 @@ DQL;
          cat.category,
          loc_type.type AS location_type
          FROM InterpretersOffice\Entity\Event e
-         LEFT JOIN e.judge j 
+         LEFT JOIN e.judge j
          JOIN e.eventType t
          JOIN t.category cat
          LEFT JOIN e.anonymousJudge aj
-         JOIN e.language lang            
+         JOIN e.language lang
          LEFT JOIN e.location loc
          LEFT JOIN loc.type as loc_type
-         LEFT JOIN loc.parentLocation ploc 
+         LEFT JOIN loc.parentLocation ploc
          WHERE e.date = :date
          ORDER BY e.time, e.id';
-        
+
         $events = $this->getEntityManager()->createQuery($dql)
                 ->setParameters([':date'=>$options['date']])
                 ->useResultCache($this->cache_enabled)
                 ->getResult();
-        
+
         if (! $events) {
             return [];
         }
-        $ids = array_column($events,'id');        
+        $ids = array_column($events,'id');
         $defendants = $this->getDefendantsForEvents($ids);
         $interpreters = $this->getInterpretersForEvents($ids);
-        
+
         return compact('events','interpreters','defendants');
     }
-    
+
     /**
      * gets defendant names for a given set of events
-     * 
+     *
      * @param array $ids array of event ids
      * @return array
      */
@@ -201,12 +201,12 @@ DQL;
          'SELECT e.id event_id, d.id, d.surnames, d.givenNames FROM '
             . 'InterpretersOffice\Entity\DefendantName d JOIN d.events e '
             . 'WHERE e.id IN (:ids)'
-        );       
+        );
         $return = [];
-        
+
         $data = $query->setParameters(['ids'=> $ids])
                 ->useResultCache($this->cache_enabled)->getResult();
-        
+
         foreach ($data as $deft) {
             $event_id = $deft['event_id'];
             unset($deft['event_id']);
@@ -214,15 +214,15 @@ DQL;
                 $return[$event_id][] = $deft;
             } else {
                 $return[$event_id] = [ $deft ];
-            }           
+            }
         }
-        
+
         return $return;
     }
-    
+
     /**
      * gets interpreters for a given set of events
-     * 
+     *
      * @param array $ids event ids
      * @return array
      */
@@ -233,13 +233,13 @@ DQL;
          . 'InterpretersOffice\Entity\InterpreterEvent ie JOIN ie.interpreter i '
          . 'JOIN ie.event e WHERE e.id IN (:ids)'
         );
-        
-       
+
+
         $return = [];
-        
+
         $data = $query->setParameters(['ids'=> $ids])
                 ->useResultCache($this->cache_enabled)->getResult();
-        
+
         foreach ($data as $interpreter) {
             $event_id = $interpreter['event_id'];
             unset($interpreter['event_id']);
@@ -247,15 +247,15 @@ DQL;
                 $return[$event_id][] = $interpreter;
             } else {
                 $return[$event_id] = [ $interpreter ];
-            }           
+            }
         }
-        
+
         return $return;
     }
-    
+
     /**
      * implements cache deletion
-     * 
+     *
      * @param string $cache_id
      * @return boolean true if successful(ish)
      */
@@ -264,7 +264,7 @@ DQL;
             return $this->cache->delete($cache_id);
         }
         $this->cache->setNamespace($this->cache_namespace);
-        
+
         return $this->cache->deleteAll();
-    }    
+    }
 }
