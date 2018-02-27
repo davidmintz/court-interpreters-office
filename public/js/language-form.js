0fc69f817ef584dcdb9edf7b01d2fe38e0b0abe4
David Mintz <david@davidmintz.org>
2018-02-27 10:54:23 -0500

    language/form and .js: rename a variable for style consistency

--- public/js/language-form.js
+++ public/js/language-form.js
@@ -5,7 +5,7 @@ $(function(){
         if (! window.confirm("Are you sure you want to delete this language?")) {
             return;
         }
-        var name = $('form#language').data('languageName');
+        var name = $('form#language').data('language_name');
         var url = $('form#language').data('redirect_url');
         var id = $('input[name="id"]').val();
         $.post('/admin/languages/delete/'+id,{name:name},function(response){
