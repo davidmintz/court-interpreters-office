0f36e3c4a05d204bca605e1f086a4ca77bd3a2eb
David Mintz <david@davidmintz.org>
2018-03-02 11:20:41 -0500

    update constructor calls in JudgeLoader data fixture

--- module/InterpretersOffice/test/DataFixture/JudgeLoader.php
+++ module/InterpretersOffice/test/DataFixture/JudgeLoader.php
@@ -10,14 +10,15 @@ class JudgeLoader implements FixtureInterface
 {
     public function load(ObjectManager $objectManager)
     {
-        $failla = new Entity\Judge();
+        $judgeHat = $objectManager->getRepository('InterpretersOffice\Entity\Hat')
+            ->findOneBy(['name' => 'Judge']);
+        $failla = new Entity\Judge($judgeHat);
         $usdj = $objectManager->getRepository('InterpretersOffice\Entity\JudgeFlavor')
             ->findOneBy(['flavor' => 'USDJ']);
         $locations =  $objectManager->getRepository(Entity\Location::class);
         $courtroom_618 = $locations->findOneBy(['name' => '618']);
 
-        $judgeHat = $objectManager->getRepository('InterpretersOffice\Entity\Hat')
-            ->findOneBy(['name' => 'Judge']);
+
         $failla
             ->setHat($judgeHat)
             ->setFlavor($usdj)
@@ -27,9 +28,8 @@ class JudgeLoader implements FixtureInterface
         $failla->setDefaultLocation($courtroom_618);
         $objectManager->persist($failla);
 
-        $daniels = new Entity\Judge();
-        $daniels->setHat($judgeHat)
-            ->setFlavor($usdj)
+        $daniels = new Entity\Judge($judgeHat);
+        $daniels->setFlavor($usdj)
             ->setFirstname('George')
             ->setLastname('Daniels')
             ->setActive(true)
@@ -39,16 +39,15 @@ class JudgeLoader implements FixtureInterface
             );
          $objectManager->persist($daniels);
         // a few make-believe
-        $dinklesnort = new Entity\Judge();
-        $dinklesnort->setHat($judgeHat)
-            ->setFlavor($usdj)
+        $dinklesnort = new Entity\Judge($judgeHat);
+        $dinklesnort->setFlavor($usdj)
             ->setFirstname('Roland')
-            ->setMiddleName('Z.')                
+            ->setMiddleName('Z.')
             ->setLastname('Dinklesnort')
             ->setActive(true)
             ->setDefaultLocation($locations->findOneBy(['name' => '14A']));
         $objectManager->persist($dinklesnort);
-        
+
         // the Magistrate
         $mag = new Entity\AnonymousJudge();
         $mag->setDefaultLocation($locations->findOneBy(['name' => '5A']))
