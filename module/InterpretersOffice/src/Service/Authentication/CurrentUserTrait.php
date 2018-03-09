befb64bbe42e0576ee67a739ec4124b9d647ae74
David Mintz <david@davidmintz.org>
2018-03-09 17:23:59 -0500

    added an experimental Trait for fetching current user

---
+++ module/InterpretersOffice/src/Service/Authentication/CurrentUserTrait.php
@@ -0,0 +1,38 @@
+<?php /** module/InterpretersOffice/src/Service/Authentication/CurrentUserTrait.php */
+
+namespace InterpretersOffice\Service\Authentication;
+
+use Zend\Authentication\AuthenticationServiceInterface;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
+
+/**
+ * gets the currently authenticated user
+ *
+ */
+trait CurrentUserTrait
+{
+    /*
+     * auth
+     *
+     * @var AuthenticationServiceInterface
+     */
+    protected $auth;
+
+    /**
+     * gets the User entity corresponding to authenticated identity
+     *
+     * @param EntityManager $em
+     * @return Entity\User
+     */
+    protected function getAuthenticatedUser(EntityManager $em)
+    {
+        $dql = 'SELECT u FROM InterpretersOffice\Entity\User u WHERE u.id = :id';
+        $id = $this->getAuth()->getIdentity()->id;
+        $query = $em->createQuery($dql)
+                ->setParameters(['id' => $id])
+                ->useResultCache(true);
+        $user = $query->getOneOrNullResult();
+
+        return $user;
+    }
+}
