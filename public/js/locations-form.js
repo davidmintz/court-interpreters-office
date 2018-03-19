84f20b05014193f0938c6342c5748231ee19855a
David Mintz <david@davidmintz.org>
2018-03-19 15:48:28 -0400

    work on "delete" in locations form etc

--- public/js/locations-form.js
+++ public/js/locations-form.js
@@ -1,14 +1,14 @@
-/**  public/js/language-form.js */
+/**  public/js/locations-form.js */
 $(function(){
     $('#btn-delete').on("click",function(event){
         event.preventDefault();
-        if (! window.confirm("Are you sure you want to delete this language?")) {
+        if (! window.confirm("Are you sure you want to delete this location?")) {
             return;
         }
-        var name = $('form#language').data('language_name');
-        var url = $('form#language').data('redirect_url');
+        var name = $('form#location').data('location_name');
+        var url = $('form#location').data('redirect_url');
         var id = $('input[name="id"]').val();
-        $.post('/admin/languages/delete/'+id,{name:name},function(response){
+        $.post('/admin/locations/delete/'+id,{name:name},function(response){
             if (response.redirect) {
                 // back to index page
                 document.location = url;
@@ -19,7 +19,7 @@ $(function(){
                     $('<div/>')
                         .addClass("alert alert-warning")
                         .attr({id:"failed_deletion_error"}).html(error)
-                        .insertBefore($('form#language'));
+                        .insertBefore($('form#location'));
                 } else {
                     $('#failed_deletion_error').html(error);
                 }
