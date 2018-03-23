584ad792dae848d8cbf6d6633f5f09a962ede0cc
David Mintz <david@davidmintz.org>
2018-03-23 16:27:40 -0400

    rework navigation config for breadcrumbs

--- config/autoload/navigation.global.php
+++ config/autoload/navigation.global.php
@@ -10,76 +10,71 @@ use InterpretersOffice\Admin\Controller as Admin;
 return [
 
     'navigation' => [
-        'default' => [
-            [
-                'label' => 'schedule',
-                'route' => 'events',
-                'resource' => Admin\EventsController::class,
-                'expand_children' => false,
-                'pages' =>
-                [
-                    [
-                        'label' => 'add',
-                        'route' => 'events/add'
-                    ],
-                    [
-                        'label' => 'edit',
-                        'route' => 'events/edit'
-                    ],
-                ],
-            ],
-            // yes, two of these: the above to make the breadcrumbs helper work,
-            // the next for the main nav
-            [
-                'label' => 'people',
-                'route' => 'people',
-                'resource' => Admin\PeopleController::class,
-                'expand_children' => false,
-                'display' => false,
-                'pages' => [
-                    [
-                    'label' => 'add',
-                    'route' => 'people/add'
-                    ],
-                    [
-                    'label' => 'edit',
-                    'route' => 'people/edit'
-                    ],
-                ]
-            ],
-
-
-            [
-                'label' => 'add event',
-                'route' => 'events/add',
-                'resource' => Admin\EventsController::class,
-                'privilege' => 'add',
-            ],
-            [
-                'label' => 'interpreters',
-                'route' => 'interpreters',
-                'resource' => Admin\InterpretersController::class,
-                'expand_children' => false,
-                'pages' => [
-                    [
-                        'label' => 'add',
-                        'route' => 'interpreters/add'
-                    ],
-                    [
-                        'label' => 'edit',
-                        'route' => 'interpreters/edit',
-                    ]
-                ]
-            ],
+        // breadcrumbs navigation helper
+        'admin_breadcrumbs' =>
+        [
             [
                 'label' => 'admin',
                 'route' => 'admin',
-                'resource' => Admin\AdminIndexController::class,
-                //'privilege' => 'index','action' => 'index',
                 'pages' => [
                     [
-                        'label' => 'main',
-                        'route' => 'admin',
+                        'label' => 'judges',
+                        'route' => 'judges',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'judges/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'judges/edit'
+                            ],
+                        ]
+                    ],
+                    [
+                        'label' => 'schedule',
+                        'route' => 'events',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'events/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'events/edit'
+                            ],
+                        ],
+                    ],
+                    [
+                        'label' => 'interpreters',
+                        'route' => 'interpreters',
+                        //'resource' => Admin\InterpretersController::class,
+                        'pages' => [
+                            [
+                                'label' => 'add',
+
+
+
+                                'route' => 'interpreters/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'interpreters/edit',
+                            ],
+                            [
+                                'label' => 'view details',
+                                'route' => 'interpreters/find_by_id',
+                            ],
+                        ],
+                    ],
+                    [
+                        'label' => 'interpreters',
+                        'route' => 'interpreters/find_by_language',
+
+                    ],
+                    [
+                        'label' => 'interpreters',
+                        'route' => 'interpreters/find_by_name',
                     ],
                     [
                         'label' => 'languages',
@@ -96,6 +91,51 @@ return [
                         ]
                     ],
                     [
+                        'label' => 'people',
+                        'route' => 'people',
+                        //'resource' => Admin\PeopleController::class,
+                        //'expand_children' => false,
+                        //'display' => false,
+                        'pages' => [
+                            [
+                            'label' => 'add',
+                            'route' => 'people/add'
+                            ],
+                            [
+                            'label' => 'edit',
+                            'route' => 'people/edit'
+                            ],
+                        ]
+                    ],
+                    [
+                        'label' => 'event-types',
+                        'route' => 'event-types',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'event-types/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'event-types/edit'
+                            ],
+                        ],
+                    ],
+                    [
+                        'label' => 'users',
+                        'route' => 'users',
+                        'pages' => [
+                            [
+                                'label' => 'add',
+                                'route' => 'users/add'
+                            ],
+                            [
+                                'label' => 'edit',
+                                'route' => 'users/edit'
+                            ],
+                        ]
+                    ],
+                    [
                         'label' => 'locations',
                         'route' => 'locations',
                         'pages' => [
@@ -108,6 +148,46 @@ return [
                                 'route' => 'locations/edit'
                             ],
                         ]
+                    ],
+                ],
+            ],
+        ],
+        // main navigation menu
+        'default' => [
+            [
+                'label' => 'schedule',
+                'route' => 'events',
+                'resource' => Admin\EventsController::class,
+
+            ],
+            [
+                'label' => 'add event',
+                'route' => 'events/add',
+                'resource' => Admin\EventsController::class,
+                'privilege' => 'add',
+            ],
+            [
+                'label' => 'interpreters',
+                'route' => 'interpreters',
+                'resource' => Admin\InterpretersController::class,
+            ],
+            [
+                'label' => 'other data',
+                'route' => 'admin',
+                'resource' => Admin\AdminIndexController::class,
+                //'privilege' => 'index','action' => 'index',
+                'pages' => [
+                    [
+                        'label' => 'main',
+                        'route' => 'admin',
+                    ],
+                    [
+                        'label' => 'languages',
+                        'route' => 'languages',
+                    ],
+                    [
+                        'label' => 'locations',
+                        'route' => 'locations',
                     ],
                     [
                         'label' => 'judges',
@@ -126,30 +206,16 @@ return [
                     [
                         'label' => 'users',
                         'route' => 'users',
-                        'pages' => [
-                            [
-                                'label' => 'add',
-                                'route' => 'users/add'
-                            ],
-                            [
-                                'label' => 'edit',
-                                'route' => 'users/edit'
-                            ],
-                        ]
                     ],
                     [
+                        'label' => 'people',
+                        'route' => 'people',
+                        'resource' => Admin\PeopleController::class,
+                    ],
+                    [
+                        'resource' => Admin\EventTypesController::class,
                         'label' => 'event-types',
                         'route' => 'event-types',
-                        'pages' => [
-                            [
-                                'label' => 'add',
-                                'route' => 'event-types/add'
-                            ],
-                            [
-                                'label' => 'edit',
-                                'route' => 'event-types/edit'
-                            ],
-                        ]
                     ],
                     [
                         'label' => 'defendants',
@@ -163,23 +229,6 @@ return [
                             [
                                 'label' => 'edit',
                                 'route' => 'admin-defendants/edit'
-                            ],
-                        ]
-                    ],
-                    // for the breadcrumbs only
-                    [
-                        'label' => 'schedule',
-                        'route' => 'events',
-                        'expand_children' => true,
-                        'display' => false,
-                        'pages' => [
-                            [
-                                'label' => 'add',
-                                'route' => 'events/add'
-                            ],
-                            [
-                                'label' => 'edit',
-                                'route' => 'events/edit'
                             ],
                         ]
                     ],
