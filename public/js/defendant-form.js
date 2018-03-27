96fea5272b8aab7cdbb893bd0218a80eb9683a42
David Mintz <david@davidmintz.org>
2018-03-27 17:21:32 -0400

    add a js file

---
+++ public/js/defendant-form.js
@@ -0,0 +1,29 @@
+/** public/js/defendant-form.js */
+
+$(function(){
+    $('#btn-submit').on("click",function(event){
+        event.preventDefault();
+        var form = $('#defendant-form');
+        var data = form.serialize();
+
+        $.post(form.attr('action'),data,function(response){
+            if (response.validation_errors) {
+                return displayValidationErrors(response.validation_errors);
+            } else {
+                $('.validation-error').hide();
+            }
+            if (response.inexact_duplicate_found) {
+                var existing = response.existing_entity;
+                form.prepend($('<input>').attr({type:'hidden',name:'duplicate_resolution_required',value:1}));
+                $('#deft-existing-duplicate-name').text(existing);
+                var shit = "p.duplicate-name-instructions, .duplicate-resolution-radio";
+                return $(shit).show();
+            }
+            if (response.error) {
+                alert('shit. there was an error: '+response.error);
+            }
+
+
+        },'json');
+    });
+});
