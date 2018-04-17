c8da5dbac7330f022478c2b99bc2fb8d2d926f7b
David Mintz <david@davidmintz.org>
2018-04-17 11:55:33 -0400

    DefendantName repository work on updating names, deft-events, remove some crap that didn't work

--- module/InterpretersOffice/src/Entity/Repository/DefendantNameRepository.php
+++ module/InterpretersOffice/src/Entity/Repository/DefendantNameRepository.php
@@ -185,21 +185,26 @@ class DefendantNameRepository extends EntityRepository implements CacheDeletionI
     }
 
     /**
-     * gets all the DefendantEvents for DefendantName
+     * gets DefendantEvents for DefendantName
      *
      * interesting fact: INDEX BY does not trigger an error but neither
      * does it seem to work unless the other columns are scalar
      *
      * @param  EntityDefendantName $defendantName
+     * @param  int $exclude_event_id event to exclude from query
      * @return Array
      */
-    public function getDefendantEventsForDefendant(Entity\DefendantName $defendantName)
+    public function getDefendantEventsForDefendant(Entity\DefendantName $defendantName,$exclude_event_id = null)
     {
         $dql = 'SELECT de FROM InterpretersOffice\Entity\DefendantEvent de
             JOIN de.defendant d JOIN de.event e WHERE d.id = :id';
-            // INDEX BY e.id -- before WHERE
+        $params = ['id' => $defendantName->getId()];
+        if ($exclude_event_id) {
+            $dql .= ' AND e.id <> :event_id';
+            $params['event_id'] = $exclude_event_id;
+        }
         return   $this->createQuery($dql)
-            ->setParameters(['id' => $defendantName->getId()])
+            ->setParameters($params)
             ->getResult();
 
     }
@@ -231,7 +236,7 @@ class DefendantNameRepository extends EntityRepository implements CacheDeletionI
         $em = $this->getEntityManager();
 
         /** is it a global update, or an update of only a subset? */
-        $logger->debug("event id: " . ($event_id ?: "null"));
+        $logger->debug("event id is: " . ($event_id ?: "null"));
 
         foreach ($occurrences as $i => $occurrence) {
             // unpack submitted JSON strings schedule
@@ -303,7 +308,7 @@ class DefendantNameRepository extends EntityRepository implements CacheDeletionI
                         $result['updated_deftname'] = $existing_name->getId();
                     }
                 // swap out $deftName for existing, and detach
-                    $deft_events = $this->getDefendantEventsForDefendant($defendantName);
+                    $deft_events = $this->getDefendantEventsForDefendant($defendantName,$event_id);
                     $result['count_deft_events_updated'] = count($deft_events);
                     foreach ($deft_events as $de) {
                         $de->setDefendantName($existing_name);
@@ -387,35 +392,7 @@ class DefendantNameRepository extends EntityRepository implements CacheDeletionI
                     $result['deftname_replaced_by'] = $existing_name->getId();
                     break;
             }
-            if ($event_id) {
-                /*
-                 * interesting problem. the related Event gets it modification timestamp
-                 * updated AFTER this has run, not before. so we can't get the new
-                 * time to send back because it hasn't yet been updated
-                 */
-                // do it manually ourself
-                /** @var Entity\Event $event_entity */
-                $event_entity = $this->getEntityManager()->find(Entity\Event::class,$event_id);
-                if (!in_array($event_id,$result['events_affected'])) {
-                    $this->logger->debug("and $event_id was NOT(?) among the affected?");
-
-                    $collection = $event_entity->getDefendantNames();
-                    $what = isset($new) ? $new : $existing_name;
-                    if (! $collection->contains($what)) {
-                        $this->logger->debug(sprintf('DID NOT add defendant name %s to event %d',$what,$event_id));
-                        //$collection->add($what);
-                        $result['event_modification_timestamp'] = $event_entity->getModified()->format("Y-m-d H:i:s");
-                    } else {
-                        $this->logger->debug(sprintf('defendant name %s already on event %d?',$what,$event_id));
-                    }
-                    $this->logger->debug("see what happens");
-                } else {
-                    $this->logger->debug("you say $event_id was NOT among the affected? we didn't do shit");
-                    // do this anyway just for now
-                    $result['event_modification_timestamp'] = $event_entity->getModified()->format("Y-m-d H:i:s");
-                }
-
-            }
+            
             try {
                 $em->flush();
                 $return = array_merge($result,[
