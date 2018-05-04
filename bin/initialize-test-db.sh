b733817bd50215e52a795a4e9c7a72d2ac031c3f
David Mintz <david@davidmintz.org>
2018-05-04 13:10:48 -0400

    try to see the database version running on travis

--- bin/initialize-test-db.sh
+++ bin/initialize-test-db.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-mysql -u root test_office < sql/doctrine-mysql-create-tables.sql 
+mysql -u root test_office < sql/doctrine-mysql-create-tables.sql
+echo -n "database version: "
+echo 'SELECT VERSION();'|mysql -s
