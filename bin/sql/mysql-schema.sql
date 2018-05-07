42309fd369f61c632c2094059028aacf07f6f705
David Mintz <david@davidmintz.org>
2018-05-07 16:08:42 -0400

    remove admin_users from mysql-schema (again)

--- bin/sql/mysql-schema.sql
+++ bin/sql/mysql-schema.sql
@@ -526,4 +526,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-30 13:00:47
+-- Dump completed on 2018-05-07 14:11:55
