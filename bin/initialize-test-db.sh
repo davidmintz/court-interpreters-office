0a04368ed808fa7c09e664aeb412ed27c16215ee
David Mintz <david@davidmintz.org>
2018-05-07 13:05:54 -0400

    Revert "try to see the database version running on travis"
    This reverts commit b733817bd50215e52a795a4e9c7a72d2ac031c3f.

--- bin/initialize-test-db.sh
+++ bin/initialize-test-db.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-mysql -u root test_office < sql/doctrine-mysql-create-tables.sql
-echo -n "database version: "
-echo 'SELECT VERSION();'|mysql -s
+mysql -u root test_office < sql/doctrine-mysql-create-tables.sql 
