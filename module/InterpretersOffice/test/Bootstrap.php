bf99ac62a0a4417faa10a2be46f7840bc5fea828
David Mintz <david@davidmintz.org>
2018-05-07 13:15:08 -0400

    Revert "rename EventTypesControllerTest to disable it; test Bootstrap checks database driver"
    This reverts commit b6f9657014da16bafe461a84eba5fcbffa52c545.

--- module/InterpretersOffice/test/Bootstrap.php
+++ module/InterpretersOffice/test/Bootstrap.php
@@ -4,7 +4,76 @@ namespace ApplicationTest;
 
 class Bootstrap
 {
-    // maybe later
+
+    /**
+     * entity manager
+     *
+     * @var \Doctrine\ORM\EntityManager
+     */
+    private static $entityManager;
+
+    /**
+     * fixture executor
+     *
+     * @var \Doctrine\Common\DataFixtures\Executor\ORMExecutor
+     */
+    private static $fixtureExecutor;
+
+
+    /**
+     * gets entity manager
+     *
+     * @return \Doctrine\ORM\EntityManager
+     */
+    static public function getEntityManager()
+    {
+        if (self::$entityManager) {
+            return self::$entityManager;
+        }
+        $em = require(__DIR__.'/config/bootstrap.php');
+        self::$entityManager = $em;
+
+        return $em;
+    }
+
+    /**
+     * gets fixture manager
+     *
+     * @return \Doctrine\Common\DataFixtures\Executor\ORMExecutor
+     */
+    static public function getFixtureExecutor()
+    {
+        if (self::$fixtureExecutor) {
+            return self::$fixtureExecutor;
+        }
+        $purger = new ORMPurger();
+        $executor = new ORMExecutor(self::getEntityManager(), $purger);
+        self::$fixtureExecutor = $executor;
+        return $executor;
+    }
+
+    /**
+     * loads fixtures
+     *
+     * wraps ORMExecutor::load inside SET FOREIGN_KEY_CHECKS because we could
+     * not come up with any other way to beat the foreign key constraint errors
+     * brought about by Location entity's self-referencing foreign keys
+     *
+     * @var \Doctrine\Common\DataFixtures\FixtureInterface[]
+     *
+     * @return void
+     */
+    static public function load(Array $fixtures)
+    {
+        $executor = self::getFixtureExecutor();
+        $em = self::getEntityManager();
+        $pdo = $em->getConnection()->getWrappedConnection();
+        $pdo->exec('SET FOREIGN_KEY_CHECKS = 0');
+        $executor->execute($fixtures);
+        $pdo->exec('SET FOREIGN_KEY_CHECKS = 1');
+
+    }
+
 }
 putenv('APP_ENV=testing');
 /*
