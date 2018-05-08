ef365bd6a809ff5becb8aa4bb04084b2eeb84b4c
David Mintz <david@davidmintz.org>
2018-05-08 16:04:49 -0400

    add mysql|sqlite test db config files

---
+++ module/InterpretersOffice/test/config/autoload/doctrine-mysql.test.php
@@ -0,0 +1,20 @@
+<?php
+return [
+
+    'doctrine' => [
+        'connection' => [
+            // default connection name
+            'orm_default' => [
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => [
+                    'user' => 'travis',
+                    'password' => '',
+                    'dbname' => 'test_office',
+                    'driver' => 'pdo_mysql',
+                    'host' => 'localhost',
+                    'port' => '3306',
+                ],
+            ],
+        ],
+    ],
+];
