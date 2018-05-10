95cd449319f179aefe65c5d56eadcd3165a29775
David Mintz <david@davidmintz.org>
2018-05-10 15:40:01 -0400

    more assertions in  casper event-form test

--- bin/test-event-form.js
+++ bin/test-event-form.js
@@ -45,69 +45,78 @@ casper.test.begin("Authenticate and Add Event", function suite(test)
             this.click("td a.ui-state-highlight");
             this.waitWhileVisible("#ui-datepicker-div");
             var event_date = this.getFormValues("#event-form")["event[date]"];
-            //var moment;
             var expected = this.evaluate(function(){
                 return moment().format("MM/DD/YYYY");
             });
             test.assertEquals(event_date, expected, "today's date is in the date field");
-            test.assertExists("#event-type","event-type element exists");
-            var judge_name = this.evaluate(
-                function(){return $("#judge option:contains('Forrest')").text();}
-            );
-            test.assert(typeof judge_name === "string" && judge_name.length > 0,
-                "judge name was found: "+judge_name);
-            var sentence = this.evaluate(
-                function(){ return $("#event-type option:contains('sentence')").text(); }
-            );
-            test.assert(sentence === "sentence","event-type option \"sentence\" was found");
-            this.fillSelectors(
-                "#event-form",{
-                    "#event-type":sentence,
-                    "#judge": judge_name
-                }
-            );
-            test.assertExists("#docket","docket element exists");
-            this.sendKeys("#docket","18cr4321");
-            this.waitFor(function(){
-                return this.evaluate(function(){return $("#docket").val() != "18cr4321";});
-            });
-            var docket = this.getFormValues("#event-form")["event[docket]"];
-            test.assertEquals(docket,"2018-CR-4321","docket is automatically formatted as expected");
-            test.assertExists("#location","location element exists");
-            test.assertExists("#parent_location","parent location element exists");
-            this.waitFor(function(){
-                return this.evaluate(function(){return $("#parent_location").val() != "";});
-            });
-            var courthouse = this.evaluate(function(){
-                return $("#parent_location option:selected").text();
-            });
-            test.assertEquals(courthouse,"500 Pearl","courthouse is automatically selected when a judge and in-court event are set");
-            var initial_location_count = this.evaluate(
-                function() { return $("#location option").length; }
-            );
-            test.assertEquals(1,initial_location_count,"there is initially only one option in location select");
-            this.waitFor(function check()  {
-                return this.evaluate(function(){
-                    return $("#location option").length > 10;
-                });
-            },function then() {
-                var location_option_count = this.evaluate(function(){
-                    return $("#location option").length;
-                });
-                test.assert(location_option_count > 10,
-                    "there are now more than 10 location options");
-                var courtroom = this.evaluate(function(){
-                    return $("#location option:selected").text();
-                });
-                // courtroom names look like "23B" or " 706"
-                var re = /^\d{2}(?:[A-D]|\d)$/;
-                test.assert( 1 === courtroom.match(re).length,
-                    "courtroom is automatically selected when judge and in-court event are set");
-            });
+
         });
-        
     /**
-     * set language to Spanish and assign an interpreter
+     * set the event type and judge, and test automatic setting
+     * of judge's default courthouse and courtroom. test automatic
+     * docket formatting
+     */
+    casper.then(function(){
+
+        test.assertExists("#event-type","event-type element exists");
+        var judge_name = this.evaluate(
+            function(){return $("#judge option:contains('Forrest')").text();}
+        );
+        test.assert(typeof judge_name === "string" && judge_name.length > 0,
+            "judge name was found: "+judge_name);
+        var sentence = this.evaluate(
+            function(){ return $("#event-type option:contains('sentence')").text(); }
+        );
+        test.assert(sentence === "sentence","event-type option \"sentence\" was found");
+        var initial_location_count = this.evaluate(
+            function() { return $("#location option").length; }
+        );
+        test.assertEquals(1,initial_location_count,"there is initially only one option in location select");
+
+        this.fillSelectors(
+            "#event-form",{
+                "#event-type":sentence,
+                "#judge": judge_name
+            }
+        );
+        test.assertExists("#docket","docket element exists");
+        this.sendKeys("#docket","18cr4321");
+        this.waitFor(function(){
+            return this.evaluate(function(){return $("#docket").val() != "18cr4321";});
+        });
+        var docket = this.getFormValues("#event-form")["event[docket]"];
+        test.assertEquals(docket,"2018-CR-4321","docket is automatically formatted as expected");
+        test.assertExists("#location","location element exists");
+        test.assertExists("#parent_location","parent location element exists");
+        this.waitFor(function(){
+            return this.evaluate(function(){return $("#parent_location").val() != "";});
+        });
+        var courthouse = this.evaluate(function(){
+            return $("#parent_location option:selected").text();
+        });
+        test.assertEquals(courthouse,"500 Pearl","courthouse is automatically selected when a judge and in-court event are set");
+        this.waitFor(function check()  {
+            return this.evaluate(function(){
+                return $("#location option").length > 10;
+            });
+        },function then() {
+            var location_option_count = this.evaluate(function(){
+                return $("#location option").length;
+            });
+            test.assert(location_option_count > 10,
+                "there are now more than 10 location options");
+            var courtroom = this.evaluate(function(){
+                return $("#location option:selected").text();
+            });
+            // courtroom names look like "23B" or " 706"
+            var re = /^\d{2}(?:[A-D]|\d)$/;
+            test.assert( 1 === courtroom.match(re).length,
+                "courtroom is automatically selected when judge and in-court event are set");
+        });
+    });
+
+    /**
+     * test setting language to Spanish and assignment of an interpreter
      */
     casper.then(function setLanguageAndAssignAnInterpreter() {
 
@@ -120,7 +129,11 @@ casper.test.begin("Authenticate and Add Event", function suite(test)
         this.fillSelectors(
             "#event-form", {"#language":"Spanish" },false
         );
-
+        // sanity check
+        var assigned = this.evaluate(
+            function(){ return $("li.interpreter-assigned").length; }
+        );
+        test.assert(0 === assigned, "no interpreter assigned as yet");
         this.waitFor(function check(){
             return this.evaluate(function() {
                 return $("#interpreter-select option").length > 10;}
@@ -153,8 +166,23 @@ casper.test.begin("Authenticate and Add Event", function suite(test)
                     "element li.interpreter-assigned contains string \""+name+"\"");
             });
         });
-        /** add a defendant name */
+
+        /** test adding a defendant name */
         casper.then(function addDefendantName(){
+
+            test.assertExists("#defendant-search","there is a defendant-search element");
+            this.sendKeys("#event-form #defendant-search","rodr, j", {keepFocus: true});
+            this.waitUntilVisible("ul.ui-autocomplete")
+            .then(function(){
+                this.click("ul.ui-autocomplete li:nth-child(3)");
+            })
+            .then(function(){
+                this.waitUntilVisible("#defendant-names li.defendant");
+            })
+            .then(function(){
+                var name = this.fetchText("#defendant-names li.defendant").trim();
+                this.echo(name);
+            });
 
         });
 
@@ -162,7 +190,8 @@ casper.test.begin("Authenticate and Add Event", function suite(test)
 
 
     });
-    casper.run(function() {  test.done();
-    /*require('utils').dump(this.result.log);*/
+    casper.run(function() {
+        test.done();
+        /*require('utils').dump(this.result.log);*/
     });
 });
