a2a6b90b1512d7c0d46d3f64464d043ad0ceb71c
David Mintz <david@davidmintz.org>
2018-05-16 17:40:37 -0400

    tweak event-delete.js

--- public/js/event-delete.js
+++ public/js/event-delete.js
@@ -11,13 +11,13 @@ $(function(){
         }
         console.log("deleting shit...");
         var url = '/admin/schedule/delete/'+event_id;
-        var redirect_url = document.referrer||'/admin/schedule';
+
         $.post(url,"json")
             .success(function(response){
                 if (response.status === "success" ) {
-                    document.location = redirect_url;
+                    document.location = '/admin/schedule';
                 } else {
-                    // deal with it
+                    // to do: deal with it. trigger a global ajax error?
                 }
             });
     });
