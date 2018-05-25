65cfa8bf86f40fdc2eb39faa9935f8c52806d4f5
David Mintz <david@davidmintz.org>
2018-05-25 13:51:47 -0400

    cosmetic changes to a data import script

--- bin/sdny/import-judges-and-courtrooms.php
+++ bin/sdny/import-judges-and-courtrooms.php
@@ -213,14 +213,12 @@ $judge_map = $db->query('select old.judge_id old_id, new.id new_id FROM dev_inte
             ->fetchAll(PDO::FETCH_KEY_PAIR);
 
 /*
-
 INSERT INTO clerks_judges (SELECT u.id, p.id FROM  dev_interpreters.judges j
 JOIN people p ON j.lastname = p.lastname AND j.firstname = p.firstname and
 p.discr = "judge" LEFT JOIN dev_interpreters.clerks_judges cj
 ON j.judge_id = cj.judge_id JOIN dev_interpreters.request_users ru
 ON cj.user_id = ru.id JOIN people p2 ON p2.email = ru.email
 join users u on p2.id = u.person_id);
-Query OK, 251 rows affe
  */
 
 exit(0);
