54647889d4a2fd68b3383bb99626ccc9ba4b45c6
David Mintz <david@davidmintz.org>
2018-06-14 12:18:43 -0400

    add a Registration controller test, tweak Hat fixture loader

---
+++ module/InterpretersOffice/test/Controller/AccountControllerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ApplicationTest\Controller;
+
+use ApplicationTest\AbstractControllerTest;
+use ApplicationTest\FixtureManager;
+use ApplicationTest\DataFixture;
+
+use Zend\Dom\Query;
+
+class AccountControllerTest extends AbstractControllerTest
+{
+
+    protected $data = [
+        'user' => [
+            'person' => [
+                'lastname' => 'Boinker',
+                'firstname' => 'Wank',
+                'hat' => 0,// placeholder
+                'email' => 'wank_boinker@nysd.uscourts.gov',
+            ],
+            'judges' => [],// also a placeholder
+            'password' => 'fuck you',
+            'password-confirm' => 'fuck you',
+            'username' => 'boinker',
+        ],
+
+    ];
+    public function setUp()
+    {
+        parent::setUp();
+        $fixtureExecutor = FixtureManager::getFixtureExecutor();
+        $fixtureExecutor->execute([
+            new DataFixture\LocationLoader(),
+            new DataFixture\HatLoader(),
+            new DataFixture\JudgeLoader(),
+        ]);
+    }
+
+    function testSubmitNewRegistration()
+    {
+
+        $this->dispatch('/user/register');
+        $this->assertResponseStatusCode(200);
+        $this->assertQuery("input#csrf");
+        $dom = new Query($this->getResponse()->getBody());
+        $csrf_element = $dom->execute("input#csrf")->current();
+        $this->assertTrue(is_object($csrf_element));
+        $token = $csrf_element->getAttribute('value');
+        $this->assertTrue(is_string($token));
+
+        // figure out the id of our hat
+        $hat_id = null;
+        $judge_staff = false;
+        $hats = $dom->execute('select#hat > option');
+        $this->assertGreaterThan(3, count($hats));
+        foreach ($hats as $option) {
+            if ('Law Clerk' == $option->textContent) {
+                $hat_id = $option->getAttribute('value');
+                $judge_staff = $option->getAttribute('data-is_judges_staff');
+                break;
+            }
+        }
+        $this->assertTrue((boolean)$judge_staff);
+
+        // choose a judge
+        $judge_id = null;
+        $judges = $dom->execute('select#judge-select > option');
+        foreach($judges as $judge) {
+            if (strstr($judge->nodeValue,'Dinklesnort')) {
+                $judge_id = $judge->getAttribute('value');$this->reset(true);
+        $this->getRequest()->getHeaders()
+            ->addHeaderLine('X-Requested-With','XMLHttpRequest');
+                break;
+            }
+        }
+        $this->assertTrue(is_string($judge_id));
+        $this->assertGreaterThan(0,(int)$judge_id);
+
+        $this->reset(true);
+        $this->getRequest()->getHeaders()
+            ->addHeaderLine('X-Requested-With','XMLHttpRequest');
+        $post = $this->data;
+        $post['csrf'] = $token;
+        $post['user']['person']['hat'] = $hat_id;
+        $post['user']['judges'][] = $judge_id;
+        $post['user']['id']='';
+        $this->dispatch('/user/register','POST',$post);
+        // to be continued
+        //$this->dumpResponse();
+
+    }
+
+}
--- module/InterpretersOffice/test/DataFixture/HatLoader.php
+++ module/InterpretersOffice/test/DataFixture/HatLoader.php
@@ -34,24 +34,26 @@ class HatLoader implements FixtureInterface
 
         // create the Hat entities: name, can_be_anonymous, role
         $hats = [
-            ['AUSA', 2, null],
-            ['contract court interpreter', 0, null],
-            ['Courtroom Deputy', 0, $submitter],
-            ['defense attorney', 2, null],
-            ['Law Clerk', 0, $submitter],
-            ['paralegal', 2, null],
-            ['Pretrial Services Officer', 0, $submitter],
-            ['staff court interpreter', 0, $submitter],
-            ['Interpreters Office staff', 0, $manager],
-            ['staff, US Attorneys Office', 2, null],
-            ['USPO', 0, $submitter],
-            ['Magistrates', 1, null],
-            ['Pretrial', 1, null],
-            ['Judge', 0, null],
+            ['AUSA', 2, null,false],
+            ['contract court interpreter', 0, null,false],
+            ['Courtroom Deputy', 0, $submitter,true],
+            ['defense attorney', 2, null,false],
+            ['Law Clerk', 0, $submitter,true],
+            ['paralegal', 2, null,false],
+            ['Pretrial Services Officer', 0, $submitter,false],
+            ['staff court interpreter', 0, $manager,false],
+            ['Interpreters Office staff', 0, $manager,false],
+            ['staff, US Attorneys Office', 2, null, false],
+            ['USPO', 0, $submitter,false],
+            ['Magistrates', 1, null,false],
+            ['Pretrial', 1, null,false],
+            ['Judge', 0, null,false],
         ];
         foreach ($hats as $hat) {
+
             $entity = new Entity\Hat();
-            $entity->setName($hat[0])->setAnonymity($hat[1]);
+            $entity->setName($hat[0])->setAnonymity($hat[1])
+                ->setIsJudgeStaff($hat[3]);
             if ($hat[2]) {
                 $entity->setRole($hat[2]);
             }
