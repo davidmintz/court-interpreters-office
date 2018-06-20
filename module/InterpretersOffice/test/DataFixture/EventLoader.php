b9439d2f48960c0b7a2a0b7ec262e9be197908ff
David Mintz <david@davidmintz.org>
2018-06-20 11:37:35 -0400

    trying to recover from efforts to revert

--- module/InterpretersOffice/test/DataFixture/EventLoader.php
+++ module/InterpretersOffice/test/DataFixture/EventLoader.php
@@ -43,7 +43,6 @@ class EventLoader implements FixtureInterface
         $collection = new ArrayCollection([
             (new Entity\InterpreterEvent($interpreter, $event))->setCreatedBy($user),
         ]);
-        $de = new Entity\DefendantEvent($defendant,$event);
         $event
             ->setDate($date)
             ->setTime($time)
@@ -61,13 +60,12 @@ class EventLoader implements FixtureInterface
             ->setModifiedBy($user)
              ->setSubmissionDate(new \DateTime('-1 hour'))
              ->setSubmissionTime(new \DateTime('-1 hour'))
-             ->addDefendantsEvent($de)
+             ->addDefendant($defendant)
              ->addInterpreterEvents(
                  $collection
              );
 
         $objectManager->persist($event);
-        $objectManager->persist($de);
         $objectManager->flush();
 
         //$objectManager->remove($event);
