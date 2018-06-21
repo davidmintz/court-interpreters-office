7e74c4e3d820e8daa29eb8008be0658594e7aa1d
David Mintz <david@davidmintz.org>
2018-06-21 11:22:43 -0400

    add stub AccountManager service class

---
+++ module/InterpretersOffice/src/Service/AccountManager.php
@@ -0,0 +1,23 @@
+<?php
+/* module/InterpretersOffice/src/Service/AccountManager.php */
+
+namespace InterpretersOffice\Service;
+
+use Zend\Mail;
+use Zend\View\ViewModel;
+
+/**
+ * manages user account service
+ */
+class AccountManager
+{
+
+    public function __construct()
+    {
+
+
+
+    }
+
+
+}
