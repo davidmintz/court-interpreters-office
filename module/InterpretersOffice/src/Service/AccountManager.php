3614c33d9da4f5f140a9ebaacfc337296872c681
David Mintz <david@davidmintz.org>
2018-06-29 17:06:19 -0400

    twiddle with AccountManager service

--- module/InterpretersOffice/src/Service/AccountManager.php
+++ module/InterpretersOffice/src/Service/AccountManager.php
@@ -21,9 +21,10 @@ use Zend\Mime\Message as MimeMessage;
 use Zend\Mime\Mime;
 use Zend\Mime\Part as MimePart;
 
-use Zend\View\Renderer\PhpRenderer;
+
 use Zend\View\Renderer\RendererInterface as Renderer;
 
+use Doctrine\ORM\Query\ResultSetMappingBuilder;
 
 /**
  * manages user account service
@@ -83,9 +84,11 @@ class AccountManager implements LoggerAwareInterface
         $log->info(get_class($event).": new user registration has been submitted for: "
             .$user->getUsername());
 
-        $opts = new $this->config['transport_options']['class'](
-            $this->config['transport_options']['options']);
-        $transport = new $this->config['transport']($opts);
+        /** @var Entity\VerificationToken $token */
+        $token = $this->createVerificationToken($user);
+        $this->purge($token->getId());
+        $this->objectManager->persist($token);
+
         $view = (new ViewModel(['person'=>$user->getPerson()]))
             ->setTemplate('interpreters-office/email/layout.tidy.phtml');
 
@@ -108,6 +111,9 @@ class AccountManager implements LoggerAwareInterface
         $contentTypeHeader = $message->getHeaders()->get('Content-Type');
         $contentTypeHeader->setType('multipart/alternative');
 
+        $opts = new $this->config['transport_options']['class'](
+            $this->config['transport_options']['options']);
+        $transport = new $this->config['transport']($opts);
         $transport->send($message);
 
         //*/
@@ -121,8 +127,69 @@ class AccountManager implements LoggerAwareInterface
         //$view = new ViewModel();
         //$view->setTemplate('interpreters-office/email/user_registration.phtml');
         //$layout->addChild($view);
+        //
+        // for argument's sake
+        //  $result = $this->verify($token->getId());
+        // printf("\nshit is a: %s\n",
+        //      gettype($result)
+        //  );
     }
 
+    // not working... yet
+    public function verify($hash)
+    {
+        printf("\nshit says: $hash\n");
+        $db = $this->objectManager->getConnection();
+        //  $db-> ...
+        // http://doctrine-orm.readthedocs.org/projects/doctrine-orm/en/latest/reference/native-sql.html
+        $rsm = new ResultSetMappingBuilder($this->objectManager);
+        $rsm->addRootEntityFromClassMetadata(Entity\Person::class, 'people');
+        // our inheritance scheme is not yet supported?
+        //
+
+
+        //$rsm->addJoinedEntityFromClassMetadata('MyProject\Address', 'a', 'u', 'address', array('id' => 'address_id'));
+        //FROM users u INNER JOIN address a ON u.address_id = a.id";
+        //$rsm->addJoinedEntityFromClassMetadata(Entity\Person::class,'p','u','person',['id'=>'person_id']);
+        // there could be an additional, inactive, "historic" account with the same email. if so, assume
+        // the more recently created is the one we're interested in. hence "ORDER BY... LIMIT"
+        $sql = 'SELECT * FROM people WHERE md5(lower(email)) = :hash
+         ORDER BY id DESC LIMIT 1';
+        $query = $this->objectManager->createNativeQuery($sql,$rsm)->setParameters(['hash'=>$hash]);
+        return $query->getOneorNullResult();
+
+
+
+
+
+    }
+
+    /**
+     * creates a VerificationToken
+     *
+     * @return Entity\VerificationToken
+     */
+    public function createVerificationToken(Entity\User $user)
+    {
+        $token = new Entity\VerificationToken();
+        $id = md5(strtolower($user->getPerson()->getEmail()));
+        $random = bin2hex(openssl_random_pseudo_bytes(16));
+        $hash = password_hash($random,PASSWORD_DEFAULT);
+        $expiration = new \DateTime('+ 30 minutes');
+        $token->setId($id)->setToken($hash)->setExpiration($expiration);
+
+        return $token;
+
+    }
+
+    public function purge($id)
+    {
+        $DQL = 'DELETE InterpretersOffice\Entity\VerificationToken t
+            WHERE t.expiration > CURRENT_TIMESTAMP() OR t.id = :id';
+        $query = $this->objectManager->createQuery($DQL)
+        ->setParameters(['id'=>$id,]);
+        return $query->getResult();
+    }
     /**
      * gets config
      *
