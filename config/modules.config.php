ff4f71ff48ba28392fa736d3c6f805656bf50ef9
David Mintz <david@davidmintz.org>
2018-07-12 15:31:39 -0400

    re-enable Vault module and clean up some stuff in modules.config.php

--- config/modules.config.php
+++ config/modules.config.php
@@ -9,26 +9,18 @@
  * This should be an array of module namespaces used in the application.
  */
 $modules = [
-    //'Zend\Log', // maybe not necessary
     'Zend\Mvc\Plugin\FlashMessenger',
     'Zend\Session',// yes necessary
     'DoctrineModule',
     'DoctrineORMModule',
-    //'Zend\Cache',
     'Zend\Form',// yes necessary
-    //'Zend\InputFilter',
-    //'Zend\Filter',
-    //'Zend\Paginator',
-    //'Zend\Hydrator',
     'Zend\Router',
-    //'Zend\Validator',
     'Zend\Navigation',
     'InterpretersOffice',
     'InterpretersOffice\Admin',
     'InterpretersOffice\Requests',
-
 ];
 if (! getenv('TRAVIS')) {
-//    $modules[] = 'SDNY\Vault';
+    $modules[] = 'SDNY\Vault';
 }
 return $modules;
