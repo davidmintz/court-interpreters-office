b155fbdc76eba8629eacee6a6f04acbe01f2558d
david <david@davidmintz.org>
2018-09-04 21:21:27 -0400

    work on fetch, display admin/court-closings

--- public/js/closings-index.js
+++ public/js/closings-index.js
@@ -8,16 +8,22 @@ var $, moment;
 $(function(){
     $(".closing-link").on("click",function(event){
         event.preventDefault();
-        var href = $(this).attr('href');
-        var list = $(this).parent().children("ul");
+        var link = $(this);
+        var href = link.attr('href');
+        var list = link.parent().children("ul");
+        if (list.is(":visible")) {
+            return list.slideUp();
+        }
         $.getJSON(href,function(data){
             var items  = data.map(obj => {
                 var el = $("<li>").addClass("list-group-item");
-                // to be continued!
-                el.text("shit");
+                var str = obj.date.date.substring(0,10);
+                var date = moment(str,"YYYY-MM-DD").format("dddd MMMM D");
+                var text = obj.holiday ? obj.holiday.name : obj.description_other;
+                el.html(date + ' - ' +  text);
                 return el;
             });
-            list.append(items).show();
+            list.html(items).slideDown();
         });
     });
 });
