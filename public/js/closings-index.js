e801405a0f39b990c8564224b8e5af7042abf50b
David Mintz <david@davidmintz.org>
2018-09-07 15:49:59 -0400

    more work on admin/court-closings

--- public/js/closings-index.js
+++ public/js/closings-index.js
@@ -26,16 +26,23 @@ $(function(){
             list.html(items).slideDown();
         });
     });
+    var datepickerOptions = {
+        changeMonth : true,
+        changeYear : true,
+        selectOtherMonths: true,
+        showOtherMonths: true
+    };
 
     $("#btn-add").on("click",function(){
         $("#form-modal .modal-body").load('/admin/court-closings/add form',
         function() {
             $("#form-label").text("add a court closing");
-            $("#form-modal").modal("show");
-            $("#date").datepicker({});
+            $("#form-modal").modal({ focus : false, backdrop : false });
         });
     });
-
+    $("#form-modal").on("shown.bs.modal",function(){
+        $("#date").datepicker(datepickerOptions);
+    });
 
     $("ul").on("click","li ul li a",function(event){
         event.preventDefault();
@@ -43,8 +50,7 @@ $(function(){
         $("#form-modal .modal-body").load(url +' form',
             function(){
                 $("#form-label").text("edit court closing");
-                $("#form-modal").modal("show");
-                $("#date").datepicker({});
+                $("#form-modal").modal({ focus : false, backdrop : false });
             }
         );
     });
@@ -64,12 +70,43 @@ $(function(){
         var form = $("#form-modal form");
         var url = form.attr("action");
         console.log("submitting to "+url);
+        var action = url.indexOf("add") === -1 ? "updated" : "added";
         $.post(url,form.serialize())
-            .then(function(response){
-                if (response.validation_errors) {
-                    return displayValidationErrors(response.validation_errors);
-                }
-                console.log("saved entity");
+        .fail(function(response){
+            var html = `<p>Sorry, we've encountered an unexpected error.`;
+            if (response.responseJSON) {
+                html += ` The message was: ${response.responseJSON.message}`;
+            }
+            html +=
+                `</p><p>Please consult your site administrator for help</p>`;
+            $("#status").removeClass('alert-success')
+            .addClass('alert-warning').html(html)
+            .show();
+        })
+        .done(function(response)
+        {
+            if (response.validation_errors) {
+                return displayValidationErrors(response.validation_errors);
+            }
+            var when = moment($("#date").datepicker( "getDate" ))
+                        .format("DD-MMM-YYYY");
+            $("#status").html(
+                `<p>The closing for <b>${when}</b> has been successfully ${action}</p>`
+            ).slideDown();
+            $("#form-modal .validation-error").hide();
+            if (action === "updated") {
+                // close the modal automatically
+                window.setTimeout(
+                    function(){$("#form-modal").modal("hide");},3000);
+            } else {
+                // make it easy to continue
+                $("#status").append(
+                    `<p>To add more dates, update the form below and
+                        save again.</p>`
+                );
+            }
+            // to do:  refresh the data in the main listing
+            // add a Delete button and implement
         });
     });
 
