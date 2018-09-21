e9f4ebd31a3b96c52eafd91adb6b7239412359a9
David Mintz <david@davidmintz.org>
2018-09-21 13:22:16 -0400

    fix a mistake in initial location data

--- bin/sdny/more-locations.sql
+++ bin/sdny/more-locations.sql
@@ -18,12 +18,12 @@ SET @PEARL = (SELECT id FROM locations WHERE name = '500 Pearl');
 SET @FOLEY = (SELECT id FROM locations WHERE name = '40 Foley');
 SET @WPLAINS = (SELECT id FROM locations WHERE name = 'White Plains');
 SET @TYPE_COURTROOM = (SELECT id FROM location_types WHERE type = 'courtroom');
-INSERT INTO `locations` VALUES 
+INSERT INTO `locations` VALUES
 (NULL,3,@PEARL,'4th floor cellblock','',1),
 (NULL,8,@PEARL,'cafeteria','',1),
 (NULL,6,@PEARL,'Interpreters Office','room 280',1),
 (NULL,4,@PEARL,'Probation','6th and 7th floors',1),
 (NULL, @TYPE_COURTROOM, @PEARL,'5A','5th floor Magistrate courtroom',1),
 (NULL,5,@PEARL,'Pretrial','5th floor',1),
-(NULL,3,@WPLAINS,'Probation','',1),
+(NULL,4,@WPLAINS,'Probation','',1),
 (NULL,3,@FOLEY,'3rd floor cellblock','',1);
