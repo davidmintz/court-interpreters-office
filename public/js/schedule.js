5c762256bcc764cc297fac679d4534b19381f1c9
David Mintz <david@davidmintz.org>
2018-10-12 16:58:06 -0400

    work on auto-refresh schedule

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -2,7 +2,6 @@
 
 $(function() {
 
-    $('[data-toggle="tooltip"]').tooltip();
     var date_input = $('#date-input');
     var schedule_table = $('#schedule-table');
     /** @todo refactor for DRYness' sake. trigger custom event? */
@@ -41,23 +40,25 @@ $(function() {
     });
 
     // reload every so often
-    var interval = 3 * 1000;
+    var interval = 30 * 1000;
     var timer;
-    var previousData = document.querySelector("#schedule-table").outerHTML;
+    var previous = document.querySelector("#schedule-table").outerHTML.trim();
+    $('[data-toggle="tooltip"]').tooltip();
+
     (function run(){
-        //console.log("starting... ");
+
         timer = window.setTimeout(function(){
-            $.get(document.location.href).done((data)=>{
-                //data = data.trim();
-                if (data !== previousData) {
-                    console.log("shit CHANGED!");
-                    //console.log(data);console.log(previousData);
-                    previousData = data;
+            $.get(document.location.href).done((data, textSatus, jqXHR)=>{
+
+                html = jqXHR.responseText.trim();
+                if (html != previous ) {
+                    console.log("shit CHANGED!?");
                     $("#schedule-table").replaceWith(data);
+                    previous = html;
                 } else {
                     console.log("shit has not changed");
                 }
-                //run();
+                run();
             }).fail(()=> console.warn("shit happened!")
             );
         },interval);
