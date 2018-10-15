f90f45f8dd14631f1b2c6c1c23031d8eff1450da
David Mintz <david@davidmintz.org>
2018-10-15 10:59:59 -0400

    get basic schedule periodic-refresh working

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,5 +1,6 @@
 /**  public/js/schedule.js */
 
+
 $(function() {
 
     var date_input = $('#date-input');
@@ -41,22 +42,24 @@ $(function() {
 
     // reload every so often
     var interval = 30 * 1000;
-    var timer;
-    var previous = document.querySelector("#schedule-table").outerHTML.trim();
+    var previous =  $("#schedule-table").html();
     $('[data-toggle="tooltip"]').tooltip();
 
     (function run(){
 
-        timer = window.setTimeout(function(){
+        window.timer = window.setTimeout(function(){
             $.get(document.location.href).done((data, textSatus, jqXHR)=>{
 
-                html = jqXHR.responseText.trim();
-                if (html != previous ) {
-                    console.log("shit CHANGED!?");
-                    $("#schedule-table").replaceWith(data);
-                    previous = html;
+                //var existing = $("#schedule-table");
+                var new_data = $(data).html();
+                var changed = previous != new_data;//.html();
+                console.log("changed? "+changed);
+                if (changed) {
+                    previous = new_data;
+                    $("#schedule-table").html(new_data);
+                    $('table [data-toggle="tooltip"]').tooltip();
                 } else {
-                    console.log("shit has not changed");
+                    console.log("no change");
                 }
                 run();
             }).fail(()=> console.warn("shit happened!")
@@ -65,3 +68,5 @@ $(function() {
     })()
 
 });
+
+var stop = function(){ window.clearTimeout(timer)};
