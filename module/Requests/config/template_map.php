8bba0c4978773b6cb75edd580cb181c6aa3aec2a
David Mintz <david@davidmintz.org>
2018-10-23 15:45:20 -0400

    use template mapping in Request module, re-organize views

---
+++ module/Requests/config/template_map.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'interpreters-office/requests/index/index' => __DIR__ . '/../view/index/index.phtml',
+    'interpreters-office/requests/index/list' => __DIR__ . '/../view/index/list.phtml',
+    'interpreters-office/requests/index/create' => __DIR__ . '/../view/index/form.phtml',
+    'interpreters-office/requests/index/update' => __DIR__ . '/../view/index/form.phtml',
+    'interpreters-office/requests/index/search' => __DIR__ . '/../view/index/search.phtml',
+    'interpreters-office/requests/index/view' => __DIR__ . '/../view/index/view.phtml',
+];
