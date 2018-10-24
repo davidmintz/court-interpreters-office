265a5568a3d94b40e366a089aebeaa630804bce6
David Mintz <david@davidmintz.org>
2018-10-24 15:54:15 -0400

    work on Request module UI, tests

--- module/InterpretersOffice/test/DataFixture/UserLoader.php
+++ module/InterpretersOffice/test/DataFixture/UserLoader.php
@@ -35,10 +35,11 @@ class UserLoader implements FixtureInterface
                     $objectManager->getRepository('InterpretersOffice\Entity\Hat')
                         ->findOneBy(['name' => 'Law Clerk'])
                 );
-        $another_user->setRole(
-            $objectManager->getRepository('InterpretersOffice\Entity\Role')
-                ->findOneBy(['name' => 'submitter'])
-        )
+
+        $submitter_role = $objectManager->getRepository('InterpretersOffice\Entity\Role')
+            ->findOneBy(['name' => 'submitter']);
+
+        $another_user->setRole($submitter_role)
             ->setActive(true)->setLastLogin(new \DateTime("-24 hours"))
             ->setPerson($person)
             ->setCreated(new \DateTime())
@@ -58,10 +59,7 @@ class UserLoader implements FixtureInterface
                         ->findOneBy(['name' => 'Law Clerk'])
                 );
         $user_account = new Entity\User();
-        $user_account->setRole(
-            $objectManager->getRepository('InterpretersOffice\Entity\Role')
-                ->findOneBy(['name' => 'submitter'])
-        )
+        $user_account->setRole($submitter_role)
             ->setActive(true)->setLastLogin(new \DateTime("-24 hours"))
             ->setPerson($clerk_to_dinklesnort)
             ->setPassword('gack!')->setCreated(new \DateTime())
@@ -69,6 +67,25 @@ class UserLoader implements FixtureInterface
              ->findOneBy(['lastname' => 'Dinklesnort']));
         $objectManager->persist($clerk_to_dinklesnort);
         $objectManager->persist($user_account);
+
+        $other_clerk_to_dinklesnort = new Entity\Person();
+        $other_clerk_to_dinklesnort->setFirstname('Bill')
+                ->setLastname('Dooflicker')
+                ->setEmail('bill_dooflicker@nysd.uscourts.gov')
+                ->setActive(true)
+                ->setHat(
+                    $objectManager->getRepository('InterpretersOffice\Entity\Hat')
+                        ->findOneBy(['name' => 'Courtroom Deputy'])
+                );
+        $objectManager->persist($other_clerk_to_dinklesnort);
+        $other_user_account = (new Entity\User())->setRole($submitter_role)
+            ->setActive(true)->setLastLogin(new \DateTime("-24 hours"))
+            ->setPerson($other_clerk_to_dinklesnort)
+            ->setPassword('gack!')->setCreated(new \DateTime())
+             ->addJudge($objectManager->getRepository('InterpretersOffice\Entity\Judge')
+             ->findOneBy(['lastname' => 'Dinklesnort']));
+             
+        $objectManager->persist($other_user_account);
         $objectManager->flush();
 
         //$another_user->setPassword("something else");
