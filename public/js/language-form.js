cdb3d617359b01bab845972a0c1cb00d063ed351
David Mintz <david@davidmintz.org>
2018-10-24 15:52:52 -0400

    work on language entity deletion in Admin module

--- public/js/language-form.js
+++ public/js/language-form.js
@@ -1,4 +1,22 @@
 /**  public/js/language-form.js */
+
+var fail = function(response){
+    var message, json = response.responseJSON;
+    if (json && json.error && json.error.message) {
+        message = json.error.message;
+    } else {
+        message = "Sorry -- there's been an error that prevented us from completing this operation.";
+    }
+    if (! $('#error-message').length) {
+        $('<div/>')
+            .addClass("alert alert-warning shadow-sm border")
+            .attr({id:"error-message"}).html(message)
+            .insertBefore($("form").first();
+    } else {
+        $('#error-message').html(message);
+    }
+};
+
 $(function(){
     $('#btn-delete').on("click",function(event){
         event.preventDefault();
@@ -8,22 +26,8 @@ $(function(){
         var name = $('form#language').data('language_name');
         var url = $('form#language').data('redirect_url');
         var id = $('input[name="id"]').val();
-        $.post('/admin/languages/delete/'+id,{name:name},function(response){
-            if (response.redirect) {
-                // back to index page
-                document.location = url;
-            } else {
-                // stay here and display error
-                var error = response.error.message;
-                if (! $('#failed_deletion_error').length) {
-                    $('<div/>')
-                        .addClass("alert alert-warning")
-                        .attr({id:"failed_deletion_error"}).html(error)
-                        .insertBefore($('form#language'));
-                } else {
-                    $('#failed_deletion_error').html(error);
-                }
-            }
-        },'json');
-    });
+        $.post('/admin/languages/delete/'+id,{name:name})
+            .done(function(response){document.location = url;})
+            .fail(fail);
+        });
 });
