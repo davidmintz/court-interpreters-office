895a97cd9a43438e7e5fa0d0e41780c8df0b220d
david <david@davidmintz.org>
2018-10-26 13:52:06 -0400

    cs fixes

--- module/Admin/src/Controller/Factory/BasicEntityControllerFactory.php
+++ module/Admin/src/Controller/Factory/BasicEntityControllerFactory.php
@@ -1,5 +1,4 @@
 <?php
-
 /** module/InterpretersOffice/src/Controller/Factory/BasicEntityControllerFactory.php */
 
 namespace InterpretersOffice\Admin\Controller\Factory;
@@ -27,12 +26,13 @@ class BasicEntityControllerFactory implements FactoryInterface
      *
      * @return Zend\Mvc\Controller\AbstractActionController
      */
-    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    public function __invoke(ContainerInterface $container, $requestedName,
+        array $options = null)
     {
         $basename = substr($requestedName, strrpos($requestedName, '\\') + 1);
-        $shortName = strtolower((new Filter)->filter(substr($basename, 0, -10)));
-        // $shortName is for identifying cache id/namespace and maybe composing path
-        // to a form.phtml viewscript
+        $shortName = strtolower((new Filter)
+            ->filter(substr($basename, 0, -10)));
+        // $shortName is for identifying cache id/namespace
         /**
          * @todo rethink this whole plan
          */
@@ -42,16 +42,21 @@ class BasicEntityControllerFactory implements FactoryInterface
             case 'event-types':
                 $factory = $container->get('annotated-form-factory');
                 $entityManager = $container->get('entity-manager');
-                $controller = new $requestedName($entityManager, $factory, $shortName);
+                $controller = new $requestedName($entityManager, $factory,
+                    $shortName);
                 break;
             case 'court-closings':
-                $controller = new $requestedName($container->get('entity-manager'));
+                $controller = new $requestedName(
+                        $container->get('entity-manager'));
                 break;
             default:
-                throw new \RuntimeException("controller factory cannot not instantiate $requestedName a/k/a $shortName");
+                throw new \RuntimeException(
+                    'controller factory cannot not instantiate ' .
+                    "$requestedName a/k/a $shortName");
         }
         // ensure UpdateListener knows who current user is
-        $container->get(Listener\UpdateListener::class)->setAuth($container->get('auth'));
+        $container->get(Listener\UpdateListener::class)
+            ->setAuth($container->get('auth'));
 
         return $controller;
     }
