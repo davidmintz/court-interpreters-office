b3adfe2a5a9b948b36468c314cf413edbb873424
David Mintz <david@davidmintz.org>
2018-11-01 16:06:00 -0400

    Request Module.php attach a listener to EventForm's pre.populate event

--- module/Requests/src/Module.php
+++ module/Requests/src/Module.php
@@ -30,8 +30,8 @@ class Module
         /**
          * the pre.populate event is triggered in EventsController
          * edit action, after the entity is fetched and bound to the
-         * form but before $form->setData($post). The EventForm needs to
-         * be informed as to whether the Event was originally set from a
+         * form but before $form->setData($post). We inform the Event form
+         * as to whether the Event was originally set from a
          * Request entity.
          *
         */
@@ -39,11 +39,8 @@ class Module
             'InterpretersOffice\Admin\Controller\EventsController',
             'pre.populate',
             function($e) use ($log, $em){
-                $log->debug(sprintf(
-                    'running "pre.populate" event listener in %s',__CLASS__
-                ));
+
                 $entity_id = $e->getParams()['entity']->getId();
-                //var_dump(Entity\Request::class);
                 $request = $em->createQuery(
                     'SELECT r.id FROM '. Entity\Request::class
                     . ' r JOIN r.event e WHERE e.id = :event_id')
@@ -52,6 +49,9 @@ class Module
                 if ($request) {
                     $form = $e->getParams()['form'];
                     $form->setElectronic(true);
+                    $log->debug(sprintf(
+                        'set shit = TRUE in "pre.populate" event listener in %s',__CLASS__
+                    ));
                 }
             },200
         );
