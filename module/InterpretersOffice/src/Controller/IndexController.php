61ad4990a9fda6e00befa77875e41b3fe1379823
David Mintz <david@davidmintz.org>
2018-11-02 14:33:12 -0400

    (experimental) render local custom content  within main index.phtml

--- module/InterpretersOffice/src/Controller/IndexController.php
+++ module/InterpretersOffice/src/Controller/IndexController.php
@@ -29,6 +29,7 @@ class IndexController extends AbstractActionController
      */
     public function indexAction()
     {
+        $router = $this->getEvent()->getRouter();        
         return new ViewModel();
     }
 
