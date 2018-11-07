ffa14632e6d266e41f05b130b3276bd3a5e14d18
David Mintz <david@davidmintz.org>
2018-11-07 16:47:38 -0500

    revise ideas about Request event-handling configuration

--- module/Requests/src/View/Helper/ConfigCheckbox.php
+++ module/Requests/src/View/Helper/ConfigCheckbox.php
@@ -8,15 +8,15 @@ class ConfigCheckbox extends AbstractHelper
     private $template = <<<TEMPLATE
     <div class="form-row form-check">
         <input type="checkbox" %s id="%s" name="%s" value="1" class="form-check-input">
-        <label for="%s" class="form-check-label" >%s</label>
+        <label for="%s" class="form-check-label text-left" >%s</label>
     </div>
 
 TEMPLATE;
 
-    public function __invoke($event,$interp_type,$action, $checked = true, $label = null)
+    public function __invoke($user_event,$language_type,$event_type,$action, $checked = true, $label = null)
     {
-        $id =  "{$event}.{$interp_type}.{$action}";
-        $element_name = "{$event}[{$interp_type}][{$action}]";
+        $id =  "{$user_event}.{$event_type}.{$language_type}.{$action}";
+        $element_name = "{$user_event}[${event_type}][{$language_type}][{$action}]";
         if (! $label) {
             $label = str_replace('-',' ',$action);
         }
