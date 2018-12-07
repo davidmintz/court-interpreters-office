01dd330e2ccfd12ae4d8ebcaf8f4d574d7720cf9
David Mintz <david@davidmintz.org>
2018-12-07 10:46:15 -0500

    update Request's ConfigFieldset

--- module/Requests/src/Form/ConfigFieldset.php
+++ module/Requests/src/Form/ConfigFieldset.php
@@ -8,9 +8,21 @@ use Zend\Hydrator\ArraySerializable;
 use Zend\InputFilter\InputFilterProviderInterface;
 use Zend\Form\Fieldset;
 
-
+/**
+ * fieldset for requests-configuration page
+ */
 class ConfigFieldset extends Fieldset implements InputFilterProviderInterface
 {
+    /**
+     * constructor
+     *
+     * contructor options get passed on up to the parent except for
+     * $options['config'], which supplies values we need to initialize
+     * our checkbox element(s).
+     *
+     * @param string $name
+     * @param array  $options
+     */
     public function __construct($name = null, $options = [])
     {
         parent::__construct($name, $options);
@@ -27,7 +39,6 @@ class ConfigFieldset extends Fieldset implements InputFilterProviderInterface
                     ],
                     'options' => [
                         'label' => str_replace('-',' ',$label),
-                        //'value' => $value,
                         'use_hidden_element' => true,
                         'checked_value' => 1,
                         'unchecked_value' => 0,
@@ -36,6 +47,14 @@ class ConfigFieldset extends Fieldset implements InputFilterProviderInterface
             );
         }
     }
+
+    /**
+     * gets input filter specification
+     *
+     * doesn't do anything as of yet.
+     *
+     * @return array
+     */
     public function getInputFilterSpecification()
     {
         return [];
