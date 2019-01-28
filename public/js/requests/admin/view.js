e0ec8b910780850e70f907f677cb5b6fc9674688
David Mintz <david@davidmintz.org>
2019-01-28 16:20:31 -0500

    add a couple new files

---
+++ public/js/requests/admin/view.js
@@ -0,0 +1,19 @@
+var moment, schedule_request_callback, show_error_message;
+$(function(){
+    $("#btn-schedule-request").on("click",function(){
+        var id = $(this).data().request_id;
+        $.post(`/admin/requests/schedule/${id}`)
+        .then((response) =>{
+            if (response.status === "success") {
+                schedule_request_callback(response);
+                $("#request-status").text("scheduled");
+            }
+            if (response.status === "error") {
+                show_error_message(response);
+                if (response.message.match(/already.*schedule/i)) {                    
+                    $(this).remove();
+                }
+            }
+        }).fail(fail);
+    });
+});
