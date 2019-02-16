dffb7aaabf8d302f60b57f9620980fb40c43887b
David Mintz <david@davidmintz.org>
2019-02-15 22:53:16 -0500

    revise judge-scraper.js

--- bin/sdny/judge-scraper.js
+++ bin/sdny/judge-scraper.js
@@ -1,104 +1,64 @@
-/**
- * effort to write a screen scraper with node for SDNY judges,
- * still haven't entirely mastered this asynchronous thing
- */
 
 const axios = require("axios");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+
 var data = {USMJ: {}, USDJ: {}};
 
-const parseJudgeData = function(elements){
-    var courthouse, courtroom, match;
-    elements.forEach(function(el){
-        var text = el.textContent.trim();
-        match = text.match(/courtroom:? *(\S+)\s+/i);
-        if (match) {
-            courtroom = match[1];
-        }
-        match = text.match(/(500 Pearl|40 Foley|White Plains)/);
-        if (match) {
-            courthouse = match[1];
-        }
-    });
-    return { courthouse, courtroom };
-};
-
-axios.get("http://nysd.uscourts.gov/judges/District")
-.then(function(response){
-     var dom = new JSDOM(response.data);
-     var links = dom.window.document.querySelectorAll("table.judge_info tr td a");
-     return links;
- })
-.then(function(links){
-     var num_links = links.length;
-     for (var i = 0; i < num_links; i++) {
-        var el = links[i];
-        (function(el){
-            //console.log(`fetching ${el.href} for ${el.textContent}...${i} of ${links.length}`);
-            return axios.get(el.href)
-            .then(function(response){
-                var name = el.textContent.trim();
-                var doc = new JSDOM(response.data).window.document;
-                var elements = doc.querySelectorAll(".mainblock table tr.whiteback td table tr td p");
-                data.USDJ[name] = parseJudgeData(elements);
-                if (Object.keys(data.USDJ).length === links.length) {
-                    console.log(JSON.stringify(data));
-                    return data;
-                }
+const get_judge_links = function(flavor){
+    return new Promise(
+        resolve => {
+            axios.get(`http://nysd.uscourts.gov/judges/${flavor}`)
+            .then((response) => {
+                var dom = new JSDOM(response.data);
+                var links = dom.window.document.querySelectorAll("table.judge_info tr td a");
+                resolve(links);
             })
-            // .then((data)=>{
-            //     if (data){
-            //         console.log(JSON.stringify(data));
-            //     }
-            // })
-            .catch(function(err){console.log(err)});
-        })(el);
-     }
- })
-.catch(function(err){console.log(err)});
-
-// and do it again for Magistrates?...
-/*
-axios.get("http://nysd.uscourts.gov/judges/Magistrate")
-.then(function(response){
-     var dom = new JSDOM(response.data);
-     var links = dom.window.document.querySelectorAll("table.judge_info tr td a");
-     data.USMJ._total = links.length;
-     return links;
- })
-.then(function(links){
-     for (i = 0; i < links.length; i++) {
-         var el = links[i];
-        (function(el,i){
-            return axios.get(el.href)
-            //console.log(`fetching ${el.href} for ${el.textContent}...`);
-            .then(function(response){
-                var name = el.textContent.trim();
-                var doc = new JSDOM(response.data).window.document;
-                var elements = doc.querySelectorAll(".mainblock table tr.whiteback td table tr td p");
-                //console.log(`name is: ${name}`);
-                data.USMJ[name] = {};
+            .catch((error)=>{console.warn("oops"); console.log(error)});
+        },
+        reject => {console.warn("oops")}
+    );
+};
+const parse_judge_info = function(url){
+    return new Promise(
+        resolve => {
+            axios.get(url).then((response)=>{
+                var dom = new JSDOM(response.data);
+                var elements = dom.window.document
+                    .querySelectorAll(".mainblock table tr.whiteback td table tr td p");
+                var courthouse, courtroom, match;
                 elements.forEach(function(el){
                     var text = el.textContent.trim();
-                    //var courthouse,courtroom;
-                    if (m = text.match(/courtroom:? *(\S+)\s+/i)) {
-                        data.USMJ[name].courtroom = m[1];
-                        //console.log(`courtroom is ${data[name].courtroom}`);
+                    match = text.match(/courtroom:? *(\S+)\s+/i);
+                    if (match) {
+                        courtroom = match[1];
                     }
-                    if (m = text.match(/500 Pearl|40 Foley|White Plains/)) {
-                        data.USMJ[name].courthouse = m[1];
+                    match = text.match(/(500 Pearl|40 Foley|White Plains)/);
+                    if (match) {
+                        courthouse = match[1];
                     }
                 });
-                if (i == links.length - 1) {
-                    return data;
-                }
-            })
-            .then((data)=>{
-                //if (data){console.log(data); console.log("done? FOR REAL??")}
+                resolve({courthouse,courtroom});
             });
-        })(el,i);
-     }
- })
-.catch(function(err){console.log(err)});
-*/
+        }
+    );
+};
+
+(async function() {
+
+    var usdj_links = await get_judge_links("District");
+    for (var i = 0; i < usdj_links.length; i++) {
+        var name = usdj_links[i].textContent.trim();
+        var url = usdj_links[i].href;
+        console.log(`fetching ${url} for: ${name}, USDJ`);
+        data.USDJ[name] = await parse_judge_info(url);
+    }
+    var usmj_links = await get_judge_links("Magistrate");
+    for (var i = 0; i < usmj_links.length; i++) {
+        var name = usmj_links[i].textContent.trim();
+        var url = usmj_links[i].href;
+        console.log(`fetching ${url} for: ${name}, USMJ`);
+        data.USMJ[name] = await parse_judge_info(url);
+    }
+    console.log(JSON.stringify(data));
+})();
