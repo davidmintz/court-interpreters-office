f1eb588358890857dd5459ea20126369d0b723bc
David Mintz <david@davidmintz.org>
2019-03-22 10:21:10 -0400

    work on UI for emailing event-details

--- public/js/event-email.js
+++ public/js/event-email.js
@@ -1,4 +1,6 @@
 $(function(){
+    // decide whether to display a suggestion that they send an email
+    // about a noteworthy update
     var email_flag = false;
     if ( $('span.interpreter').length && $("ins, del").length) {
         var noteworthy = ["date","time","type","interpreters","location"];
@@ -12,9 +14,10 @@ $(function(){
     }
     console.log(`email flag? ${email_flag}`);
 
-    $("#btn-email").on("click",function(e){e.preventDefault()});
+    $("#btn-email").on("click",function(e){e.preventDefault();});
+
     $("#email-dialog").on("show.bs.modal",function(event){});
 
-    $(".dropdown-menu .custom-control-input").on("click",function(e){e.preventDefault();})
+    //$(".dropdown-menu .custom-control-input").on("click",function(e){e.preventDefault();})
 
 });
