f3d9354b9f01ece1caee3b0f3728db3c4c3d6156
David Mintz <david@davidmintz.org>
2019-03-22 13:30:36 -0400

    more work on event-email UI

--- public/js/event-email.js
+++ public/js/event-email.js
@@ -12,12 +12,26 @@ $(function(){
             }
         });
     }
-    console.log(`email flag? ${email_flag}`);
+    console.log(`email flag? ${email_flag}`);// to be continued
 
     $("#btn-email").on("click",function(e){e.preventDefault();});
 
-    $("#email-dialog").on("show.bs.modal",function(event){});
+    $("#email-dialog").on("show.bs.modal",function(event){
+    });
+    $("#btn-add-recipients").on("click",function(e){
+        e.preventDefault();
+        if (! $("#email-dropdown input:checked").length) {
+            $("#email-dropdown .validation-error").text(
+                "select at least one recipient"
+            ).show();
+            return false;
+        } else {
+            $("#email-dropdown .validation-error").hide();
+        }
+        console.log("looks good, now do shit...");
+        return true;
 
+    });
     //$(".dropdown-menu .custom-control-input").on("click",function(e){e.preventDefault();})
 
 });
