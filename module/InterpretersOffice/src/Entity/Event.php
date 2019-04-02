4185ee9d3ae90fa4bea16ccfac5540924b51aa04
David Mintz <david@davidmintz.org>
2019-04-02 13:21:36 -0400

    update Event::setLocation to permit null value

--- module/InterpretersOffice/src/Entity/Event.php
+++ module/InterpretersOffice/src/Entity/Event.php
@@ -433,7 +433,7 @@ class Event
      *
      * @return Event
      */
-    public function setLocation(Location $location)
+    public function setLocation(Location $location = null)
     {
         $this->location = $location;
 
