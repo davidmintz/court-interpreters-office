041f2662b9078d6813515fcdbe4e6410f51365bc
David Mintz <david@davidmintz.org>
2019-04-22 12:59:10 -0400

    SQL snippet to un-falsify some event metadata we import

--- bin/sdny/un-falsify.sql
+++ bin/sdny/un-falsify.sql
@@ -1,7 +1,10 @@
-SELECT u.username, h.name hat, e.submitter_id, comments FROM events e JOIN users u
-ON u.id = e.created_by_id JOIN hats h ON e.anonymous_submitter_id = h.id
-AND h.name = "Magistrates" AND e.comments REGEXP '\\bmag(i?s(trates?)?)? +cal'
- ORDER BY e.id DESC LIMIT 125;
-
-
-/* UPDATE events SET .... */
+UPDATE events e
+JOIN hats h
+ON e.anonymous_submitter_id = h.id
+JOIN users u ON u.id = e.created_by_id
+JOIN people p ON u.person_id = p.id
+SET e.submitter_id = p.id,
+e.anonymous_submitter_id = NULL
+WHERE h.name = "Magistrates"
+AND e.submitter_id IS NULL
+AND e.comments REGEXP '([[:<:]]|^)mag(i?s(trates?)?)? +cal(endar)?([[:<:]]|$)';
