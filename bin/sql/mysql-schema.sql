a752f5183491aeb172c73ac5d7ca81738832d004
David Mintz <david@davidmintz.org>
2019-04-22 17:14:06 -0400

    update mysql schema

--- bin/sql/mysql-schema.sql
+++ bin/sql/mysql-schema.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: office
 -- ------------------------------------------------------
--- Server version	5.7.25-0ubuntu0.18.04.2
+-- Server version	5.7.25-0ubuntu0.16.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -199,6 +199,30 @@ CREATE TABLE `event_categories` (
   `category` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_event_category` (`category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `event_emails`
+--
+
+DROP TABLE IF EXISTS `event_emails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `event_emails` (
+  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `event_id` mediumint(8) unsigned DEFAULT NULL,
+  `timestamp` datetime NOT NULL,
+  `user_id` smallint(5) unsigned NOT NULL,
+  `recipient_id` smallint(5) unsigned DEFAULT NULL,
+  `email` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `subject` varchar(250) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `comments` varchar(250) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  KEY `event_id` (`event_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `event_emails_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `event_emails_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -676,4 +700,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-02-28 15:08:10
+-- Dump completed on 2019-04-22 17:13:32
