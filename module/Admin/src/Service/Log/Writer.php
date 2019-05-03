9122ace7b3f1d035073d161a056ab327d0657511
David Mintz <david@davidmintz.org>
2019-05-02 22:13:15 -0400

    add a log writer for application-event INFO messages

---
+++ module/Admin/src/Service/Log/Writer.php
@@ -0,0 +1,82 @@
+<?php /**   */
+
+namespace InterpretersOffice\Admin\Service\Log;
+
+use Zend\Log\Writer\AbstractWriter;
+
+/**
+ * Writer for logging application-event messages
+ * at the INFO level
+ *
+ * ```
+ * $pdo  = new \PDO("mysql:dbname={$params['dbname']}",$params['user'],$params['password']);
+ * $log = new Logger();
+ * $log->addWriter(new Writer(['pdo'=>$pdo]));
+ * $log->info("say shit to my ass",['entity_class'=> "Foo"]);
+ * ```
+ */
+class Writer extends AbstractWriter
+{
+
+    /**
+     * database
+     *
+     * @var \PDO
+     */
+    private $pdo;
+
+    /**
+     * sql
+     *
+     * @var string
+     */
+    private $sql = 'INSERT INTO app_event_log (timestamp, message, entity_id,
+            entity_class, priority, priority_name, extra)
+        VALUES (:timestamp, :message, :entity_id, :entity_class,
+             :priority, :priorityName, :extra)';
+
+    /**
+     * Constructor
+     *
+     * @param  array|Traversable $options
+     * @throws Exception\InvalidArgumentException
+     */
+    public function __construct($options = null)
+    {
+        $this->pdo = $options['pdo'];
+        parent::__construct( $options);
+    }
+
+    /**
+     * implements Zend\Log\Writer\WriterInterface
+     *
+     * @param  Array  $event
+     * @return void
+     */
+    public function doWrite(Array $event)
+    {
+        /** @var $db \PDO */
+        $db = $this->pdo;
+        // move this to a processor class!
+        $timestamp = $event['timestamp']->format('Y-m-d H:i:s');
+        $params = $event;
+        $params['timestamp'] = $timestamp;
+        $extra = $event['extra'];
+        $defaults = ['entity_id'=> null, 'entity_class'=> ''];
+        foreach (['entity_class','entity_id'] as $field) {
+            if (!empty($extra[$field])) {
+                $params["{$field}"] = $extra[$field];
+                unset($params['extra'][$field]);
+            } else {
+                $params[$field] = $defaults[$field];
+            }
+        }
+        if (count($params['extra'])) {
+            $params['extra'] = json_encode($params['extra']);
+        } else {
+            $params['extra'] = '';
+        }
+        $stmt = $db->prepare($this->sql);
+        $stmt->execute($params);
+    }
+}
