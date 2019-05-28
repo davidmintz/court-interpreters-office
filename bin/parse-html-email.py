d3ceade3c186bfdaffe55a9d7eb4a2852352e8d1
David Mintz <david@davidmintz.org>
2019-05-28 13:26:53 -0400

    Python snippet for parsing HTML email body from .eml files created by tests

---
+++ bin/parse-html-email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# possibly useful in tests, for getting html payload from
+# .eml files produced by Zend\Mail
+
+from email import parser;
+import argparse;
+
+args = argparse.ArgumentParser()
+args.add_argument("filename", help="path to .eml file")
+filename = args.parse_args().filename
+data = open(filename)
+msg = parser.Parser().parse(data)
+html = msg.get_payload()[1]
+print(html.get_payload(decode=True))
