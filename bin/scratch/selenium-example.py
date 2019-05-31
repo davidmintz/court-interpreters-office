001abd8aafa76b1a884f8e2f6c47d8285384ec8f
David Mintz <david@davidmintz.org>
2019-05-31 16:22:25 -0400

    play with Selenium/Python

--- bin/scratch/selenium-example.py
+++ bin/scratch/selenium-example.py
@@ -1,27 +1,41 @@
 
-import unittest
+import unittest, time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class PythonOrgSearch(unittest.TestCase):
+class InterpretersOffice(unittest.TestCase):
 
     def setUp(self):
-        options = webdriver.ChromeOptions()
-        options.add_argument('--ignore-certificate-errors')
+        driver = webdriver.Firefox()
+        #options.add_argument('--ignore-certificate-errors')
         #options.add_argument("--test-type")
-        options.binary_location = "/usr/bin/chromium-browser"
-        driver = webdriver.Chrome(options=options)
+        #options.binary_location = "/usr/bin/chromium-browser"
+        #driver = webdriver.Chrome(options=options)
         self.driver = driver
         #self.driver = webdriver.Firefox()
 
-    def test_search_in_python_org(self):
+    # def test_search_in_python_org(self):
+    #     driver = self.driver
+    #     driver.get("http://www.python.org")
+    #     self.assertIn("Python", driver.title)
+    #     elem = driver.find_element_by_name("q")
+    #     elem.send_keys("pycon")
+    #     elem.send_keys(Keys.RETURN)
+    #     assert "No results found." not in driver.page_source
+
+    def test_login_from_main_page(self):
+
         driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+        driver.get("https://office.localhost")
+        element = driver.find_element_by_css_selector("#login button")
+        element.click()
+        self.assertIn("user login",  driver.title)
+        driver.find_element_by_id("identity").send_keys("david")
+        driver.find_element_by_id("password").send_keys("boink")
+
+        button = driver.find_element_by_css_selector("form button[type=submit]")
+        button.click()
+        self.assertNotIn("user login",  driver.title)
 
 
     def tearDown(self):
