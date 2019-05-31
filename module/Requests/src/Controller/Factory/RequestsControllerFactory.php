6b4de5305dc17539b3128710e3f4906f7f281168
David Mintz <david@davidmintz.org>
2019-05-31 14:14:00 -0400

    update comments in RequestsControllerFactory

--- module/Requests/src/Controller/Factory/RequestsControllerFactory.php
+++ module/Requests/src/Controller/Factory/RequestsControllerFactory.php
@@ -18,20 +18,20 @@ use InterpretersOffice\Admin\Service\ScheduleUpdateManager;
 use InterpretersOffice\Admin\Service\Log\Writer as DbWriter;
 
 /**
- * Factory class for instantiating Requests\IndexController.
+ * Factory class for instantiating controllers in Requests module.
  *
  * This factory produces the read-only IndexController as well as the
- * WriteController for the Request module. If the $requestedName (controller)
- * we're creating is the WriteController, we update the ACL (which is mostly
- * configured by now) on the fly, so that we can add an ACL assertion that
- * depends on conditions we can't know until this controller is created, and
- * which is also expensive enough to warrant delaying until we know we
- * need it.
+ * WriteController for the Request module, and the Admin\IndexController.
+ * If the $requestedName (controller) we're creating is the WriteController,
+ * we update the ACL (which is mostly configured by now) on the fly, so that
+ * we can add an ACL assertion that depends on conditions we can't know until
+ * this controller is created, and which is also expensive enough to warrant
+ * delaying until we know we need it.
  */
 class RequestsControllerFactory implements FactoryInterface
 {
     /**
-     * invocation, if you will.
+     * invocation, if you please.
      *
      * @param ContainerInterface $container
      * @param string             $requestedName
@@ -63,7 +63,7 @@ class RequestsControllerFactory implements FactoryInterface
             // add another ACL rule
             $acl = $container->get('acl');
             $controller = new $requestedName($entityManager, $auth, $acl);
-            // @todo optimize this...
+            // @todo optimize this horribly inefficient query...
             $user = $entityManager->find(
                 'InterpretersOffice\Entity\User',
                 $auth->getIdentity()->id
