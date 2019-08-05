2c31c3840724ed789bcc54e8472fde568faa001a
David Mintz <david@davidmintz.org>
2019-08-05 12:37:39 -0400

    dick around with sandbox for experimentation

--- module/Sandbox/src/Controller/IndexController.php
+++ module/Sandbox/src/Controller/IndexController.php
@@ -10,17 +10,18 @@ class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        $em = $this->getEvent()->getApplication()->getServiceManager()->get('entity-manager');
-        //$event = $em->find('InterpretersOffice\Requests\Entity\RepositoryEvent',115656);
-        $repo = $em->getRepository('InterpretersOffice\Requests\Entity\Request');
-        //$request = $repo->getRequestWithEvent(20572);
-        $result = $repo->list_v2();
         $serviceManager = $this->getEvent()->getApplication()->getServiceManager();
-        $auth = $serviceManager->get('auth');
-        $user = $auth->getIdentity();
-        //$entity = $em->find('InterpretersOffice\Entity\User',$user->id);
-        $entity = $em->getRepository('InterpretersOffice\Entity\User')->getUser($user->id);
-        echo $entity->getRole();
+        $em = $serviceManager->get('entity-manager');
+        //$event = $em->find('InterpretersOffice\Requests\Entity\RepositoryEvent',115656);
+        // $repo = $em->getRepository('InterpretersOffice\Requests\Entity\Request');
+        //$request = $repo->getRequestWithEvent(20572);
+
+        // looks like trying to update a read-only entity merely fails
+        // without error
+        $hat = $em->getRepository('InterpretersOffice\Entity\Hat')->find(1);
+        $hat->setName("some other shit");
+        $em->flush();
+
         return (new ViewModel())->setTemplate('sandbox/index/index');
         //$event = $em->getRepository('InterpretersOffice\Entity\Event')->load(115656);
         //$event->getJudge()->getLastname();
