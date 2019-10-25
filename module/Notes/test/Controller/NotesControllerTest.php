2edb46e88a67776ea2adf23a5e97208034953f0f
David Mintz <david@davidmintz.org>
2019-10-25 13:53:04 -0400

    add test assertions for Notes module

--- module/Notes/test/Controller/NotesControllerTest.php
+++ module/Notes/test/Controller/NotesControllerTest.php
@@ -7,6 +7,7 @@ use ApplicationTest\AbstractControllerTest;
 use ApplicationTest\FixtureManager;
 use ApplicationTest\DataFixture;
 use Zend\Stdlib\Parameters;
+use InterpretersOffice\Admin\Notes\Entity\MOTD;
 
 class NotesControllerTest extends AbstractControllerTest
 {
@@ -26,6 +27,17 @@ class NotesControllerTest extends AbstractControllerTest
                 new DataFixture\UserLoader(),
             ]
         );
+    }
+
+    public function tearDown()
+    {
+        $em = FixtureManager::getEntityManager();
+        $motd = $em->createQuery("SELECT n FROM InterpretersOffice\Admin\Notes\Entity\MOTD n ORDER BY n.date DESC" )
+           ->getOneOrNullResult();
+        if ($motd) {
+            $em->remove($motd);
+            $em->flush();
+        }
     }
 
     public function testGetMethodWorksIfUserIsLoggedIn()
@@ -49,5 +61,59 @@ class NotesControllerTest extends AbstractControllerTest
         $this->dispatch('/admin/notes/date/2019-09-12/motd');
         $this->assertResponseStatusCode(200);
     }
+
+    public function testLoadFormToCreateMOTD()
+    {
+        $this->login('david','boink');
+        $this->reset(true);
+        $when = new \DateTime('next Tuesday');
+        $date_str = $when->format('Y-m-d');
+        $this->dispatch('/admin/notes/create/motd/'.$date_str);
+        $this->assertResponseStatusCode(200);
+        $this->assertQueryCount('form textarea',1);
+
+    }
+
+    public function testCreateMOTD()
+    {
+        $em = FixtureManager::getEntityManager();
+        $count_before = $em->getRepository(MOTD::class)->count([]);
+        $when = new \DateTime('next Tuesday');
+        $date_str = $when->format('Y-m-d');
+
+        $url = "/admin/notes/create/motd/$date_str";
+        $this->login('david', 'boink');
+        $this->reset(true);
+        $token = $this->getCsrfToken($url, 'csrf');
+        $content = 'Here is your test message, hope you enjoy it';
+        $this->getRequest()
+            ->setMethod('POST')->setPost(
+            new Parameters([
+                'content' => $content,
+                'type' => 'motd',
+                'date' => $date_str,
+                'id' => '',
+                'csrf' => $token,
+            ])
+        )->getHeaders()->addHeaderLine('X-Requested-With','XMLHttpRequest');
+        $this->dispatch('/admin/notes/create/motd');
+        $this->assertResponseStatusCode(200);
+        $this->assertResponseHeaderRegex('Content-type', '|application/json|');
+        $body = $this->getResponse()->getBody();
+        $data = json_decode($body);
+        $this->assertIsObject($data);
+        $this->assertEquals('success',$data->status);
+        $this->assertIsObject($data->motd);
+        $this->assertObjectHasAttribute('id', $data->motd);
+        $this->assertObjectHasAttribute('created_by', $data->motd);
+        $this->assertEquals('david',$data->motd->created_by);
+        $this->assertIsString($data->motd->content);
+        $this->assertEquals($content,$data->motd->content);
+        $count_after = $em->getRepository(MOTD::class)->count([]);
+        $this->assertEquals($count_after, $count_before + 1);
+
+        return $data->motd; // for a possible future @depends etc
+    }
+
 
 }
