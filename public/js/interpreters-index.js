bf4e59b42e377cbbc416007ccd4e1e83afa32666
David Mintz <david@davidmintz.org>
2019-10-28 10:33:35 -0400

    interpreters-index.js: take out conditional "back" button

--- public/js/interpreters-index.js
+++ public/js/interpreters-index.js
@@ -22,12 +22,6 @@ $(function(){
            $('#btn-search-name').trigger("click");
         }
     });
-    if (document.referrer.indexOf('interpreters/language')!== -1 ||
-        document.referrer.indexOf("admin/people") !== -1) {
-        var btn_back = $(`<a class="btn btn-outline-secondary" role="button" title="back to ${document.referrer}"/>`)
-            .attr({href:document.referrer}).text("back");
-        $("#btn-edit").after(btn_back);
-    }
     $('#btn-search-name').on("click",function(event){
         event.preventDefault();
         var name = nameElement.val().trim();
