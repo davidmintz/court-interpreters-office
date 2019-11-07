ef0448533715ec00f80e3293f3189e1a43f8a1cd
David Mintz <david@davidmintz.org>
2019-11-07 13:05:47 -0500

    update judge-scraper for new SDNY site markup

--- bin/sdny/judge-scraper.js
+++ bin/sdny/judge-scraper.js
@@ -1,31 +1,44 @@
+/**
+ * SDNY judge screen-scraper for node.js
+ *
+ * crawls over the USDJs and USMJs information on the nysd.uscourts.gov website
+ * and compiles a basic directory -- name, courthouse, courtroom -- as JSON
+ * data.
+ *
+ */
 
 const axios = require("axios");
 const jsdom = require("jsdom");
+const https = require("https");
 const { JSDOM } = jsdom;
+const httpsAgent = new https.Agent({
+    // unfortunate, but couldn't avoid...
+    rejectUnauthorized : false
+    // ...because I couldn't solve the site's certificate errors
+});
 
-var data = {USMJ: {}, USDJ: {}};
+const base = "https://nysd.uscourts.gov";
 
 const get_judge_links = function(flavor){
     return new Promise(
         resolve => {
-            axios.get(`http://nysd.uscourts.gov/judges/${flavor}`)
+            axios.get(`${base}/judges/${flavor}-judges`,{httpsAgent})
             .then((response) => {
                 var dom = new JSDOM(response.data);
-                var links = dom.window.document.querySelectorAll("table.judge_info tr td a");
+                var links = dom.window.document.querySelectorAll(".judges-view-result a");
                 resolve(links);
             })
             .catch((error)=>{console.warn("oops"); console.log(error)});
-        },
-        reject => {console.warn("oops")}
+        }
     );
 };
 const parse_judge_info = function(url){
     return new Promise(
         resolve => {
-            axios.get(url).then((response)=>{
+            axios.get(`${base}${url}`,{httpsAgent}).then((response)=>{
                 var dom = new JSDOM(response.data);
                 var elements = dom.window.document
-                    .querySelectorAll(".mainblock table tr.whiteback td table tr td p");
+                    .querySelectorAll(".judge-detail-left, .courthouse-info");
                 var courthouse, courtroom, match;
                 elements.forEach(function(el){
                     var text = el.textContent.trim();
@@ -43,30 +56,21 @@ const parse_judge_info = function(url){
         }
     );
 };
-
+var data = {USMJ: {}, USDJ: {}};
 (async function() {
-
-    var usdj_links = await get_judge_links("District");
+    var usdj_links = await get_judge_links("district");
     for (var i = 0; i < usdj_links.length; i++) {
         var name = usdj_links[i].textContent.trim();
         var url = usdj_links[i].href;
         //console.log(`fetching ${url} for: ${name}, USDJ`);
-        info = await parse_judge_info(url);
-        if (info.courthouse && info.courtroom) {
-            data.USDJ[name] = info;
-        }
-        //data.USDJ[name] = await parse_judge_info(url);
+        data.USDJ[name] = await parse_judge_info(url);
     }
-    var usmj_links = await get_judge_links("Magistrate");
+    var usmj_links = await get_judge_links("magistrate");
     for (var i = 0; i < usmj_links.length; i++) {
         var name = usmj_links[i].textContent.trim();
         var url = usmj_links[i].href;
         //console.log(`fetching ${url} for: ${name}, USMJ`);
-        //info = await parse_judge_info(url);
-        if (info.courthouse && info.courtroom) {
-            data.USMJ[name] = info;
-        }
-        //data.USMJ[name] = await parse_judge_info(url);
+        data.USMJ[name] = await parse_judge_info(url);
     }
     console.log(JSON.stringify(data));
 })();
