5b091997e82657daed2353d64ec8d92d2cc8070c
David Mintz <david@davidmintz.org>
2019-11-25 11:14:47 -0500

    Rotation test: use repo getTask() to ensure full hydration

--- module/Rotation/test/RotationEntityTest.php
+++ module/Rotation/test/RotationEntityTest.php
@@ -70,14 +70,44 @@ class RotationEntityTest extends TestCase
         //return $task;
 
     }
+
+    public function __testFuckingSanity()
+    {
+        /** @var $repo InterpretersOffice\Admin\Rotation\Entity|RotationRepository */
+        $repo = $this->em->getRepository(Entity\Rotation::class);
+        $id = $this->em->createQuery('SELECT t.id FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
+            ->setParameters(['name'=>'%scheduling%'])->getSingleScalarResult();
+        $task = $repo->getTask($id);
+        $this->assertTrue(is_object($task));
+        $this->assertTrue($task instanceof Entity\Task);
+        $rotations = $task->getRotations();
+        $this->assertTrue($rotations instanceof \Doctrine\ORM\PersistentCollection);
+
+        //printf("\nrotations has : %s\n",count($rotations));
+        $r = $rotations->get(0);
+        $this->assertTrue(is_object($r));
+        $this->assertEquals('2015-05-18',$r->getStartDate()->format('Y-m-d'));
+        //printf("\nrotation at index 0 has start date: %s\n",$r->getStartDate()->format('Y-m-d'));
+        $example_date = new \DateTime('2019-11-06');
+        $repo = $this->em->getRepository(Entity\Rotation::class);
+        $result = $repo->getDefaultAssignedPerson($task,$example_date);
+        printf("\ngetDefaultAssignedPerson returns: %s\n",gettype($result));
+        $this->assertTrue($result instanceof \InterpretersOffice\Entity\Person);
+        $name = $result->getFullName();
+        printf("\nfucking name is %s\n",$name);
+
+    }
     /**
      *
      *
      */
     public function testGetDefaultSchedulingVictim()
     {
-        $task = $this->em->createQuery('SELECT t FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
-            ->setParameters(['name'=>'%scheduling%'])->getOneOrNullResult();
+        /** @var $repo InterpretersOffice\Admin\Rotation\Entity|RotationRepository */
+        $repo = $this->em->getRepository(Entity\Rotation::class);
+        $id = $this->em->createQuery('SELECT t.id FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
+            ->setParameters(['name'=>'%scheduling%'])->getSingleScalarResult();
+        $task = $repo->getTask($id);
         $this->assertTrue($task instanceof Entity\Task);
         $repo = $this->em->getRepository(Entity\Rotation::class);
         $example_date = new \DateTime('2019-11-06');
@@ -97,10 +127,11 @@ class RotationEntityTest extends TestCase
 
     public function testGetActualSchedulingVictimWhenSubstitutionOccurs()
     {
-        $task = $this->em->createQuery('SELECT t FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
-            ->setParameters(['name'=>'%scheduling%'])->getOneOrNullResult();
-
+        /** @var $repo InterpretersOffice\Admin\Rotation\Entity|RotationRepository */
         $repo = $this->em->getRepository(Entity\Rotation::class);
+        $id = $this->em->createQuery('SELECT t.id FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
+            ->setParameters(['name'=>'%scheduling%'])->getSingleScalarResult();
+        $task = $repo->getTask($id);
         $example_date = new DateTime('2019-10-25');
         $actual = $repo->getAssignedPerson($task,$example_date);
         $this->assertEquals("Humberto",$actual['assigned']->getFirstName());
@@ -109,10 +140,11 @@ class RotationEntityTest extends TestCase
 
     public function testGetDefaultSaturdayVictim()
     {
-        $task = $this->em->createQuery('SELECT t FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
-            ->setParameters(['name'=>'%saturday%'])->getOneOrNullResult();
-        $this->assertTrue($task instanceof Entity\Task);
+        /** @var $repo InterpretersOffice\Admin\Rotation\Entity|RotationRepository */
         $repo = $this->em->getRepository(Entity\Rotation::class);
+        $id = $this->em->createQuery('SELECT t.id FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
+            ->setParameters(['name'=>'%saturday%'])->getSingleScalarResult();
+        $task = $repo->getTask($id);
         $example_date = new \DateTime('2019-11-06');
         $default = $repo->getDefaultAssignedPerson($task,$example_date);
         $this->assertEquals("Erika",$default->getFirstName());
@@ -121,10 +153,11 @@ class RotationEntityTest extends TestCase
 
     public function testGetActualSaturdayVictimWhenSubstitutionOccurs()
     {
-        $task = $this->em->createQuery('SELECT t FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
-            ->setParameters(['name'=>'%saturday%'])->getOneOrNullResult();
-
+        /** @var $repo InterpretersOffice\Admin\Rotation\Entity|RotationRepository */
         $repo = $this->em->getRepository(Entity\Rotation::class);
+        $id = $this->em->createQuery('SELECT t.id FROM '.Entity\Task::class. ' t WHERE t.name LIKE :name')
+            ->setParameters(['name'=>'%saturday%'])->getSingleScalarResult();
+        $task = $repo->getTask($id);
         $example_date = new DateTime('2019-11-02');
         $actual = $repo->getAssignedPerson($task,$example_date);
         //printf("\ndate %s; actually assigned: %s\n",$example_date->format('D d-M-Y'),$actual['assigned']->getFirstName());
