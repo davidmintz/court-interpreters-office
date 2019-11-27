c4f8fa8db5150f226345e5adcc34e336c27d03d4
David Mintz <david@davidmintz.org>
2019-11-27 13:48:13 -0500

    task-rotation admin UI

--- public/js/admin/tasks.js
+++ public/js/admin/tasks.js
@@ -41,29 +41,30 @@ $(function(){
         var date = new moment(pretty_date, "ddd DD-MMM-YYYY").format("YYYY-MM-DD");
         var task_id = $(".task").data("task_id");
         $.get(`/admin/rotations/assignments/${date}/${task_id}`)
-        .then(
-            res => {
+        .then(res =>
+            {
                 console.warn(`${res.start_date} is start date of the newly-fetched rotation`);
                 if (res.start_date !== $("#dialog").data("rotation_start_date")) {
                     console.warn("which differs from that currently displayed");
                     var n = $("#dialog .form-check").length - 1;
                     slice = $("#dialog .person-wrapper").slice(0,n);
+                    // yes, ugly. requires us to maintain HTML in both here
+                    // and in the viewscript
                     let html = "";
                     res.rotation.forEach((p, i)=>{
-                        html += `<div class="person-wrapper border border-bottom-0 px-2 py-1">
+                        html +=
+                         `<div class="person-wrapper border border-bottom-0 px-2 py-1">
                             <div class="form-check">
-                                <input data-id="${p.id}" class="form-check-input person" type="radio" name="person" id="person-${p.id}" value="person">
+                                <input data-id="${p.id}" class="form-check-input person" value="${p.id}" type="radio" name="person" id="person-${p.id}" value="person">
                                 <label class="form-check-label" for="person-${p.id}">
                                     ${p.name}
                                 </label>
                             </div>
-                        </div>`;
+                         </div>`;
                     })
                     $("#dialog p.subtitle").after(html);
                     slice.remove();
                     $("#dialog").data({rotation_start_date:res.start_date});
-                } else {
-                    console.log("start dates match");
                 }
                 var current = $(".assignment-person");
                 var disabled = $(".person:disabled");
@@ -76,14 +77,12 @@ $(function(){
         );
         $("#dialog .modal-title .assignment-date").text(`, ${pretty_date}`);
 
-    })
-    .on("click",`option[value="other"]`,
-        (e)=>{
-            console.log("shit was clicked, display input for autocompletion etc");
-            $("#dialog select").attr({hidden: true});
-            $("#dialog input").removeAttr("hidden");
-        }
-    );
+    });
+    var radio = $("#dialog input[type=radio]").last();
+    var autocomplete_field = $("#rotation-autocomplete");
+    autocomplete_field.on("focus",e=>{
+        if (! radio.attr("checked")) { radio.attr({checked:true});}
+    });
 
-
+    radio.on("click",()=>autocomplete_field.focus());
 });
