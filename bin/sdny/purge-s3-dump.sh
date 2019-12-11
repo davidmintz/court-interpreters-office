fc6081985d92b12c4709d883f3f2fcd40b5c1aa6
david <david@davidmintz.org>
2019-12-11 09:56:19 -0500

    mumble mumble bash script

--- bin/sdny/purge-s3-dump.sh
+++ bin/sdny/purge-s3-dump.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+# needs work!
+
 FILES=($(s3cmd -c $HOME/.dh-sdny.s3cfg  ls s3://sdny/office|grep sql.gz|awk '{ print $4}'))
-count=${#FILES[@]}
+count=$((1 + ${#FILES[@]}))
+
 echo "we have ${count} files."
-if [ ${#FILES[@]} > 4 ];
+if [ ${count} > 4 ];
 then
-    num_to_remove=$((count - 5))
+    num_to_remove=$((count - 4))
     echo "need to remove: $num_to_remove files"
-    for ((i=0; i<=$num_to_remove; i++));
+    for ((i=0; i<$num_to_remove; i++));
         do s3cmd  -c $HOME/.dh-sdny.s3cfg del ${FILES[i]}
     done
 fi
