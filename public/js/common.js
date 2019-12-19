f74df07dc126aeafcfe42c216a98a27a109e9a31
David Mintz <david@davidmintz.org>
2019-12-19 13:55:51 -0500

    rewrite displayValidatorErrors()

--- public/js/common.js
+++ public/js/common.js
@@ -27,13 +27,103 @@ jQuery.ajaxSetup({
 });
 
 /**
+ * helper function for showing validation error messages
+ *
+ * @param  {string} element_name
+ * @param  {string} message
+ * @param  {boolean} verbose
+ * @return {void}
+ */
+const show_error_message = function(element_name, message, verbose){
+    var debug = function(message){
+        if (verbose) {console.log(message);}
+    };
+    debug(`show_error_message: element name ${element_name}, message ${message}`);
+    var error_div = $(`#error_${element_name}`);
+    if (error_div.length) {
+        debug("found existing error div");
+        return error_div.html(message).show();
+    } // else
+    var element = $(`#${element_name}`);
+    if (! element.length) {
+        // try harder
+        var selector = "#" + field.replace(/([A-Z])/g,"_$1").toLowerCase();
+        element = $(selector);
+    }
+    if (element.length) {
+        debug(`found element for ${element_name}, creating div...`);
+        $("<div/>").addClass("alert alert-warning validation-error")
+            .html(message).attr({id:`error_${element_name}`})
+            .insertAfter(element);
+    } else {
+        debug(`can't figure out where to put error "${message}" for element ${element_name}`);
+    }
+};
+
+/**
  * displays validation errors on a form
  *
  * @param object validationErrors
  * @param object options
  * @returns void
  */
-var displayValidationErrors = function(validationErrors,options) {
+const displayValidationErrors = function(errors,options,verbose)
+{
+    var debug = function(message) {
+        if (verbose) { console.log(message);}
+    }
+    debug("running NEW edition displayValidationErrors()");
+    $(".validation-error").hide().empty();
+    var keys = Object.keys(errors);
+    keys.forEach((element)=>{
+        for (var prop in errors[element]) {
+            // debug("within which errors[element][prop] is a: "+typeof errors[element][prop])
+            if (typeof errors[element][prop] === "string") {
+                // debug(`found error message "${errors[element][prop]}" for element ${element}`);
+                show_error_message(element,errors[element][prop]);
+            } else {
+                //debug("recursing... ");
+                displayValidationErrors(errors[element]);
+            }
+        }
+    });
+}
+
+
+/**
+ * on xhr failure
+ *
+ * used in conjunction with view helper
+ * module/InterpretersOffice/src/View/Helper/ErrorMessage.php
+ *
+ */
+const fail = function(response) {
+    var msg;
+    if (response.status === 403 && response.responseJSON) {
+        msg = response.responseJSON.message;
+    } else {
+        msg = `<p>Sorry &mdash; a system error happened while
+        processing your last request. If the problem recurs, please notify your site
+        administrator for assistance.</p><p>We apologize for the inconvenience.</p>`;
+    }
+    $(".alert-success").hide();
+    $("#error-message").html(msg).parent().show();
+    // for development...
+    //$("html").html(response.responseText);
+    console.log(response.responseText);
+};
+
+$(".alert button[data-hide]").on("click",function(e){
+    $(this).parent().slideUp();
+});
+
+/* cat picture? */
+// if (43 === parseInt(Math.random()*1000)) {
+//
+// }
+//
+/*
+var __displayValidationErrors = function(validationErrors,options) {
     var debug = function(message) {
         if (! options || !options.debug) { return; }
         console.log(message);
@@ -41,6 +131,7 @@ var displayValidationErrors = function(validationErrors,options) {
     $(".validation-error").hide().empty();
     debug("running displayValidationErrors()");
     //var debug = (options && options.debug) || false;
+    debug(Object.keys(validationErrors));
     for (var field in validationErrors) {
         debug("looking at field: "+field);
         for (var key in validationErrors[field]) {
@@ -86,35 +177,4 @@ var displayValidationErrors = function(validationErrors,options) {
         }
     }
 };
-
-/**
- * on xhr failure
- *
- * used in conjunction with view helper
- * module/InterpretersOffice/src/View/Helper/ErrorMessage.php
- *
  */
-const fail = function(response) {
-    var msg;
-    if (response.status === 403 && response.responseJSON) {
-        msg = response.responseJSON.message;
-    } else {
-        msg = `<p>Sorry &mdash; a system error happened while
-        processing your last request. If the problem recurs, please notify your site
-        administrator for assistance.</p><p>We apologize for the inconvenience.</p>`;
-    }
-    $(".alert-success").hide();
-    $("#error-message").html(msg).parent().show();
-    // for development...
-    //$("html").html(response.responseText);
-    console.log(response.responseText);
-};
-
-$(".alert button[data-hide]").on("click",function(e){
-    $(this).parent().slideUp();
-});
-
-/* cat picture? */
-// if (43 === parseInt(Math.random()*1000)) {
-//
-// }
