9c90128ac92314e16082820662c35fa0d023a578
david <david@davidmintz.org>
2020-03-04 13:16:00 -0500

    split off docket-annotations controller REST methods to separate controller

---
+++ module/Admin/src/Controller/RestfulDocketAnnotationsController.php
@@ -0,0 +1,53 @@
+<?php
+
+/** module/Admin/src/Controller/DocketAnnotationsController.php */
+
+namespace InterpretersOffice\Admin\Controller;
+
+use Laminas\Mvc\Controller\AbstractRestfulController;
+use Laminas\View\Model\ViewModel;
+use Laminas\View\Model\JsonModel;
+use Laminas\Authentication\AuthenticationServiceInterface;
+use Laminas\Validator\Csrf;
+use Doctrine\ORM\EntityManagerInterface;
+use InterpretersOffice\Entity;
+
+use InterpretersOffice\Admin\Service\DocketAnnotationService;
+
+class RestfulDocketAnnotationsController extends AbstractRestfulController
+{
+
+    /**
+     * annotation service
+     * @var DocketAnnotationService
+     */
+    private $service;
+
+    public function testAction()
+    {
+        return new JsonModel(['status'=>'test is working']);
+    }
+
+    /**
+     * constructor
+     *
+     * @param DocketAnnotationService $service
+     */
+    public function __construct(DocketAnnotationService $service)
+    {
+        $this->service = $service;
+    }
+    public function delete($id)
+    {
+        return new JsonModel(['status'=>'boink! DELETE']);
+    }
+
+    public function update($id, $data) {
+        return new JsonModel(['status'=>'boink! UPDATE']);
+    }
+
+    public function create($data)
+    {
+        return new JsonModel(['status'=>'boink! CREATE']);
+    }
+}
