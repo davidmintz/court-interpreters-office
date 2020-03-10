35e39ab2183a7fd8fec6892abd1fb2c747111dd1
David Mintz <david@davidmintz.org>
2020-03-10 13:46:51 -0400

    add crude interactive personal-data import script

---
+++ bin/sdny/personal-data-import.php
@@ -0,0 +1,95 @@
+#!/usr/bin/env php
+<?php
+
+require(__DIR__.'/../../vendor/autoload.php');
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+
+use Laminas\Crypt\BlockCipher;
+use Laminas\Crypt\Symmetric\Openssl;
+
+class PersonalDataImportCommand extends Command
+{
+    // the name of the command (the part after "bin/console")
+    protected static $defaultName = 'setup:import:personal-data';
+
+    /**
+     * @todo database names as command-line options
+     * 
+     * @return void
+     */
+    protected function configure()
+    {
+        // $this
+        //     ->setDescription('Describe args behaviors')
+        //     ->setDefinition(
+        //         new InputDefinition([
+        //             new InputOption('foo', 'f'),
+        //             new InputOption('bar', 'b', InputOption::VALUE_REQUIRED),
+        //             new InputOption('cat', 'c', InputOption::VALUE_OPTIONAL),
+        //         ])
+        //     );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        
+        $helper = $this->getHelper('question');
+        $question = new Question('Enter the encryption cipher for the target db:  ');
+        $question->setHidden(true)->setHiddenFallback(false);        
+        $cipher = $helper->ask($input, $output, $question);
+        
+
+        $question2 = new Question('Enter the cipher for the old db:  ');
+        $question2->setHidden(true)->setHiddenFallback(false);
+        
+        $old_cipher = $helper->ask($input, $output, $question2);        
+        $db = require(__DIR__.'/connect.php');        
+        $db->exec("use dev_interpreters");
+        $select = $db->prepare('SELECT interp_id AS id, AES_DECRYPT(ssn,:cipher) AS ssn, '
+        . 'AES_DECRYPT(dob,:cipher) AS dob FROM interpreters '
+        . 'WHERE (ssn IS NOT NULL OR dob IS NOT NULL)');        
+        $result = $select->execute([':cipher'=>$old_cipher]);
+        if (! $result) {
+            exit(print_r($db->errorInfo(),true));
+        }
+        $total = $select->rowCount();
+        echo "total found: $total\n";
+        $block_cipher = new BlockCipher(new Openssl);
+        $block_cipher->setKey($cipher);
+        $db->exec("use office");
+        $update = $db->prepare("UPDATE interpreters SET ssn = :ssn, dob = :dob WHERE id = :id");
+        $i = 0;
+        while($row = $select->fetch()) {
+            if (!$row['dob'] && !$row['ssn']) {
+                continue;
+            }
+            $params = [
+                ':ssn' =>$row['ssn'] ? $block_cipher->encrypt($row['ssn']) : null, 
+                ':dob' =>$row['dob'] ? $block_cipher->encrypt($row['dob']) : null, 
+                ':id'  => $row['id'],
+            ];
+            $outcome = $update->execute($params);
+            // var_dump($outcome);
+            //print_r($row);
+            printf ("completed: %d\r",++$i);
+            echo "\ndone\n";
+        }
+
+        return 0;
+    }   
+}
+
+
+$application = new Application();
+$application->add(new PersonalDataImportCommand);
+$application->run();
+
+
