18c27f798ffd1dbf744176a49c918e137d570f37
david <david@davidmintz.org>
2020-03-18 12:07:42 -0400

    improve regexp in SQL de-falsification script for "mag cal"

--- bin/sdny/un-falsify.sql
+++ bin/sdny/un-falsify.sql
@@ -7,4 +7,11 @@ SET e.submitter_id = p.id,
 e.anonymous_submitter_id = NULL
 WHERE h.name = "Magistrates"
 AND e.submitter_id IS NULL
-AND e.comments REGEXP '([[:<:]]|^)mag(i?s(trates?)?)? +cal(; *|endar)?([[:<:]]|$)';
+AND e.comments REGEXP  '([[:<:]]|^)mag\.?(i?s(trates?)?)? *cal(endar)?(; *)?([[:>:]]|\.|$)';
+-- AND e.comments REGEXP "[[:<:]]mag(is(trates?)?)? *cal(endar)?[[:>:]]" 
+--  "[[:<:]]mag(is(trates?)?)? *cal(endar)?[[:>:]]" 
+-- https://www.regular-expressions.info/wordboundaries.html
+-- The POSIX standard defines [[:<:]] as a start-of-word boundary, and [[:>:]] as an end-of-word boundary. 
+-- Though the syntax is borrowed from POSIX bracket expressions, these tokens are word boundaries that have 
+-- nothing to do with and cannot be used inside character classes. Tcl and GNU also support POSIX word boundaries. 
+-- PCRE supports POSIX word boundaries starting with version 8.34. Boost supports them in all its grammars.
