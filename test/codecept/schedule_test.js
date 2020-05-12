0eb97b6da741c7fd791443691217b7a1129ddfe7
david <david@davidmintz.org>
2020-05-12 00:29:46 -0400

    work on a CodeceptJS test

--- test/codecept/schedule_test.js
+++ test/codecept/schedule_test.js
@@ -2,19 +2,19 @@
 
 Feature('MOTD/MOTW');
 
-// Before(login => {
-//    // login('admin'); // login using user session
-//  });
+Before(login => {
+    login('admin'); // login using user session
+ });
 
-// Scenario('view schedule', (I) => {
-//     // I.login();
-//     I.amOnPage("admin/schedule/2020/05/13");
-//     I.see("Wed 13 May 2020");
-// });
+Scenario('view schedule', (I) => {
+    // I.login();
+    I.amOnPage("admin/schedule/2020/05/13");
+    I.see("Wed 13 May 2020");
+});
 
 Scenario('edit MOTD via schedule', (I) => {
 
-    I.login();
+    // I.login();
     I.amOnPage("admin/schedule/2020/05/13");
     I.see("Wed 13 May 2020");
     var link = locate("a.nav-link").withText("notes");
@@ -25,14 +25,16 @@ Scenario('edit MOTD via schedule', (I) => {
     I.seeElement("#motd");
     I.see("MOTD: Wed 13-May-2020",{css:"#motd"});
     I.click("#motd a.btn");
-    I.see("MOTD for Wednesday 13-May-2020",{css: "#motd-content h4"});
+    I.see("MOTD for Wednesday 13-May-2020",{css: "h4"});
     I.seeElement("textarea[name=content]");
     I.fillField("textarea[name=content]","This is now the MOTD for Wed 13-May-2020");
+    I.click("save");
+    I.see("MOTD saved");
 });
 
 Scenario('edit MOTD directly', (I) => {
 
-    I.login();
+    // I.login();
     I.amOnPage("admin/notes/date/2020-05-18/motd");
     I.see("MOTD for Monday 18-May-2020");
     // var link = locate("a.nav-link").withText("notes");
