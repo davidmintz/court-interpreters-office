05432f48eabb9c03039b45dfd4014d81737785dc
david <david@davidmintz.org>
2020-06-30 17:58:05 -0400

    restore RedisFactory

---
+++ module/InterpretersOffice/src/Service/Factory/RedisFactory.php
@@ -0,0 +1,32 @@
+<?php /** module/InterpretersOffice/src/Service/Factory/RedisFactory.php */
+declare(strict_types=1);
+
+namespace InterpretersOffice\Service\Factory;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use Redis;
+
+/**
+ * factory for Redis instance used for result caching
+ */
+class RedisFactory implements FactoryInterface
+{
+    /**
+     * implements FactoryInterface.
+     *
+     * @param ContainerInterface $container
+     * @param string             $requestedName
+     * @param array              $options
+     *
+     * @return Redis $redis instance for result cache
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null): Redis
+    {
+        /** @var Redis $redis */
+        $redis = new \Redis();
+        $redis->connect("localhost");
+
+        return $redis;
+    }
+}
