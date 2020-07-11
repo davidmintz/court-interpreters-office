41c9f4178b092b16d73fb1545518044967df79c4
david <david@davidmintz.org>
2020-07-10 23:54:46 -0400

    add forgotten admin IndexController factory

---
+++ module/Admin/src/Controller/Factory/IndexControllerFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace InterpretersOffice\Admin\Controller\Factory;
+
+use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\Factory\FactoryInterface;
+use InterpretersOffice\Admin\Controller\IndexController;
+use InterpretersOffice\Requests\Entity\Request;
+
+class IndexControllerFactory implements FactoryInterface
+{
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param null|array $options
+     * @return IndexController
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+        
+        $module_manager = $container->get("ModuleManager");
+        $modules = array_keys($module_manager->getLoadedModules());
+        $config = [
+            'acl'=>$container->get('acl'),
+            'modules' => $modules,
+        ];
+        if (in_array('InterpretersOffice\Requests',$modules)) {
+            /** @var InterpretersOffice\Requests\Entity\RequestRepository $repo  */
+            $repo = $container->get('entity-manager')->getRepository(Request::class);
+            $config['requests_pending'] = $repo->countPending();
+        }
+
+        return new IndexController($config);
+    }
+}
